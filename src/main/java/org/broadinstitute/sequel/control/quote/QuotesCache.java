c071eb7d6f8bbe724fbe0292e424326abea9e6e3
U-CHARLES\andrew <andrew@CPAF5-0E0.broad.mit.edu>
2012-03-01 12:42:01 -0500

    Added documentation

--- src/main/java/org/broadinstitute/sequel/control/quote/QuotesCache.java
+++ src/main/java/org/broadinstitute/sequel/control/quote/QuotesCache.java
@@ -13,18 +13,36 @@ import java.util.Set;
 public class QuotesCache {
     
     private final Quotes quotes;
-    
+
+    /**
+     * New one up, using the #quotes 
+     * as the backing store.  This is because
+     * we assume that #quotes is going
+     * to come from {@link org.broadinstitute.sequel.control.quote.QuoteServiceImpl#getAllSequencingPlatformQuotes()}
+     * @param quotes
+     */
     public QuotesCache(Quotes quotes) {
         if (quotes == null) {
             throw new NullPointerException("Quotes cannot be null");
         }
         this.quotes = quotes;
     }
-    
+
+    /**
+     * Returns the underlying
+     * list of {@link Quote}s
+     * @return
+     */
     public Collection<Quote> getQuotes() {
         return this.quotes.getQuotes();
     }
-    
+
+    /**
+     * Returns the unique set of
+     * {@link Funding} objects from
+     * the cache.
+     * @return
+     */
     public Collection<Funding> getAllFundingSources() {
         Set<Funding> fundingSources = new HashSet<Funding>();
         for (Quote quote : quotes.getQuotes()) {
@@ -38,7 +56,13 @@ public class QuotesCache {
         }
         return fundingSources;
     }
-    
+
+    /**
+     * Finds all {@link Quote} objects that reference
+     * the given #grantDescription as {@link Funding#grantDescription}.
+     * @param grantDescription
+     * @return
+     */
     public Collection<Quote> getQuotesForGrantDescription(String grantDescription) {
         if (grantDescription == null) {
             throw new NullPointerException("grantDescription cannot be null.");
