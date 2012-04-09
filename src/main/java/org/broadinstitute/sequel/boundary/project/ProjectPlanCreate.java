35b0854a528879ca6103d75ccfb7738be76ded83
Brian Reilly <breilly@broadinstitute.org>
2012-04-09 16:04:02 -0400

    added dependency on seam-faces (still need to review to ensure a minimal dependency set) finished page for adding project plans to a project

--- src/main/java/org/broadinstitute/sequel/boundary/project/ProjectPlanCreate.java
+++ src/main/java/org/broadinstitute/sequel/boundary/project/ProjectPlanCreate.java
@@ -2,29 +2,51 @@ package org.broadinstitute.sequel.boundary.project;
 
 import org.broadinstitute.sequel.entity.DB;
 import org.broadinstitute.sequel.entity.project.Project;
+import org.broadinstitute.sequel.entity.project.ProjectPlan;
+import org.broadinstitute.sequel.entity.project.WorkflowDescription;
+import org.broadinstitute.sequel.presentation.AbstractJsfBean;
 
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author breilly
  */
-@Model
-public class ProjectPlanCreate {
+@Named
+@ConversationScoped
+public class ProjectPlanCreate extends AbstractJsfBean implements Serializable {
 
     @Inject private DB db;
+    @Inject private ProjectDetail projectDetail;
+    @Inject private Conversation conversation;
 
     private String projectName;
     private Project project;
 
     private String projectPlanName;
+    private WorkflowDescription workflowDescription;
 
-    public void loadProject() {
-        project = db.findByProjectName(projectName);
+    public void beginAdd() {
+        if (project == null) {
+            project = db.findByProjectName(projectName);
+            conversation.begin();
+        }
+    }
+
+    public List<WorkflowDescription> getAllWorkflowDescriptions() {
+        return db.getAllWorkflowDescriptions();
     }
 
     public String addProjectPlan() {
-        return null;
+        ProjectPlan plan = new ProjectPlan(project, projectPlanName, workflowDescription);
+        conversation.end();
+        projectDetail.setProjectName(projectName);
+        return redirect("detail");
     }
 
     public String getProjectName() {
@@ -49,5 +71,13 @@ public class ProjectPlanCreate {
 
     public void setProjectPlanName(String projectPlanName) {
         this.projectPlanName = projectPlanName;
+    }
+
+    public WorkflowDescription getWorkflowDescription() {
+        return workflowDescription;
+    }
+
+    public void setWorkflowDescription(WorkflowDescription workflowDescription) {
+        this.workflowDescription = workflowDescription;
     }
 }
--- src/main/java/org/broadinstitute/sequel/entity/DB.java
+++ src/main/java/org/broadinstitute/sequel/entity/DB.java
@@ -1,11 +1,15 @@
 package org.broadinstitute.sequel.entity;
 
 import org.broadinstitute.sequel.entity.project.Project;
+import org.broadinstitute.sequel.entity.project.WorkflowDescription;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,6 +19,14 @@ import java.util.Map;
 public class DB {
 
     private Map<String, Project> projects = new HashMap<String, Project>();
+    private Map<String, WorkflowDescription> workflowDescriptions = new HashMap<String, WorkflowDescription>();
+
+    public DB() {
+        addWorkflowDescription(new WorkflowDescription("Hybrid Selection", "v7.2", null, null));
+        addWorkflowDescription(new WorkflowDescription("Whole Genome Shotgun", "v7.2", null, null));
+    }
+
+    // Project
 
     public void addProject(Project project) {
         if (project.getProjectName() == null) {
@@ -32,5 +44,32 @@ public class DB {
 
     public Project findByProjectName(String projectName) {
         return projects.get(projectName);
+    }
+
+    // WorkflowDescription
+
+    public void addWorkflowDescription(WorkflowDescription workflowDescription) {
+        if (workflowDescription.getWorkflowName() == null) {
+            throw new IllegalArgumentException("Non-null constraint violation: WorkflowDescription.workflowName");
+        }
+        if (workflowDescriptions.containsKey(workflowDescription.getWorkflowName())) {
+            throw new IllegalArgumentException("Unique constraint violation: WorkflowDescription.workflowName");
+        }
+        workflowDescriptions.put(workflowDescription.getWorkflowName(), workflowDescription);
+    }
+
+    public List<WorkflowDescription> getAllWorkflowDescriptions() {
+        List<WorkflowDescription> result = new ArrayList<WorkflowDescription>(workflowDescriptions.values());
+        Collections.sort(result, new Comparator<WorkflowDescription>() {
+            @Override
+            public int compare(WorkflowDescription w1, WorkflowDescription w2) {
+                return w1.getWorkflowName().compareTo(w2.getWorkflowName());
+            }
+        });
+        return result;
+    }
+
+    public WorkflowDescription findByWorkflowDescriptionName(String workflowDescriptionName) {
+        return workflowDescriptions.get(workflowDescriptionName);
     }
 }
