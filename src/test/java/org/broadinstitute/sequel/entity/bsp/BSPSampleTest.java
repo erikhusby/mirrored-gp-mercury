ab51f9cea69ed41fda45f0690362dd6c293904a0
Andrew Zimmer <andrew@broadinstitute.org>
2012-04-27 15:34:02 -0400

    fixed bsp sample test...i think i did this on master already...

--- src/test/java/org/broadinstitute/sequel/entity/bsp/BSPSampleTest.java
+++ src/test/java/org/broadinstitute/sequel/entity/bsp/BSPSampleTest.java
@@ -38,13 +38,14 @@ public class BSPSampleTest extends ContainerTest {
     @Test(groups = {EXTERNAL_INTEGRATION})
     public void test_patient_id_mock() {
         List<String[]> resultColumns = new ArrayList<String[]>(1);
-        resultColumns.add(new String[] {"Bill the Cat","2","3","4","5","6","7"});
+        resultColumns.add(new String[] {"Bill the Cat","2","3","4","5","6","7","8"});
         BSPSampleSearchService service = EasyMock.createMock(BSPSampleSearchService.class);
         Collection<String> samplesNames = new ArrayList<String>();
         String sampleName = "Sample1";
         samplesNames.add(sampleName);
         EasyMock.expect(service.runSampleSearch(
                 (Collection<String>) EasyMock.anyObject(),
+                (BSPSampleSearchColumn)EasyMock.anyObject(),
                 (BSPSampleSearchColumn)EasyMock.anyObject(),
                 (BSPSampleSearchColumn)EasyMock.anyObject(),
                 (BSPSampleSearchColumn)EasyMock.anyObject(),
