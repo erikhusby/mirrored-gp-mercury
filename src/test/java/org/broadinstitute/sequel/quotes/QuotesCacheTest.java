34d4b4c6e1f101b983a392ed3d127c09926c393e
Brian Reilly <breilly@broadinstitute.org>
2012-05-04 08:42:53 -0400

    changed some test group specifications to use constants in TestGroups

--- src/test/java/org/broadinstitute/sequel/quotes/QuotesCacheTest.java
+++ src/test/java/org/broadinstitute/sequel/quotes/QuotesCacheTest.java
@@ -17,7 +17,7 @@ public class QuotesCacheTest {
     Quote quote3 = new Quote("DNA34",new QuoteFunding(new FundingLevel("100",new Funding(Funding.FUNDS_RESERVATION,"Cheap Grant"))));
     Quote quote4 = new Quote("DNA35",new QuoteFunding(new FundingLevel("50",new Funding(Funding.FUNDS_RESERVATION,"NHGRI"))));
 
-    @Test(groups = {DATABASE_FREE})
+    @Test(groups = DATABASE_FREE)
     public void test_quotes_for_funding_source() throws Exception {
         Quotes quotes = new Quotes();
         Funding targetSource = new Funding(Funding.FUNDS_RESERVATION,"Magical Infinite Grant");
@@ -39,7 +39,7 @@ public class QuotesCacheTest {
         Assert.assertFalse(foundQuotes.contains(quote3));
     }
 
-    @Test(groups = {DATABASE_FREE})
+    @Test(groups = DATABASE_FREE)
     public void test_unique_funding_soures() throws Exception {
         Quotes quotes = new Quotes();
         quotes.addQuote(quote1);
@@ -62,7 +62,7 @@ public class QuotesCacheTest {
         Assert.assertEquals(quotes.getQuotes(),cache.getQuotes());
     }
 
-    @Test(groups = {EXTERNAL_INTEGRATION})
+    @Test(groups = EXTERNAL_INTEGRATION)
     public void test_known_good_funding_sources() throws Exception {
 
         long start = System.currentTimeMillis();
