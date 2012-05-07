42b5ccbaf270adbbb9d1a422c9c8e8596fa28002
scott matmatthews <scottmat@broadinstitute.org>
2012-05-07 16:28:21 -0400

    Added Unit Test Cases

--- src/main/java/org/broadinstitute/sequel/entity/authentication/AuthorizedRole.java
+++ src/main/java/org/broadinstitute/sequel/entity/authentication/AuthorizedRole.java
@@ -1,6 +1,13 @@
 package org.broadinstitute.sequel.entity.authentication;
 
 /**
+ *
+ * AuthorizedRole is an entity that defines the roles that a user can have within the system.  These roles are utilized
+ * in the Authorization process of the application level security
+ *
+ *
+ * {@see PageAuthorization}
+ *
  * @author Scott Matthews
  *         Date: 5/1/12
  *         Time: 4:02 PM
@@ -10,23 +17,53 @@ public class AuthorizedRole {
     private Long roleId;
     private String roleName;
 
+    /**
+     * Basic constructor taking in the name of the role
+     * @param roleNameInIn
+     */
     public AuthorizedRole(String roleNameInIn) {
-        roleName = roleNameInIn;
+        setRoleName(roleNameInIn);
     }
 
+    /**
+     * Basic accessor for retrieving the database generated ID for a particular Authorized Role Instance
+     * @return
+     */
     public Long getRoleId() {
         return roleId;
     }
 
+    /**
+     * Basic setter for setting the unique ID for this role
+     * @param roleIdIn
+     */
     public void setRoleId(Long roleIdIn) {
         roleId = roleIdIn;
     }
 
+    /**
+     * Basic accessor for retrieving the unique name for the role
+     * @return
+     */
     public String getRoleName() {
         return roleName;
     }
 
+
+    /**
+     * Basic setter for defining the name for the role
+     * @param roleNameIn
+     */
     public void setRoleName(String roleNameIn) {
+
+        if(null == roleNameIn) {
+            throw new NullPointerException("The role name cannot be null");
+        }
+
+        if(roleNameIn.isEmpty()) {
+            throw new IllegalArgumentException("The role name must be set");
+        }
+
         roleName = roleNameIn;
     }
 }
