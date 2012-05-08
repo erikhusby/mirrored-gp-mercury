52c3e14f75d7976a5ccc9dfc8bd64418a24e4f14
scott matmatthews <scottmat@broadinstitute.org>
2012-05-08 10:32:19 -0400

    Added Unit Test Case for authentication service

---
+++ src/test/java/org/broadinstitute/sequel/boundary/authentication/AuthenticationServiceTest.java
@@ -0,0 +1,48 @@
+package org.broadinstitute.sequel.boundary.authentication;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author Scott Matthews
+ *         Date: 5/7/12
+ *         Time: 4:37 PM
+ */
+public class AuthenticationServiceTest {
+
+
+    @Test
+    public void test_retrieve_authorized_roles() throws Exception {
+
+    }
+
+    @Test
+    public void test_add_new_authorization() throws Exception {
+
+    }
+
+    @Test
+    public void test_is_page_protected() throws Exception {
+
+    }
+
+    @Test
+    public void test_get_all_authorized_pages() throws Exception {
+
+    }
+
+    @Test
+    public void test_find_by_page_name() throws Exception {
+
+    }
+
+    @Test
+    public void test_add_roles_to_page() throws Exception {
+
+    }
+
+    @Test
+    public void test_retrieve_all_roles() throws Exception {
+
+    }
+
+}
