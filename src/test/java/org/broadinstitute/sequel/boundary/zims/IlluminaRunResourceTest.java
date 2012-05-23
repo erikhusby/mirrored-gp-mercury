d4befa11561d2818f1719459800b007e4bb7f462
Andrew Zimmer <andrew@broadinstitute.org>
2012-05-23 09:14:25 -0400

    removed sleep()

--- src/test/java/org/broadinstitute/sequel/boundary/zims/IlluminaRunResourceTest.java
+++ src/test/java/org/broadinstitute/sequel/boundary/zims/IlluminaRunResourceTest.java
@@ -98,10 +98,6 @@ public class IlluminaRunResourceTest extends Arquillian  {
         assertEquals(run.getName(),RUN_NAME);
         doAssertions(zamboniRun,run);
 
-        try {
-            Thread.sleep(60 * 1000);
-        }
-        catch(InterruptedException e) {}
     }      
     
     public static void doAssertions(TZamboniRun thriftRun,ZimsIlluminaRun runBean) {
