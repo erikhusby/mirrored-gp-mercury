3824c364e85b7e8ccf5462282fb07639c5fd8859
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-06-08 13:15:59 -0400

    handle summarized PASS selection with a dummy dialog, still needs to actually fetch the non-summarized PASS and use it to populate non-dummy dialog

--- src/main/java/org/broadinstitute/sequel/presentation/pass/PassDashboard.java
+++ src/main/java/org/broadinstitute/sequel/presentation/pass/PassDashboard.java
@@ -5,6 +5,8 @@ import org.broadinstitute.sequel.boundary.PassStatus;
 import org.broadinstitute.sequel.boundary.SummarizedPass;
 import org.broadinstitute.sequel.boundary.pass.PassSOAPService;
 import org.broadinstitute.sequel.presentation.AbstractJsfBean;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -26,8 +28,13 @@ public class PassDashboard extends AbstractJsfBean {
     @Inject
     private Log log;
 
+    private boolean rebuild = true;
 
-    private List<SummarizedPass> passes = null;
+
+    private SummarizedPass selectedPass = null;
+
+
+    private SummarizedPassDataModel passModel = new SummarizedPassDataModel();
 
 
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
@@ -43,25 +50,27 @@ public class PassDashboard extends AbstractJsfBean {
     private boolean onlyActive;
 
 
-    public List<SummarizedPass> getPassList() {
+    public SummarizedPassDataModel getPassModel() {
 
-        if (passes == null) {
+        if ( rebuild ) {
 
             log.info("Loading PASSes with onlyMine = " + onlyMine + ", onlyActive = " + onlyActive);
 
+            List<SummarizedPass> passList;
+
             if ( onlyMine )
                 // TODO mlc figure out how to get the logged in user
-                passes = service.searchPassesByCreator("mcovarr").getSummarizedPassList();
+                passList = service.searchPassesByCreator("mcovarr").getSummarizedPassList();
 
             else
-                passes = service.searchPasses().getSummarizedPassList();
+                passList = service.searchPasses().getSummarizedPassList();
 
 
             // cheating and doing ACTIVE filtering on the JEE "client" of the Squid WS since Squid does not offer a
             // WS that will calculate this for us
             if ( onlyActive ) {
 
-                final Iterator<SummarizedPass> iterator = passes.iterator();
+                final Iterator<SummarizedPass> iterator = passModel.iterator();
 
                 while (iterator.hasNext()) {
 
@@ -73,17 +82,18 @@ public class PassDashboard extends AbstractJsfBean {
                 }
             }
 
+            passModel.setWrappedData(passList);
+
+            rebuild = false;
+
         }
 
-        return passes;
+        return passModel;
 
     }
 
 
 
-    public void setSelectedPass(SummarizedPass pass) {
-
-    }
 
 
     public int sortByCalendar(Object cal1, Object cal2) {
@@ -128,21 +138,35 @@ public class PassDashboard extends AbstractJsfBean {
     }
 
 
-    private void issueSummarizedPassReload() {
-        passes = null;
-
-    }
-
-
     public void onOnlyMine(AjaxBehaviorEvent event) {
         onlyMine = !onlyMine;
-        issueSummarizedPassReload();
+        rebuild = true;
     }
 
 
     public void onOnlyActive(AjaxBehaviorEvent event) {
         onlyActive = !onlyActive;
-        issueSummarizedPassReload();
+        rebuild = true;
+    }
+
+
+    public void onRowSelect(SelectEvent event) {
+        this.selectedPass = (SummarizedPass) event.getObject();
+    }
+
+
+    public void onRowUnselect(UnselectEvent event) {
+        this.selectedPass = null;
+    }
+
+
+
+    public SummarizedPass getSelectedPass() {
+        return selectedPass;
+    }
+
+    public void setSelectedPass(Object selectedPass) {
+        this.selectedPass = (SummarizedPass) selectedPass;
     }
 
 }
