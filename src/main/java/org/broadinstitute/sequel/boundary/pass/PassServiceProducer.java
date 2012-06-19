e1c3f28f1128b6024e3c8758a235648cc01b386e
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-06-19 12:43:19 -0400

    stub implementation of PassService

--- src/main/java/org/broadinstitute/sequel/boundary/pass/PassServiceProducer.java
+++ src/main/java/org/broadinstitute/sequel/boundary/pass/PassServiceProducer.java
@@ -4,7 +4,6 @@ import org.apache.commons.logging.Log;
 import org.broadinstitute.sequel.control.pass.PassService;
 import org.broadinstitute.sequel.infrastructure.deployment.Deployment;
 import org.broadinstitute.sequel.infrastructure.deployment.Impl;
-import org.broadinstitute.sequel.infrastructure.squid.SquidConfiguration;
 
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
@@ -13,22 +12,23 @@ import javax.inject.Inject;
 
 public class PassServiceProducer {
 
+
+    @Inject
+    private Log log;
+
+
     @Inject
     private Deployment deployment;
 
-    @Inject
-    private SquidConfiguration squidConfiguration;
 
     @Inject
     @Impl
     // We need the {@link @Impl} qualifier or we get errors with multiple {@link @Default} implementations
     // to @Inject PassService injection points,
     // and putting {@link @Alternative} on {@link PassSOAPServiceImpl} makes it impossible to inject instances of that
-    // class here
+    // class here without a change to beans.xml
     private PassSOAPServiceImpl impl;
 
-    @Inject
-    private Log log;
 
 
     // The instance we'll return to our @Injection points
