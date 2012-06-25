2e5ee299c4757a4d7416884fe63e02755daaea38
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-06-25 13:38:38 -0400

    have DeploymentProducer not implement BasicProducer<Deployment> since that causes the artifact to fail deployment with no useful diagnostics

--- src/main/java/org/broadinstitute/sequel/infrastructure/deployment/DeploymentProducer.java
+++ src/main/java/org/broadinstitute/sequel/infrastructure/deployment/DeploymentProducer.java
@@ -22,8 +22,13 @@ import javax.naming.NamingException;
  * {@link Startup} and {@link Singleton}, so it will do this JNDI lookup on artifact deployment.  Failure to resolve
  * SEQUEL_DEPLOYMENT or have the value match one of DEV, TEST, QA, PROD, or STUBBY is a fatal error that will halt
  * deployment.
+ *
+ *
+ * Having DeploymentProducer implement the BasicProducer&lt;Deployment&gt; interface makes artifact deployment fail with
+ * no helpful diagnostics.  I have no idea why.
+ *
  */
-public class DeploymentProducer implements BasicProducer<Deployment> {
+public class DeploymentProducer /* implements BasicProducer<Deployment> */ {
 
 
     @Inject
@@ -79,7 +84,6 @@ public class DeploymentProducer implements BasicProducer<Deployment> {
     }
 
 
-    @Override
     @Produces
     public Deployment produce() {
         return deployment;
