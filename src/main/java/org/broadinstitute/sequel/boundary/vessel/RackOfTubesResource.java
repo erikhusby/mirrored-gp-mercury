82b4c583163ff721f06f9c458fc66669abdc07b2
Jon Thompson <thompson@broadinstitute.org>
2012-07-24 17:37:12 -0400

    Improved import from BSP

--- src/main/java/org/broadinstitute/sequel/boundary/vessel/RackOfTubesResource.java
+++ src/main/java/org/broadinstitute/sequel/boundary/vessel/RackOfTubesResource.java
@@ -1,5 +1,6 @@
 package org.broadinstitute.sequel.boundary.vessel;
 
+import org.broadinstitute.sequel.control.dao.vessel.BSPSampleAuthorityTwoDTubeDao;
 import org.broadinstitute.sequel.control.dao.vessel.RackOfTubesDao;
 import org.broadinstitute.sequel.control.dao.vessel.TwoDBarcodedTubeDAO;
 import org.broadinstitute.sequel.entity.bsp.BSPStartingSample;
@@ -11,6 +12,7 @@ import org.broadinstitute.sequel.entity.vessel.BSPSampleAuthorityTwoDTube;
 import org.broadinstitute.sequel.entity.vessel.RackOfTubes;
 import org.broadinstitute.sequel.entity.vessel.TwoDBarcodedTube;
 import org.broadinstitute.sequel.entity.vessel.VesselPosition;
+import org.broadinstitute.sequel.entity.workflow.LabBatch;
 import org.broadinstitute.sequel.infrastructure.jira.JiraServiceStub;
 import org.broadinstitute.sequel.infrastructure.jira.issue.CreateIssueRequest;
 
@@ -35,36 +37,57 @@ public class RackOfTubesResource {
     @Inject
     private TwoDBarcodedTubeDAO twoDBarcodedTubeDAO;
 
+    @Inject
+    private BSPSampleAuthorityTwoDTubeDao bspSampleAuthorityTwoDTubeDao;
+
     @POST
     public String createRack(RackBean rackBean) {
         String label = rackBean.barcode + "_" + Long.toString(System.currentTimeMillis());
         RackOfTubes rackOfTubes = new RackOfTubes(label);
         BasicProjectPlan projectPlan = null;
+        JiraTicket jiraTicket = null;
         if (rackBean.lcSet != null) {
             // todo jmt fix workflow
+            jiraTicket = new JiraTicket(new JiraServiceStub(), rackBean.lcSet, rackBean.lcSet);
             projectPlan = new BasicProjectPlan(new BasicProject(rackBean.lcSet,
-                    new JiraTicket(new JiraServiceStub(), rackBean.lcSet, rackBean.lcSet)), rackBean.lcSet,
+                    jiraTicket), rackBean.lcSet,
                     new WorkflowDescription("", null, CreateIssueRequest.Fields.Issuetype.Whole_Exome_HybSel));
         }
 
-        List<String> barcodes = new ArrayList<String>();
+        List<String> tubeBarcodes = new ArrayList<String>();
+        List<String> sampleBarcodes = new ArrayList<String>();
         for (TubeBean tubeBean : rackBean.tubeBeans) {
-            barcodes.add(tubeBean.barcode);
+            tubeBarcodes.add(tubeBean.barcode);
+            sampleBarcodes.add(tubeBean.sampleBarcode);
         }
-        Map<String,TwoDBarcodedTube> mapBarcodeToTube = twoDBarcodedTubeDAO.findByBarcodes(barcodes);
+        Map<String, TwoDBarcodedTube> mapBarcodeToTube = twoDBarcodedTubeDAO.findByBarcodes(tubeBarcodes);
+        Map<String, BSPSampleAuthorityTwoDTube> mapBarcodeToSample = bspSampleAuthorityTwoDTubeDao.findByLabels(sampleBarcodes);
+
         for (TubeBean tubeBean : rackBean.tubeBeans) {
             TwoDBarcodedTube twoDBarcodedTube = mapBarcodeToTube.get(tubeBean.barcode);
             if (twoDBarcodedTube == null) {
+                BSPSampleAuthorityTwoDTube bspSampleAuthorityTwoDTube = mapBarcodeToSample.get(tubeBean.sampleBarcode);
+                if(bspSampleAuthorityTwoDTube == null) {
+                    BSPStartingSample bspStartingSample = new BSPStartingSample(tubeBean.sampleBarcode + ".aliquot", projectPlan);
+                    bspSampleAuthorityTwoDTube = new BSPSampleAuthorityTwoDTube(bspStartingSample);
+                    mapBarcodeToSample.put(tubeBean.sampleBarcode, bspSampleAuthorityTwoDTube);
+                }
                 if(tubeBean.sampleBarcode == null) {
                     twoDBarcodedTube = new TwoDBarcodedTube(tubeBean.barcode);
                 } else {
-                    BSPSampleAuthorityTwoDTube bspAliquot = new BSPSampleAuthorityTwoDTube(new BSPStartingSample(tubeBean.sampleBarcode + ".aliquot", projectPlan, null));
-
-                    twoDBarcodedTube = bspAliquot;
+                    if (projectPlan != null) {
+                        projectPlan.addStarter(bspSampleAuthorityTwoDTube);
+                        // todo jmt not sure if this is necessary, but TransferTraverserTest expects it
+                        projectPlan.addAliquotForStarter(bspSampleAuthorityTwoDTube, bspSampleAuthorityTwoDTube);
+                    }
+                    twoDBarcodedTube = bspSampleAuthorityTwoDTube;
                 }
             }
             rackOfTubes.getVesselContainer().addContainedVessel(twoDBarcodedTube,
                     VesselPosition.getByName(tubeBean.position));
+        }
+        if (jiraTicket != null) {
+            jiraTicket.setLabBatch(new LabBatch(projectPlan, rackBean.lcSet, projectPlan.getStarters()));
         }
         rackOfTubes.makeDigest();
         List<RackOfTubes> byDigest = rackOfTubesDao.findByDigest(rackOfTubes.getDigest());
---
+++ src/main/java/org/broadinstitute/sequel/control/dao/vessel/BSPSampleAuthorityTwoDTubeDao.java
@@ -0,0 +1,44 @@
+package org.broadinstitute.sequel.control.dao.vessel;
+
+import org.broadinstitute.sequel.control.dao.GenericDao;
+import org.broadinstitute.sequel.entity.vessel.BSPSampleAuthorityTwoDTube;
+import org.broadinstitute.sequel.entity.vessel.LabVessel_;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Data Access Object for BSPSampleAuthorityTwoDTube
+ */
+public class BSPSampleAuthorityTwoDTubeDao extends GenericDao {
+
+    /**
+     * Fetches tubes for a given list of labels.
+     * @param labels list of labels
+     * @return map from label to tube (may be null), iterable in same order as input list
+     */
+    public Map<String, BSPSampleAuthorityTwoDTube> findByLabels(List<String> labels) {
+        Map<String, BSPSampleAuthorityTwoDTube> mapLabelToTube = new LinkedHashMap<String, BSPSampleAuthorityTwoDTube>();
+        for (String label : labels) {
+            mapLabelToTube.put(label, null);
+        }
+
+        EntityManager entityManager = this.getThreadEntityManager().getEntityManager();
+        CriteriaQuery<BSPSampleAuthorityTwoDTube> criteriaQuery =
+                entityManager.getCriteriaBuilder().createQuery(BSPSampleAuthorityTwoDTube.class);
+        Root<BSPSampleAuthorityTwoDTube> root = criteriaQuery.from(BSPSampleAuthorityTwoDTube.class);
+        criteriaQuery.where(root.get(LabVessel_.label).in(labels));
+        List<BSPSampleAuthorityTwoDTube> bspSampleAuthorityTwoDTubes =
+                entityManager.createQuery(criteriaQuery).getResultList();
+
+        for (BSPSampleAuthorityTwoDTube bspSampleAuthorityTwoDTube : bspSampleAuthorityTwoDTubes) {
+            mapLabelToTube.put(bspSampleAuthorityTwoDTube.getLabel(), bspSampleAuthorityTwoDTube);
+        }
+
+        return mapLabelToTube;
+    }
+}
