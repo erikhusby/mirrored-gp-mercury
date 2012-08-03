53a6f819fa139b1b014ad4b22104a68b2274b8ff
Jon Thompson <thompson@broadinstitute.org>
2012-08-03 14:37:40 -0400

    Consolidate code to import data from Squid

--- src/main/java/org/broadinstitute/sequel/control/dao/GenericDao.java
+++ src/main/java/org/broadinstitute/sequel/control/dao/GenericDao.java
@@ -3,6 +3,8 @@ package org.broadinstitute.sequel.control.dao;
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
 
 /**
  * Superclass for Data Access Objects.  Managed beans can't be parameterized types, so this DAO can't be typesafe.
@@ -27,5 +29,12 @@ public class GenericDao {
 
     public void persist(Object entity) {
         this.threadEntityManager.getEntityManager().persist(entity);
+    }
+
+    public void persistAll(List<?> entities) {
+        EntityManager entityManager = this.threadEntityManager.getEntityManager();
+        for (Object entity : entities) {
+            entityManager.persist(entity);
+        }
     }
 }
