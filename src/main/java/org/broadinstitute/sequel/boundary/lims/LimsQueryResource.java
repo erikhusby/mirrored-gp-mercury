a273f04b57b9f18ff790e288d1d6a7ca34b341ee
Brian Reilly <breilly@broadinstitute.org>
2012-08-07 13:06:49 -0400

    added empty stubs for the remainder of the 1st round of thrift service wrappers

--- src/main/java/org/broadinstitute/sequel/boundary/lims/LimsQueryResource.java
+++ src/main/java/org/broadinstitute/sequel/boundary/lims/LimsQueryResource.java
@@ -8,6 +8,7 @@ import org.broadinstitute.sequel.control.lims.LimsQueryResourceResponseFactory;
 import org.broadinstitute.sequel.infrastructure.thrift.ThriftService;
 import org.broadinstitute.sequel.limsquery.generated.FlowcellDesignationType;
 import org.broadinstitute.sequel.limsquery.generated.LibraryDataType;
+import org.broadinstitute.sequel.limsquery.generated.PoolGroupType;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -52,6 +53,10 @@ public class LimsQueryResource {
         return result;
     }
 
+    // TODO round 2: map<string,ConcentrationAndVolume> fetchConcentrationAndVolumeForTubeBarcodes(1:list<string> tubeBarcodes)
+
+    // TODO round 2: bool areLibrariesAllTheSameType(1:list<string> tubeBarcodes) throws (1:NotFoundException details)
+
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
@@ -59,6 +64,8 @@ public class LimsQueryResource {
     public boolean doesLimsRecognizeAllTubes(List<String> barcodes) throws TException, TZIMSException {
         return thriftService.doesSquidRecognizeAllLibraries(barcodes);
     }
+
+    // TODO round 2: list<string> fetchMaterialTypesForTubeBarcodes(1:list<string> tubeBarcodes)
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
@@ -82,6 +89,39 @@ public class LimsQueryResource {
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
+    @Path("/findFlowcellDesignationByTaskName")
+    public FlowcellDesignationType findFlowcellDesignationByReagentBlockBarcode(@QueryParam("reagentBlockBarcode") String reagentBlockBarcode) {
+        // TODO: thrift proxy implementation
+        return null;
+    }
+
+    // TODO round 2: bool checkReceptaclesInTask(1:list<string> tubeBarcodes, 2:string taskName) throws(1:NotFoundException details)
+
+    // TODO round 2: list<string> findImmediatePlateParents(1:string plateBarcode)
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/fetchLibraryDetailsByLibraryName")
+    public List<LibraryDataType> fetchLibraryDetailsByLibraryName(List<String> libraryNames) {
+        // TODO: thrift proxy implementation
+        return null;
+    }
+
+    // TODO round 2: list<string> fetchUnfulfilledDesignations()
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/findRelatedDesignationsForAnyTube")
+    public List<String> findRelatedDesignationsForAnyTube(List<String> tubeBarcodes) {
+        // TODO: thrift proxy implementation
+        return null;
+
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
     @Path("/fetchUserIdForBadgeId")
     public String fetchUserIdForBadgeId(@QueryParam("badgeId") String badgeId) throws TException, TZIMSException {
         return thriftService.fetchUserIdForBadgeId(badgeId);
@@ -94,6 +134,9 @@ public class LimsQueryResource {
         return thriftService.fetchParentRackContentsForPlate(plateBarcode);
     }
 
+    // TODO round 2: TZamboniRun fetchSingleLane(1:string runName, 2:i16 laneNumber) throws (1:TZIMSException details)
+
+    // TODO round 2: TZamboniRun fetchRun(1:string runName) throws (1:TZIMSException details)
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
@@ -107,5 +150,22 @@ public class LimsQueryResource {
     @Path("/fetchQuantForTube")
     public Double fetchQuantForTube(@QueryParam("tubeBarcode") String tubeBarcode, @QueryParam("quantType") String quantType) throws TException, TZIMSException {
         return thriftService.fetchQuantForTube(tubeBarcode, quantType);
+    }
+
+    // TODO round 2: list<WellAndSourceTube> fetchSourceTubesForPlate(1:string plateBarcode)
+
+    // TODO ???: PlateInfo fetchPlateInfo(1:string plateBarcode)
+
+    // TODO round 2: list<PlateTransfer> fetchTransfersForPlate(1:string plateBarcode, 2:i16 depth)
+
+    // TODO round 2: list<PlateTransfer> fetchTransfersForRack(1:string rackBarcode, 2:list<WellAndSourceTube> positionMap, 3:i16 depth)
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/fetchPoolGroups")
+    public List<PoolGroupType> fetchPoolGroups(List<String> tubeBarcodes) {
+        // TODO: thrift proxy implementation
+        return null;
     }
 }
