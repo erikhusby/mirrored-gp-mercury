9edc4edb21a6946a35d2fa87210b8e82e371f042
Hugh McCrory <mccrory@broadinstitute.org>
2012-09-19 14:32:20 -0400

    moved products and orders under entity

--- athena/src/main/java/org/broadinstitute/gpinformatics/athena/entity/products/MeanTargetCoverageModel.java
+++ athena/src/main/java/org/broadinstitute/gpinformatics/athena/entity/products/MeanTargetCoverageModel.java
@@ -1,57 +1,61 @@
-package org.broadinstitute.gpinformatics.athena.products;
+package org.broadinstitute.gpinformatics.athena.entity.products;
 
 import org.broadinstitute.gpinformatics.athena.Namespaces;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import java.io.Serializable;
 import java.math.BigInteger;
 
 
 /**
- * <p>Java class for PFReadsCoverageModel complex type.
+ * <p>Java class for MeanTargetCoverageModel complex type.
  * <p/>
  * <p>The following schema fragment specifies the expected content contained within this class.
  * <p/>
  * <pre>
- * &lt;complexType name="PFReadsCoverageModel">
+ * &lt;complexType name="MeanTargetCoverageModel">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;attribute name="readsDesired" use="required" type="{http://www.w3.org/2001/XMLSchema}integer" />
+ *       &lt;sequence>
+ *         &lt;element name="coverageDesired" type="{http://www.w3.org/2001/XMLSchema}integer"/>
+ *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
  * </pre>
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(namespace = Namespaces.PRODUCT_NS)
-public class PFReadsCoverageModel
+@XmlType(namespace = Namespaces.PRODUCT_NS, propOrder = {
+        "coverageDesired"
+})
+public class MeanTargetCoverageModel
         implements Serializable {
 
     private final static long serialVersionUID = 12343L;
-    @XmlAttribute(required = true)
-    protected BigInteger readsDesired;
+    @XmlElement(required = true)
+    protected BigInteger coverageDesired;
 
     /**
-     * Gets the value of the readsDesired property.
+     * Gets the value of the coverageDesired property.
      *
      * @return possible object is
      *         {@link java.math.BigInteger }
      */
-    public BigInteger getReadsDesired() {
-        return readsDesired;
+    public BigInteger getCoverageDesired() {
+        return coverageDesired;
     }
 
     /**
-     * Sets the value of the readsDesired property.
+     * Sets the value of the coverageDesired property.
      *
      * @param value allowed object is
      *              {@link java.math.BigInteger }
      */
-    public void setReadsDesired(BigInteger value) {
-        this.readsDesired = value;
+    public void setCoverageDesired(BigInteger value) {
+        this.coverageDesired = value;
     }
 
 }
