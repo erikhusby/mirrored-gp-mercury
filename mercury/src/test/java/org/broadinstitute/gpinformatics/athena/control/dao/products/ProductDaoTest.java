3d90ffc1876dad89c08b7cf56257bd4d236f89f3
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-10-05 14:59:00 -0400

    GPLIM-68 PriceItem and Product Daos and tests

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/control/dao/products/ProductDaoTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/control/dao/products/ProductDaoTest.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.gpinformatics.athena.control.dao.products;
 
 
+import org.broadinstitute.gpinformatics.athena.entity.products.Product;
 import org.broadinstitute.gpinformatics.infrastructure.test.ContainerTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,6 +9,7 @@ import org.testng.annotations.Test;
 import javax.ejb.EJBException;
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
+import java.util.List;
 
 @Test(enabled = false)
 public class ProductDaoTest extends ContainerTest {
@@ -18,16 +20,33 @@ public class ProductDaoTest extends ContainerTest {
 
     public void testFindTopLevelProducts() {
 
-        // just a smoke test for now
-        dao.findTopLevelProducts();
+        final List<Product> topLevelProducts = dao.findTopLevelProducts();
+        Assert.assertNotNull(topLevelProducts);
+
+        // make sure exex is in there
+        for (Product product : topLevelProducts) {
+            if ("EXOME_EXPRESS-2012.11.01".equals(product.getPartNumber()))
+                return;
+        }
+
+        Assert.fail("Did not find Exome Express top-level product!");
+
     }
+
 
 
     public void testFindByPartNumber() {
 
-        // just a smoke test for now and as such needs to be prepared for NoResultExceptions (nested in EJBExceptions)
+        final Product product = dao.findByPartNumber("EXOME_EXPRESS-2012.11.01");
+        Assert.assertNotNull(product);
+
+        Assert.assertEquals(product.getPartNumber(), "EXOME_EXPRESS-2012.11.01");
+        Assert.assertTrue(product.isTopLevelProduct());
+        Assert.assertEquals(product.getWorkflowName(), "EXEX-WF-2012.11.01");
+
+        // negative test
         try {
-            dao.findProductByPartNumber("GP-GENERAL_PRODUCTS-DNA_EXTRACTION_2012.11.01");
+            dao.findByPartNumber("NONEXISTENT PART!!!");
         }
         catch (EJBException ejbx) {
             // Since Daos are @Stateful, a NoResultException that is generated within the Dao method will get
