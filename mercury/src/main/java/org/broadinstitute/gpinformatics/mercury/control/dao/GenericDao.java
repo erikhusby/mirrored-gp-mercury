b5edea132ff0b8a470a3ff6b56da8f4dae397590
Jon Thompson <thompson@broadinstitute.org>
2012-10-05 21:46:12 -0400

    GPLIM-55 continued work on reducing boilerplate in DAOs

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/dao/GenericDao.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/dao/GenericDao.java
@@ -4,6 +4,8 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.persistence.metamodel.SingularAttribute;
@@ -49,11 +51,27 @@ public class GenericDao {
         return threadEntityManager.getEntityManager();
     }
 
-    public <S, T> T findSingle(Class<T> entity, SingularAttribute<T, S> singularAttribute, S value) {
-        CriteriaQuery<T> criteriaQuery =
-                getEntityManager().getCriteriaBuilder().createQuery(entity);
-        Root<T> root = criteriaQuery.from(entity);
-        criteriaQuery.where(getEntityManager().getCriteriaBuilder().equal(root.get(singularAttribute), value));
-        return getEntityManager().createQuery(criteriaQuery).getSingleResult();
+    /**
+     * Returns a single entity that matches a specified value for a specified property.
+     * @param entity the entity to return
+     * @param singularAttribute the metadata field for the property to query
+     * @param value the value to query
+     * @param <VALUE_TYPE> the type of the value in the query, e.g. String
+     * @param <METADATA_TYPE> the type on which the property is defined, this can be different from the ENTITY_TYPE if
+     *                       there is inheritance
+     * @param <ENTITY_TYPE> the type of the entity to return
+     * @return entity that matches the value, or null if not found
+     */
+    public <VALUE_TYPE, METADATA_TYPE, ENTITY_TYPE extends METADATA_TYPE> ENTITY_TYPE findSingle(
+            Class<ENTITY_TYPE> entity, SingularAttribute<METADATA_TYPE, VALUE_TYPE> singularAttribute, VALUE_TYPE value) {
+        CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<ENTITY_TYPE> criteriaQuery = criteriaBuilder.createQuery(entity);
+        Root<ENTITY_TYPE> root = criteriaQuery.from(entity);
+        criteriaQuery.where(criteriaBuilder.equal(root.get(singularAttribute), value));
+        try {
+            return getEntityManager().createQuery(criteriaQuery).getSingleResult();
+        } catch (NoResultException ignored) {
+            return null;
+        }
     }
 }
--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/dao/vessel/IlluminaFlowcellDao.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/dao/vessel/IlluminaFlowcellDao.java
@@ -6,10 +6,6 @@ import org.broadinstitute.gpinformatics.mercury.entity.run.IlluminaFlowcell_;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 /**
  * Data Access Object for flowcells
@@ -18,16 +14,6 @@ import javax.persistence.criteria.Root;
 @RequestScoped
 public class IlluminaFlowcellDao extends GenericDao {
     public IlluminaFlowcell findByBarcode(String barcode) {
-        IlluminaFlowcell illuminaFlowcell = null;
-        EntityManager entityManager = getThreadEntityManager().getEntityManager();
-        CriteriaQuery<IlluminaFlowcell> criteriaQuery =
-                entityManager.getCriteriaBuilder().createQuery(IlluminaFlowcell.class);
-        Root<IlluminaFlowcell> root = criteriaQuery.from(IlluminaFlowcell.class);
-        criteriaQuery.where(entityManager.getCriteriaBuilder().equal(root.get(IlluminaFlowcell_.label), barcode));
-        try {
-            illuminaFlowcell = entityManager.createQuery(criteriaQuery).getSingleResult();
-        } catch (NoResultException ignored) {
-        }
-        return illuminaFlowcell;
+        return findSingle(IlluminaFlowcell.class, IlluminaFlowcell_.label, barcode);
     }
 }
