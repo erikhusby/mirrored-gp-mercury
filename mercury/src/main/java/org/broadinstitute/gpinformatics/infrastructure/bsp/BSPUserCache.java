dbd81003adc2633a2c92d63da305c5f60ac5f467
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-10-07 08:58:50 -0400

    GPLIM-67 work around some CDI problems, more research needed

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPUserCache.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPUserCache.java
@@ -2,11 +2,12 @@ package org.broadinstitute.gpinformatics.infrastructure.bsp;
 
 import com.google.common.collect.ImmutableList;
 import org.broadinstitute.bsp.client.users.BspUser;
+import org.broadinstitute.bsp.client.users.UserManager;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.plating.BSPManagerFactory;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.inject.Singleton;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -15,12 +16,13 @@ import java.util.List;
  * Application wide cache of BSP's user list.
  */
 @Named
-@ApplicationScoped
+// MLC @ApplicationScoped breaks the test, as does @javax.ejb.Singleton.  @javax.inject.Singleton is the CDI version
+// and does appear to work.  Much to learn about CDI still...
+@Singleton
 public class BSPUserCache {
+
     private final List<BspUser> users;
 
-    @Inject
-    private BSPManagerFactory bspManagerFactory;
 
     /**
      * @return list of bsp users, sorted by username.
@@ -44,14 +46,36 @@ public class BSPUserCache {
         return null;
     }
 
-    public BSPUserCache() {
+    @Inject
+    // MLC constructor injection appears to be required to get a BSPManagerFactory injected???
+    public BSPUserCache(BSPManagerFactory bspManagerFactory) {
+
         // FIXME: Update to use correct BSP API call once it's present.
-        List<BspUser> rawUsers = bspManagerFactory.createUserManager().getPrimaryInvestigators();
+        UserManager userManager = bspManagerFactory.createUserManager();
+        List<BspUser> rawUsers = userManager.getPrimaryInvestigators();
         Collections.sort(rawUsers, new Comparator<BspUser>() {
             @Override
             public int compare(BspUser o1, BspUser o2) {
                 // FIXME: need to figure out what the correct sort criteria are.
-                return o1.getUsername().compareTo(o2.getUsername());
+
+                String u1 = o1.getUsername();
+                String u2 = o2.getUsername();
+
+                // MLC these null checks proved to be necessary, though that definitely doesn't seem right.
+                // Need to talk to Jason
+                if (u1 == null && u2 == null) {
+                    return 0;
+                }
+
+                if (u1 == null) {
+                    return -1;
+                }
+
+                if (u2 == null) {
+                    return 1;
+                }
+
+                return u1.compareTo(u2);
             }
         });
         users = ImmutableList.copyOf(rawUsers);
