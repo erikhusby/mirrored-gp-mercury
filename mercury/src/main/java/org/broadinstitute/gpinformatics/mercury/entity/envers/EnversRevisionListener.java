640d842377b7ecd2744a004ab55d292af56a64a0
Jon Thompson <thompson@broadinstitute.org>
2012-10-07 23:09:38 -0400

    GPLIM-55 RevInfo entity, to allow specifying schema

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/envers/EnversRevisionListener.java
@@ -0,0 +1,13 @@
+package org.broadinstitute.gpinformatics.mercury.entity.envers;
+
+import org.hibernate.envers.RevisionListener;
+
+/**
+ * Called by Envers when it needs to create a new RevInfo entity
+ */
+public class EnversRevisionListener implements RevisionListener {
+    @Override
+    public void newRevision(Object revisionEntity) {
+        // todo jmt set username
+    }
+}
