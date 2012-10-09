365c675677bd4fea1eccb8c16f42a1e9f54810df
Andrew Zimmer <andrew@broadinstitute.org>
2012-10-09 10:31:06 -0400

    GPLIM-64.  added a comment

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/ProductOrderId.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/ProductOrderId.java
@@ -7,6 +7,9 @@ public class ProductOrderId {
 
     private String name;
 
+
+    // not sure if this should be name, id, "barcode", jira ticket
+    // it's whatever the business key is in athena for a product order
     public ProductOrderId(String name) {
         this.name = name;
     }
