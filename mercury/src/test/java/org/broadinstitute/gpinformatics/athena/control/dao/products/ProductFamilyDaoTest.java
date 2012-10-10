912ff518a80578643255064e97e8fe537fa49676
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-10-10 14:12:03 -0400

    GPLIM-70 barest list UI, enable "test" to load test data

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/control/dao/products/ProductFamilyDaoTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/control/dao/products/ProductFamilyDaoTest.java
@@ -3,9 +3,11 @@ package org.broadinstitute.gpinformatics.athena.control.dao.products;
 
 import org.broadinstitute.gpinformatics.athena.entity.products.ProductFamily;
 import org.broadinstitute.gpinformatics.infrastructure.test.ContainerTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
+import java.util.List;
 
 @Test(enabled = false)
 public class ProductFamilyDaoTest extends ContainerTest {
@@ -14,11 +16,21 @@ public class ProductFamilyDaoTest extends ContainerTest {
     private ProductFamilyDao dao;
 
 
-    public void test() {
+    public void testSingle() {
 
         // not sure if fixture data has been loaded into our test database yet, so this is just a smoke test
-        dao.find(ProductFamily.ProductFamilyName.GENERAL_PRODUCTS);
+        ProductFamily productFamily = dao.find(ProductFamily.ProductFamilyName.GENERAL_PRODUCTS);
+        Assert.assertNotNull(productFamily);
 
+    }
+
+
+    public void testAll() {
+
+        List<ProductFamily> productFamilies = dao.findAll();
+
+        Assert.assertNotNull(productFamilies);
+        Assert.assertTrue(productFamilies.size() > 0);
     }
 
 }
