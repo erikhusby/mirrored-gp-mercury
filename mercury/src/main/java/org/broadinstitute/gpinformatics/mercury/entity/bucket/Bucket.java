13b7ee19aee02bf2ba4416b40dc3338d1455e133
Scott Matthews <scottmat@broadinstitute.org>
2012-10-16 16:20:55 -0400

    Updated logic for Transition to be a little better
    added Javadocs.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/bucket/Bucket.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/bucket/Bucket.java
@@ -1,22 +1,54 @@
 package org.broadinstitute.gpinformatics.mercury.entity.bucket;
 
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Represents a bucket in the workflow diagram
  */
+@Entity
+@Audited
+@Table (schema = "mercury", uniqueConstraints = @UniqueConstraint (columnNames = {"bucketDefinition"}))
 public class Bucket {
 
     // todo wire up to workflow definition
 
+    @SequenceGenerator (name = "SEQ_BUCKET", schema = "mercury",  sequenceName = "SEQ_BUCKET")
+    @GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "SEQ_BUCKET")
+    @Id
     private Long bucketId;
 
+    @OneToMany (mappedBy = "bucketExistence")
     private Set<BucketEntry> bucketEntries = new HashSet<BucketEntry>();
 
     private String bucketDefinition;
 
+    public Bucket (  @NotNull String bucketDefinitionIn ) {
+        bucketDefinition = bucketDefinitionIn;
+    }
 
+    public String getBucketDefinition ( ) {
+        return bucketDefinition;
+    }
+
+    public Set<BucketEntry> getBucketEntries ( ) {
+        return bucketEntries;
+    }
+
+    public void setBucketEntries ( @NotNull Set<BucketEntry> bucketEntriesIn ) {
+        bucketEntries = bucketEntriesIn;
+    }
 
     /**
      * Does this bucket contain the given
--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/bucket/BucketEntry.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/bucket/BucketEntry.java
@@ -2,6 +2,18 @@ package org.broadinstitute.gpinformatics.mercury.entity.bucket;
 
 import org.broadinstitute.gpinformatics.mercury.entity.ProductOrderId;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 /**
  * An entry into a lab {@link Bucket}.  An entry is
@@ -9,17 +21,35 @@ import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
  * on and the {@link ProductOrderId product order} for which
  * the work is related.
  */
+@Entity
+@Audited
+@Table (schema = "mercury")
 public class BucketEntry {
 
+    @SequenceGenerator (name = "SEQ_BUCKET_ENTRY", schema = "mercury",  sequenceName = "SEQ_BUCKET_ENTRY")
+    @GeneratedValue (strategy = GenerationType.SEQUENCE, generator = "SEQ_BUCKET_ENTRY")
+    @Id
     private Long bucketEntryId;
 
+    @ManyToOne (cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     private LabVessel labVessel;
 
     private ProductOrderId productOrder;
 
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     private Bucket bucketExistence;
 
-    private Integer productOrderBucketRanking;
+    private Integer productOrderRanking;
+
+    public BucketEntry ( LabVessel labVesselIn, ProductOrderId productOrderIn ) {
+        this(labVesselIn, productOrderIn, null);
+    }
+
+    public BucketEntry ( LabVessel labVesselIn, ProductOrderId productOrderIn, Integer productOrderRankingIn ) {
+        labVessel = labVesselIn;
+        productOrder = productOrderIn;
+        productOrderRanking = productOrderRankingIn;
+    }
 
     public LabVessel getLabVessel() {
         throw new RuntimeException("not implemented yet");
