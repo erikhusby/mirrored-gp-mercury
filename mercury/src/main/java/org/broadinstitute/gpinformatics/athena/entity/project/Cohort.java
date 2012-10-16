4960ebd439d994bdf9a9c769cb54525442acdce0
hrafal <hrafal@broadinstitute.org>
2012-10-16 14:31:49 -0400

    start at bsp cohort stuff

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/project/Cohort.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/project/Cohort.java
@@ -1,26 +1,90 @@
 package org.broadinstitute.gpinformatics.athena.entity.project;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
 
-/**
- * Created by IntelliJ IDEA.
- * User: mccrory
- * Date: 4/2/12
- * Time: 5:25 PM
- */
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.util.Comparator;
+
 public class Cohort {
 
-
-    public final CohortID id;
+    public final String cohortId;
     public final String name;
+    public final String category;
+    public final String group;
+    public final boolean archived;
 
-    public Cohort(CohortID id, String name) {
-        this.id = id;
+    public Cohort(String cohortId, String name, String category, String group, boolean archived) {
+        this.cohortId = cohortId;
         this.name = name;
-    }
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        this.category = category;
+        this.group = group;
+        this.archived = archived;
     }
 
+    public String getCohortId() {
+        return cohortId;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if ( (this == other ) ) {
+            return true;
+        }
+
+        if ( !(other instanceof Cohort) ) {
+            return false;
+        }
+
+        Cohort castOther = (Cohort) other;
+        return new EqualsBuilder().append(getCohortId(), castOther.getCohortId()).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(getCohortId()).toHashCode();
+    }
+
+    public static class CohortComparator implements Comparator<Cohort> {
+        @Override
+        public int compare(Cohort cohort, Cohort cohort1) {
+            Integer nullCohort = nullCompare(cohort, cohort1);
+            if (nullCohort != null) {
+                return nullCohort;
+            }
+
+            // Neither cohort is null, check if the cohortId is null
+            nullCohort = nullCompare(cohort.getCohortId(), cohort1.getCohortId());
+            if (nullCohort != null) {
+                return nullCohort;
+            }
+
+            // Neither of the cohortIds are null, so compare them
+            return cohort.getCohortId().compareTo(cohort1.getCohortId());
+        }
+
+        private Integer nullCompare(Object obj1, Object obj2) {
+            if ( (obj1 == null) && (obj2 == null) ) { return 0; }
+            if (obj2 == null) { return 1; }
+            if (obj1 == null) { return -1; }
+
+            return null;
+        }
+    }
 }
