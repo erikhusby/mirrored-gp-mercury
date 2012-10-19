104f021548f6eb17558cce17a89a4c9e369d951e
Erik Husby <mhusby@broadinstitute.org>
2012-10-19 16:43:39 -0400

    REL-248 Production Release Process

--- mercury/createRelease.sh
+++ mercury/createRelease.sh
@@ -20,12 +20,15 @@ if [ $? -eq 0 ] ; then
     git status
     git branch -a
     git tag -l
+    pushd target/checkout
+    git tag -a -m "Current Production" --force PROD HEAD
+    popd
     git checkout master
     git merge QA_PROD -m "REL-000 Update pom.xml with new version"
     git branch -d QA_PROD
     git push origin :QA_PROD
     git fetch origin +master
-    #git push origin master
+    git push origin master
 else
     echo "Release failed"
     exit 1
