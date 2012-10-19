d6f4150e4151db6556a36a9e09c49b294d5b4dc5
Erik Husby <mhusby@broadinstitute.org>
2012-10-19 15:36:44 -0400

    REL-428 Production Release process

--- mercury/createRelease.sh
+++ mercury/createRelease.sh
@@ -4,13 +4,9 @@
 #
 # Usage: Release.sh
 #
-HOST="vseqlims.broadinstitute.org"
-APP_NAME="Mercury"
-WATCHERS="mhusby@broadinstitute.org"
-#andrew@broadinstitute.org "
 
-if [ -d "target" ] ; then
-    rm -rf target
+if [ -d "release" ] ; then
+    rm -rf release
 fi
 mkdir target
 
@@ -19,21 +15,7 @@ cd target
 git checkout QA
 git checkout -b QA_PROD
 cd mercury
-mvn -P\!DefaultProfile --batch-mode -DdryRun=run release:prepare release:perform
-cd target/checkout
-mvn -PPROD -DskipTests package
-exit 0
-
-
-
-if [ -e $WAR_FILE ] ; then
-    MAILTXT=/tmp/mecury.txt
-    cat >$MAILTXT <<EOF
-
-    Mercury $RELEASEID has been deployed to $HOST/$APP_NAME
-
-EOF
-    mail -s "$APP_NAMEL deployed to $HOST" $WATCHERS < $MAILTXT
-else
-    echo "Unable to deploy non-existant $WAR_FILE"
-fi
+mvn -P\!DefaultProfile --batch-mode release:prepare release:perform
+git status
+git branch -a
+git tag -l
