9362f2ff5da42a0a72de535a10b003ad49d1c0f4
Hugh McCrory <mccrory@broadinstitute.org>
2012-10-29 08:11:59 -0400

    GPLIM-138 Landing page for pm and pdm

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/login/UserLogin.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/login/UserLogin.java
@@ -44,6 +44,12 @@ public class UserLogin extends AbstractJsfBean {
     @Inject
     private FacesContext facesContext;
 
+    public static final String PDM_WELCOME_PAGE = "/orders/list";
+    public static final String PM_WELCOME_PAGE = "/projects/list";
+    public static final String INDEX_PAGE = "/index.xhtml";
+    public static final String HOME_PAGE = "/Mercury";
+    public static final String INDEX_REDIRECT = "/index";
+
     public String getUsername() {
         return username;
     }
@@ -61,18 +67,19 @@ public class UserLogin extends AbstractJsfBean {
     }
 
     public String authenticateUser() {
-        String targetPage = "/index";
+        String targetPage;
         FacesContext context = FacesContext.getCurrentInstance();
 
         try {
             HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
 
             request.login(username, password);
+            targetPage = checkTargetForRoleRedirect(request);
             BspUser bspUser = bspUserList.getByUsername(username);
             if (bspUser != null) {
                 userBean.setBspUser(bspUser);
             } else {
-                // FIXME: Temporarily map unknown users to the BSP tester user. Will need to handle this in userBean.
+// FIXME: Temporarily map unknown users to the BSP tester user. Will need to handle this in userBean.
                 userBean.setBspUser(bspUserList.getByUsername("tester"));
                 // reuse exception handling below
                 //throw new ServletException("Login error: couldn't find BspUser: " + username);
@@ -80,8 +87,10 @@ public class UserLogin extends AbstractJsfBean {
             addInfoMessage("Welcome back!", "Sign in successful");
 
             String previouslyTargetedPage = (String)request.getSession().getAttribute(AuthorizationFilter.TARGET_PAGE_ATTRIBUTE);
-
             if (previouslyTargetedPage != null) {
+                // Check for redirect to PM and PDMs landing page
+                previouslyTargetedPage = checkTargetForRoleRedirect(previouslyTargetedPage, request);
+
                 request.getSession().setAttribute(AuthorizationFilter.TARGET_PAGE_ATTRIBUTE, null);
                 try {
                     facesContext.getExternalContext().redirect(previouslyTargetedPage);
@@ -90,6 +99,7 @@ public class UserLogin extends AbstractJsfBean {
                     logger.warn("Could not redirect to: " + previouslyTargetedPage, e);
                 }
             }
+
         } catch (ServletException le) {
             logger.error("ServletException Retrieved: ", le);
             addErrorMessage("The username and password you entered is incorrect.  Please try again.", "Authentication error");
@@ -98,4 +108,42 @@ public class UserLogin extends AbstractJsfBean {
 
         return redirect(targetPage);
     }
+
+    private String checkTargetForRoleRedirect(String targetPage, final HttpServletRequest request) {
+        StringBuilder newTargetPageBuilder = new StringBuilder(targetPage);
+        final boolean hasPMRole = request.isUserInRole( "Mercury-ProductManagers");
+        final boolean hasPDMRole = request.isUserInRole( "Mercury-ProjectManagers");
+
+        if (targetPage.endsWith(HOME_PAGE) || targetPage.endsWith(HOME_PAGE + "/")) {
+            if ( hasPMRole ) {
+                newTargetPageBuilder.append(PM_WELCOME_PAGE);
+            }
+            if ( hasPDMRole ) {
+                newTargetPageBuilder.append(PDM_WELCOME_PAGE);
+            }
+        } else if ( targetPage.endsWith(INDEX_REDIRECT)  || targetPage.endsWith(INDEX_PAGE) ) {
+            if ( hasPMRole ) {
+                newTargetPageBuilder =   new StringBuilder(targetPage.replace(INDEX_REDIRECT, PM_WELCOME_PAGE));
+            }
+            if ( hasPDMRole ) {
+                newTargetPageBuilder =    new StringBuilder(targetPage.replace(INDEX_REDIRECT, PDM_WELCOME_PAGE));
+            }
+        }
+        return newTargetPageBuilder.toString();
+    }
+
+    private String checkTargetForRoleRedirect(final HttpServletRequest request) {
+        String indexRedirect = INDEX_REDIRECT;
+        final boolean hasPMRole = request.isUserInRole( "Mercury-ProductManagers");
+        final boolean hasPDMRole = request.isUserInRole( "Mercury-ProjectManagers");
+
+        if ( hasPMRole ) {
+            indexRedirect =   PM_WELCOME_PAGE;
+        }
+        if ( hasPDMRole ) {
+            indexRedirect =   PDM_WELCOME_PAGE;
+        }
+        return indexRedirect;
+    }
+
 }
