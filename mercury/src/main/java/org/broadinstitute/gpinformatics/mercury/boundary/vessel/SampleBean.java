e178ea3427a781f1202485d2fc302e899cdfacf9
Jason Carey <jcarey@bm70f-2b1.broadinstitute.org>
2012-11-02 14:41:19 -0400

    Added new columns from the user story

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/vessel/SampleBean.java
@@ -0,0 +1,11 @@
+package org.broadinstitute.gpinformatics.mercury.boundary.vessel;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jcarey
+ * Date: 10/31/12
+ * Time: 3:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SampleBean {
+}
