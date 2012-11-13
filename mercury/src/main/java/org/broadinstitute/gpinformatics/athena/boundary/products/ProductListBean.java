e5a0c7f577f38153e6677b948cf093c715275885
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-11-13 18:59:03 -0500

    GPLIM-316 filtering and sorting in request scope for product list

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/products/ProductListBean.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/products/ProductListBean.java
@@ -1,11 +1,9 @@
 package org.broadinstitute.gpinformatics.athena.boundary.products;
 
 
-import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.broadinstitute.gpinformatics.athena.control.dao.products.ProductDao;
 import org.broadinstitute.gpinformatics.athena.entity.products.Product;
 import org.broadinstitute.gpinformatics.athena.entity.products.ProductComparator;
-import org.broadinstitute.gpinformatics.infrastructure.DataTableFilteredValuesBean;
 import org.broadinstitute.gpinformatics.mercury.presentation.AbstractJsfBean;
 import org.broadinstitute.gpinformatics.mercury.presentation.UserBean;
 
@@ -20,14 +18,11 @@ import java.util.List;
 
 
 /**
- * Backing bean for Products list/view and CRUD pages
+ * Backing bean for Product list
  */
 @Named("productList")
 @RequestScoped
 public class ProductListBean extends AbstractJsfBean implements Serializable {
-
-    @Inject
-    private DataTableFilteredValuesBean filteredValuesBean;
 
     @Inject
     private ProductDao productDao;
@@ -35,55 +30,36 @@ public class ProductListBean extends AbstractJsfBean implements Serializable {
     @Inject
     private UserBean userBean;
 
-    private ProductsDataModel productsDataModel;
+    private List<Product> products;
 
-    /**
-     * Hook the JSF preRenderView event to explicitly initiate a long-running conversation in the conversation scoped
-     * {@link DataTableFilteredValuesBean}
-     */
-    public void onPreRenderView() {
-        filteredValuesBean.beginConversation();
-    }
+    public List<Product> getProducts() {
 
-    public ProductsDataModel getProductsDataModel() {
-
-        if (productsDataModel == null) {
-            // "lazy" load, except this bean is request scoped so we end up creating a new ProductsDataModel
+        if (products == null) {
+            // "lazy" load, except this bean is request scoped so we end up creating a new Product list
             // for every request, including column sorts and each character typed into the search filter.
             //
             // Making a broader scoped cache of Products introduces LIEs on the Add-on and price item associations.
             // The ProductDao find method can be modified to left join fetch these associations, but the JPA criteria
             // API has some issues with left join fetching efficiently (it selects every column twice per
             // http://stackoverflow.com/questions/4511368/jpa-2-criteria-fetch-path-navigation).
-            List<Product> products = productDao.findProducts();
+            //
+            // Jon is going to look into second level cache to deal with the round-trip-to-DB problem
+            products = productDao.findProducts();
             Collections.sort(products, new ProductComparator());
 
-            productsDataModel = new ProductsDataModel(products);
         }
 
-        return productsDataModel;
+        return products;
     }
 
-    /**
-     * Generic comparison method for column sorts.  Assumes passed in parameters implement {@link Comparable}
-     */
-    public int compare(Object o1, Object o2) {
-        return new CompareToBuilder().append(o1, o2).toComparison();
-    }
-
-    @SuppressWarnings("unchecked")
-    public List<Product> getFilteredValues() {
-        return filteredValuesBean.getFilteredValues();
-    }
-
-    public void setFilteredValues(List<Product> filteredValues) {
-        filteredValuesBean.setFilteredValues(filteredValues);
-    }
 
     /**
      * Used for auto-complete in the UI, given a search term
      * @param search list of search terms, whitespace separated. If more than one term is present, all terms must
      *               match a substring in the text. Search is case insensitive.
+     *
+     * TODO make it clear what the use case for this method is, move it to an appropriate place (not the backing bean
+     * TODO for the product list), clean up case insensitivity of search
      */
     // FIXME: refactor for common cases
     public List<Product> getProductCompletions(String search) {
@@ -113,6 +89,10 @@ public class ProductListBean extends AbstractJsfBean implements Serializable {
     }
 
     // TODO hmc may not be the best way to do this
+    // TODO make it clear what the use case for this method is, move it to an appropriate place (not the backing bean
+    // TODO for the product list), clean up case insensitivity of search, consider whitespace delimited search terms
+    // TODO like the method above is doing
+
     public List<Product> searchProduct(String query) {
         List<Product> allProducts = productDao.findProducts();
         List<Product> products = new ArrayList<Product>();
