6a696f95db61c9cf198557a76d9d4ec8148b2377
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-11-13 18:14:29 -0500

    Revert overmerge: "GPLIM-244 GPLIM-236 ajax listeners for price item selection / unselection to prevent entry of problematic duplicates"
    This reverts commit 12cbabcd4b3e13b604a110933cf7e8c342f8ee9b.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/products/ProductFormConversationData.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/products/ProductFormConversationData.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 
 /**
- * Storing {@link PriceItem} data in a conversation to record the selected priceitems across AJAX requests in the
- * {@link Product} create / edit page.
+ * Storing {@link PriceItem} data in a conversation to enable crosstalk between "all priceitems" and "default priceitem"
+ * components.
  *
  */
 @ConversationScoped
@@ -23,17 +23,22 @@ public class ProductFormConversationData implements Serializable {
     private Conversation conversation;
 
     /**
-     * JAXB {@link PriceItem} DTOs, there can be only one default price item but this is a list so the PrimeFaces
-     * {@link org.primefaces.component.autocomplete.AutoComplete} can be styled consistently with all the other
-     * multi-selecting {@link org.primefaces.component.autocomplete.AutoComplete}s in Mercury.  Selection and
-     * unselection managed by the AJAX listeners in {@link ProductForm}
+     * JAXB {@link PriceItem} DTOs
      */
-    private List<PriceItem> selectedDefaultPriceItems;
+    private List<PriceItem> priceItems;
 
     /**
-     * Currently selected optional price items as noted by the AJAX select / unselect listeners in {@link ProductForm}.
+     * JAXB {@link PriceItem} DTOs, there can be only one default price item but this is a list so the PrimeFaces
+     * {@link org.primefaces.component.autocomplete.AutoComplete} can be styled consistently with all the other
+     * multi-selecting {@link org.primefaces.component.autocomplete.AutoComplete}s in Mercury
      */
-    private List<PriceItem> selectedOptionalPriceItems;
+    private List<PriceItem> defaultPriceItems;
+
+    /**
+     * Record of the ID in case it gets lost across AJAX requests so we can always tell whether we're in create or edit
+     * mode.
+     */
+    private Long id;
 
     /**
      * maps between entity and JAXB DTOs for price items
@@ -44,6 +49,7 @@ public class ProductFormConversationData implements Serializable {
         return new PriceItem(entity.getQuoteServerId(), entity.getPlatform(), entity.getCategory(), entity.getName());
     }
 
+
     /**
      * Method that initiates the long-running conversation.  Price items are pulled from the {@link Product} parameter.
      *
@@ -52,20 +58,23 @@ public class ProductFormConversationData implements Serializable {
     public void beginConversation(Product product) {
         if (conversation.isTransient()) {
             conversation.begin();
+            // start every conversation with initialized data!
+//            priceItems = new ArrayList<PriceItem>();
+            defaultPriceItems = new ArrayList<PriceItem>();
 
-            selectedDefaultPriceItems = new ArrayList<PriceItem>();
-            selectedOptionalPriceItems = new ArrayList<PriceItem>();
-
+/*
             if (product != null) {
+                id = product.getProductId();
                 if (product.getPriceItems() != null) {
                     for (org.broadinstitute.gpinformatics.athena.entity.products.PriceItem priceItem : product.getPriceItems()) {
-                        addSelectedOptionalPriceItem(entityToDto(priceItem));
+                        addPriceItem(entityToDto(priceItem));
                     }
                 }
                 if (product.getDefaultPriceItem() != null) {
-                    setSelectedDefaultPriceItem(entityToDto(product.getDefaultPriceItem()));
+                    defaultPriceItems.add(entityToDto(product.getDefaultPriceItem()));
                 }
             }
+*/
         }
     }
 
@@ -78,49 +87,88 @@ public class ProductFormConversationData implements Serializable {
 
 
     /**
+     * Get all the latest PriceItems as visualized on the Create / Edit Product page
+     * @return
+     */
+    public List<PriceItem> getPriceItems() {
+        return priceItems;
+    }
+
+    /**
+     * Setter for UI price items
+     * @param priceItems
+     */
+    public void setPriceItems(List<PriceItem> priceItems) {
+        this.priceItems = priceItems;
+    }
+
+    /**
+     * List version of default price items getter, as used by the actual {@link org.primefaces.component.autocomplete.AutoComplete}
+     * component in the create.xhtml
+     * @return
+     */
+    public List<PriceItem> getDefaultPriceItems() {
+        return defaultPriceItems;
+    }
+
+    /**
+     * List version of default price items setter, as used by the actual {@link org.primefaces.component.autocomplete.AutoComplete}
+     * component in the create.xhtml
+     * @return
+     */
+
+    public void setDefaultPriceItems(List<PriceItem> defaultPriceItems) {
+        this.defaultPriceItems = defaultPriceItems;
+    }
+
+    /**
      * Convenience setter to box the price item into its list
      * @param priceItem
      */
-    public void setSelectedDefaultPriceItem(PriceItem priceItem) {
-        if (selectedDefaultPriceItems == null) {
-            selectedDefaultPriceItems = new ArrayList<PriceItem>();
+    public void setDefaultPriceItem(PriceItem priceItem) {
+        if (defaultPriceItems == null) {
+            defaultPriceItems = new ArrayList<PriceItem>();
         }
 
-        selectedDefaultPriceItems.clear();
+        defaultPriceItems.clear();
 
         if (priceItem != null) {
-            selectedDefaultPriceItems.add(priceItem);
+            defaultPriceItems.add(priceItem);
         }
     }
 
     /**
      * Convenience getter to "unbox" the default price item from its list
      */
-    public PriceItem getSelectedDefaultPriceItem() {
-        if (selectedDefaultPriceItems == null || selectedDefaultPriceItems.size() == 0) {
+    public PriceItem getDefaultPriceItem() {
+        if (defaultPriceItems == null || defaultPriceItems.size() == 0) {
             return null;
         }
 
-        return selectedDefaultPriceItems.get(0);
+        return defaultPriceItems.get(0);
     }
 
-
-    public void addSelectedOptionalPriceItem(PriceItem priceItem) {
-        selectedOptionalPriceItems.add(priceItem);
+    /**
+     * Convenience methods to add to the list of current price items
+     * @param priceItem
+     */
+    public void addPriceItem(PriceItem priceItem) {
+        priceItems.add(priceItem);
     }
 
-
-    public void removeSelectedOptionalPriceItem(PriceItem priceItem) {
-        selectedOptionalPriceItems.remove(priceItem);
+    /**
+     * Convenience methods to remove from the list of current price items
+     * @param priceItem
+     */
+    public void removePriceItem(PriceItem priceItem) {
+        priceItems.remove(priceItem);
     }
 
-
-    public List<PriceItem> getAllSelectedPriceItems() {
-        List<PriceItem> allSelectedPriceItems = new ArrayList<PriceItem>();
-        allSelectedPriceItems.addAll(selectedDefaultPriceItems);
-        allSelectedPriceItems.addAll(selectedOptionalPriceItems);
-        return allSelectedPriceItems;
+    public Long getId() {
+        return id;
     }
 
-
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
