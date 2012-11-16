b1a37b0fb7575320859200ed34ccdbab31337539
Erik Husby <mhusby@broadinstitute.org>
2012-11-16 15:36:33 -0500

    GPLIM-325 - Procedures for creating the ReleaseCandidate and the Release

--- mercury/createRelease.sh
+++ mercury/createRelease.sh
@@ -11,27 +11,38 @@ fi
 mkdir release
 
 git clone ssh://git@stash.broadinstitute.org:7999/GPIN/mercury.git release
-cd release
-git checkout QA
-git checkout -b QA_PROD
-cd mercury
-mvn -P\!DefaultProfile --batch-mode release:prepare release:perform
-if [ $? -eq 0 ] ; then
-    git status
-    git branch -a
-    git tag -l
-    pushd target/checkout
-    git tag -a -m "Current Production" --force PROD HEAD
-    git push origin --tags
-    popd
-    git checkout master
-    git merge QA_PROD -m "REL-000 Update pom.xml with new version"
-    git branch -d QA_PROD
-    #git push origin :QA_PROD
-    git fetch origin +master
-    git push origin master
-else
-    echo "Release failed"
-    exit 1
-fi
+cd release/mercury
+
+git checkout RCBUILD
+# Determine current version numbers
+VERSION=`groovy -e 'print new XmlParser().parse(new File("pom.xml")).version.text()'`
+#
+# Split the version into its parts
+MAJOR=`expr $VERSION : '\([0-9]*\)'`
+MINOR=`expr $VERSION : '[0-9]*\.\([0-9]*\)'`
+REV=`expr $VERSION : '[0-9]*\.[0-9]*\(.*\)'`
+REV=${REV#\.}
+REV=${REV%-SNAPSHOT}
+
+RCBRANCH="RC-$MAJOR.$MINOR"
+git checkout $RCBRANCH
+PRODVERSION=${VERSION%-SNAPSHOT}
+
+git checkout --track -b $PRODVERSION
+mvn versions:set -DnewVersion="$PRODVERSION"
+git commit -m "REL-000 Setting Production Release version $PRODVERSION" pom.xml
+git push origin :PROD
+git tag -a -m "Current Production" --force PROD HEAD
+git push origin $PRODVERSION
+git push origin --tags
+
+git checkout $RCBRANCH
+
+NEXTRCVERSION="$MAJOR.$MINOR.$REV-SNAPSHOT"
+mvn versions:set -DnewVersion="$NEXTRCVERSION"
+git commit -m "REL-000 Setting RC Version $NEXTRCVERSION" pom.xml
+git push origin :RCBUILD
+git tag -a -m "Current RC" --force RCBUILD $RCBRANCH
+git push origin $RCBRANCH
+git push origin --tags
 
