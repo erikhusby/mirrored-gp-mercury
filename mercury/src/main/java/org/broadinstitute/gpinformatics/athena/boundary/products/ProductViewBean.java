34c1fccf02f907c834c1dabc53cf7dad886bfb68
Brian Reilly <brian.ireilly@gmail.com>
2012-11-19 13:22:56 -0500

    GPLIM-315

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/products/ProductViewBean.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/products/ProductViewBean.java
@@ -1,11 +1,9 @@
 package org.broadinstitute.gpinformatics.athena.boundary.products;
 
-
 import org.broadinstitute.gpinformatics.athena.entity.products.PriceItem;
 import org.broadinstitute.gpinformatics.athena.entity.products.PriceItemComparator;
 import org.broadinstitute.gpinformatics.athena.entity.products.Product;
 import org.broadinstitute.gpinformatics.athena.entity.products.ProductComparator;
-import org.broadinstitute.gpinformatics.athena.presentation.converter.ProductConverter;
 import org.broadinstitute.gpinformatics.athena.presentation.products.ProductForm;
 import org.broadinstitute.gpinformatics.infrastructure.jsf.TableData;
 import org.broadinstitute.gpinformatics.mercury.presentation.AbstractJsfBean;
@@ -13,26 +11,16 @@ import org.broadinstitute.gpinformatics.mercury.presentation.AbstractJsfBean;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Produces;
-import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 @Named("productView")
 @RequestScoped
 public class ProductViewBean extends AbstractJsfBean {
-
-    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("MMM dd, yyyy");
-
-    @Inject
-    private Conversation conversation;
 
     /**
      * Model object
@@ -40,168 +28,63 @@ public class ProductViewBean extends AbstractJsfBean {
     private Product product;
 
     /**
-     * Cache used to store the business key in view state so it will survive across AJAX sort requests
+     * Store add-ons in conversation scope to support p:dataTable sorting.
      */
-/*
-    private UIInput vsBusinessKeyCache;
-
-    public UIInput getVsBusinessKeyCache() {
-        return vsBusinessKeyCache;
-    }
-
-    public void setVsBusinessKeyCache(UIInput vsBusinessKeyCache) {
-        this.vsBusinessKeyCache = vsBusinessKeyCache;
-    }
-*/
-
-/*
-    private UIInput viewParam;
-
-    public UIInput getViewParam() {
-        return viewParam;
-    }
-
-    public void setViewParam(UIInput viewParam) {
-        this.viewParam = viewParam;
-    }
-*/
-
-    /**
-     * Breaking out add-ons to its own field both because it needs to be a List to be fed to DataTable, plus
-     * we need to give DataTable the same instance each time during the lifecycle so it can sort it
-     */
-//    private List<Product> addOns;
-
     @ConversationScoped public static class AddOnTableData extends TableData {}
     @Inject private AddOnTableData addOnData;
 
     /**
-     * Breaking out priceItems to its own field both because it needs to be a List to be fed to DataTable, plus
-     * we need to give DataTable the same instance each time during the lifecycle so it can sort it
+     * Store price items in conversation scope to support p:dataTable sorting.
      */
-//    private List<PriceItem> priceItems;
-
     @ConversationScoped public static class PriceItemTableData extends TableData {}
     @Inject private PriceItemTableData priceItemData;
 
     @Inject
     private FacesContext facesContext;
 
-//    @Inject
-//    private ProductConverter productConverter;
+    @Inject
+    private Conversation conversation;
 
     public void initView() {
         if (!facesContext.isPostback()) {
             addOnData.setValues(new ArrayList<Product>(product.getAddOns()));
+            Collections.sort(addOnData.getValues(), new ProductComparator());
             priceItemData.setValues(new ArrayList<PriceItem>(product.getPriceItems()));
+            Collections.sort(priceItemData.getValues(), new PriceItemComparator());
             if (conversation.isTransient()) {
                 conversation.begin();
             }
         }
     }
 
-    public Product getProduct() {
-//        loadProduct();
-        return product;
-    }
-
-/*
-    public void loadProduct() {
-        // if the model is not set but we have the business key in our cache binding, run the converter
-        // to generate the model.  we expect to be in this case for every ajax request on this page.
-        if (product == null && vsBusinessKeyCache != null && vsBusinessKeyCache.getValue() != null) {
-            product = productConverter.getAsObject((String) vsBusinessKeyCache.getValue());
-        }
-        // if we have the model but have not yet set the business key into our cache binding, set the key
-        // into the cache binding.  we expect to be in this case on the initial page render when the f:param
-        // has run the productConverter to set the model into this backing bean
-        else if (vsBusinessKeyCache != null && vsBusinessKeyCache.getValue() == null && product != null) {
-            vsBusinessKeyCache.setValue(product.getBusinessKey());
-        }
-    }
-*/
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-/*
-    public List<Product> getAddOns() {
-//        loadProduct();
-
-        if (product != null && addOns == null) {
-            addOns = new ArrayList<Product>();
-            addOns.addAll(product.getAddOns());
-            // TODO make ProductComparator a static field on Product
-            Collections.sort(addOns, new ProductComparator());
-        }
-
-        return addOns;
-    }
-
-    public List<PriceItem> getPriceItems() {
-//        loadProduct();
-
-        if (product != null && priceItems == null) {
-            priceItems = new ArrayList<PriceItem>();
-            priceItems.addAll(product.getPriceItems());
-            // TODO make PriceItemComparator a static field on PriceItem
-            Collections.sort(priceItems, new PriceItemComparator());
-        }
-
-        return priceItems;
-    }
-*/
-
     public void onPreRenderView() throws IOException {
-//        loadProduct();
-
-        if ( product == null ) {
+        if (product == null) {
             addErrorMessage("No product with this part number exists.");
             facesContext.renderResponse();
         }
     }
 
-    // TODO: use dateTimeConverter instead
-    public String getAvailabilityDate() {
-//        loadProduct();
-
-        if (product == null || product.getAvailabilityDate() == null) {
-            return "";
-        }
-
-        return DATE_FORMAT.format(product.getAvailabilityDate());
-    }
-
-
-    public String getDiscontinuedDate() {
-//        loadProduct();
-
-        if (product == null || product.getDiscontinuedDate() == null) {
-            return "";
-        }
-
-        return DATE_FORMAT.format(product.getDiscontinuedDate());
-    }
-
-
     // TODO: create and use secondsToDaysConverter
     public Integer getExpectedCycleTimeDays() {
-//        loadProduct();
         return ProductForm.convertCycleTimeSecondsToDays(product.getExpectedCycleTimeSeconds()) ;
     }
     public void setExpectedCycleTimeDays(final Integer expectedCycleTimeDays) {
-//        loadProduct();
         product.setExpectedCycleTimeSeconds(ProductForm.convertCycleTimeDaysToSeconds(expectedCycleTimeDays));
     }
 
     public Integer getGuaranteedCycleTimeDays() {
-//        loadProduct();
         return ProductForm.convertCycleTimeSecondsToDays(product.getGuaranteedCycleTimeSeconds()) ;
     }
     public void setGuaranteedCycleTimeDays(final Integer guaranteedCycleTimeDays) {
-//        loadProduct();
         product.setGuaranteedCycleTimeSeconds(ProductForm.convertCycleTimeDaysToSeconds(guaranteedCycleTimeDays));
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
     }
 
     public AddOnTableData getAddOnData() {
