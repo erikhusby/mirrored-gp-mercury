f0bdbf6fbf6ed32c51fe565aa0606c0a3fda2a9d
Dennis Ryan <dryan@broadinstitute.org>
2012-11-19 11:09:22 -0500

    GPLIM-308 Add reagent for baits

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/BaitReagent.java
@@ -0,0 +1,11 @@
+package org.broadinstitute.gpinformatics.mercury.entity.reagent;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dryan
+ * Date: 11/16/12
+ * Time: 10:18 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaitReagent extends GenericReagent {
+}
