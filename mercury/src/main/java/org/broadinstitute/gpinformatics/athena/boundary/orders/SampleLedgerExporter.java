7c31dbede7ce08cffa6094d6499f29cf447a353b
Miguel Covarrubias <mcovarr@broadinstitute.org>
2012-11-24 08:33:15 -0500

    GPLIM-390 remove unused constructor

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/orders/SampleLedgerExporter.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/orders/SampleLedgerExporter.java
@@ -5,8 +5,6 @@ import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.broadinstitute.bsp.client.users.BspUser;
 import org.broadinstitute.gpinformatics.athena.boundary.util.AbstractSpreadsheetExporter;
 import org.broadinstitute.gpinformatics.athena.control.dao.billing.BillingLedgerDao;
-import org.broadinstitute.gpinformatics.athena.control.dao.orders.ProductOrderDao;
-import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrderListEntry;
 import org.broadinstitute.gpinformatics.athena.entity.billing.BillingLedger;
 import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrder;
 import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrderSample;
@@ -48,12 +46,6 @@ public class SampleLedgerExporter extends AbstractSpreadsheetExporter {
 
     private BillingLedgerDao billingLedgerDao;
 
-    /**
-     * Classic version of the constructor taking an array of {@link ProductOrder}s
-     * @param productOrders
-     * @param bspUserList
-     * @param billingLedgerDao
-     */
     public SampleLedgerExporter(ProductOrder[] productOrders, BSPUserList bspUserList, BillingLedgerDao billingLedgerDao) {
         super();
 
@@ -69,20 +61,6 @@ public class SampleLedgerExporter extends AbstractSpreadsheetExporter {
             orderMap.get(productOrder.getProduct()).add(productOrder);
         }
 
-    }
-
-
-    /**
-     * New version of constructor taking an array of {@link ProductOrderListEntry}s, forced to cram the this() call
-     * into one line.
-     *
-     * @param productOrderListEntries
-     * @param bspUserList
-     * @param billingLedgerDao
-     * @param productOrderDao
-     */
-    public SampleLedgerExporter(ProductOrderListEntry[] productOrderListEntries, BSPUserList bspUserList, BillingLedgerDao billingLedgerDao, ProductOrderDao productOrderDao) {
-        this(productOrderDao.findListByProductOrderListEntries(Arrays.asList(productOrderListEntries)).toArray(new ProductOrder[0]), bspUserList, billingLedgerDao);
     }
 
     private String getBspFullName(long id) {
