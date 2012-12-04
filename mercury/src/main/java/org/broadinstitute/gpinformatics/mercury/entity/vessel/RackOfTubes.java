324efb7a372060b1f2bcce6b07f85f26a0797394
Scott Matthews <scottmat@broadinstitute.org>
2012-12-04 17:20:30 -0500

    GPLIM-362:  Making Rack of Tubes look like master.  Merge got confused.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/RackOfTubes.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/RackOfTubes.java
@@ -1,34 +1,24 @@
 package org.broadinstitute.gpinformatics.mercury.entity.vessel;
 
-import org.broadinstitute.gpinformatics.mercury.entity.labevent.LabEvent;
-import org.broadinstitute.gpinformatics.mercury.entity.sample.SampleInstance;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Table;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-// todo jmt rename to TubeFormation, create separate (non-LabVessel) class for rack
 /**
- * A rack of tubes
+ * A piece of plastic that holds tubes.  Can be reused to hold different sets of tubes.
+ * Compare to TubeFormation.
  */
 @Entity
 @Audited
 @Table(schema = "mercury")
-public class RackOfTubes extends LabVessel implements VesselContainerEmbedder<TwoDBarcodedTube> {
+public class RackOfTubes extends LabVessel{
+
+    /** For JPA */
+    RackOfTubes() {
+    }
 
     // todo jmt create interface implemented by this and PlateType, to get display name and geometry.
     public enum RackType {
@@ -51,116 +41,26 @@ public class RackOfTubes extends LabVessel implements VesselContainerEmbedder<Tw
         }
     }
 
-    // todo jmt can't make this non-null, because all LabVessels subtypes are in the same table
-    // todo jmt unique constraint?
-//    @Column(nullable = false)
-    private String digest;
-
     @Enumerated(EnumType.STRING)
     private RackType rackType;
-
-    @Embedded
-    private VesselContainer<TwoDBarcodedTube> vesselContainer = new VesselContainer<TwoDBarcodedTube>(this);
 
     public RackOfTubes(String label, RackType rackType) {
         super(label);
         this.rackType = rackType;
     }
 
-    protected RackOfTubes() {
-    }
-
-    @Override
-    public VesselGeometry getVesselGeometry() {
-        return rackType.getVesselGeometry();
-    }
-
     public RackType getRackType() {
         return rackType;
     }
 
-    public void makeDigest() {
-        List<Map.Entry<VesselPosition, String>> positionBarcodeList = new ArrayList<Map.Entry<VesselPosition, String>>();
-        for (Map.Entry<VesselPosition, TwoDBarcodedTube> barcodedTubeEntry : vesselContainer.getMapPositionToVessel().entrySet()) {
-            positionBarcodeList.add(new AbstractMap.SimpleEntry<VesselPosition, String>(
-                    barcodedTubeEntry.getKey(), barcodedTubeEntry.getValue().getLabel()));
-        }
-        this.digest = makeDigest(positionBarcodeList);
-    }
-
-    public static String makeDigest(List<Map.Entry<VesselPosition, String>> positionBarcodeList) {
-        Collections.sort(positionBarcodeList, new Comparator<Map.Entry<VesselPosition, String>>() {
             @Override
-            public int compare(Map.Entry<VesselPosition, String> o1, Map.Entry<VesselPosition, String> o2) {
-                return o1.getKey().compareTo(o2.getKey());
-            }
-        });
-        StringBuilder stringBuilder = new StringBuilder();
-        for (Map.Entry<VesselPosition, String> positionBarcodeEntry : positionBarcodeList) {
-            stringBuilder.append(positionBarcodeEntry.getKey());
-            stringBuilder.append(positionBarcodeEntry.getValue());
-        }
-        return makeDigest(stringBuilder.toString());
-    }
-
-    public static String makeDigest(String positionBarcodeTupleString) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] array = md.digest(positionBarcodeTupleString.getBytes("UTF-8"));
-            StringBuilder sb = new StringBuilder();
-            for (byte anArray : array) {
-                sb.append(Integer.toHexString((anArray & 0xFF) | 0x100).substring(1, 3));
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
+    public VesselGeometry getVesselGeometry() {
+        return rackType.getVesselGeometry();
     }
 
     @Override
     public CONTAINER_TYPE getType() {
         return CONTAINER_TYPE.RACK_OF_TUBES;
     }
-
-    public VesselContainer<TwoDBarcodedTube> getContainerRole() {
-        return this.vesselContainer;
-    }
-
-    public void setVesselContainer(VesselContainer<TwoDBarcodedTube> vesselContainer) {
-        this.vesselContainer = vesselContainer;
-    }
-
-    public void addTube(TwoDBarcodedTube tube,VesselPosition wellLocation) {
-        if (vesselContainer.getVesselAtPosition(wellLocation) != null) {
-            throw new RuntimeException(vesselContainer.getVesselAtPosition(wellLocation) + " is already in position " + wellLocation + " on rack " + getLabel());
-        }
-        vesselContainer.addContainedVessel(tube,wellLocation);
-    }
-
-/*
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(Hibernate.getClass(o).equals(RackOfTubes.class))) return false;
-
-        RackOfTubes that = (RackOfTubes) o;
-
-        if (!this.label.equals(that.getLabel())) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return this.label.hashCode();
-    }
-*/
-
-    public String getDigest() {
-        return digest;
-    }
-
 
 }
