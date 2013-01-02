e1fd3a4432beec49c4a1e4217e0adc417b729120
Hugh McCrory <mccrory@broadinstitute.org>
2013-01-02 12:20:04 -0500

    GPLIM-1 re-enabling test

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/boundary/billing/BillingTrackerManagerContainerTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/boundary/billing/BillingTrackerManagerContainerTest.java
@@ -26,7 +26,7 @@ import java.util.*;
 import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV;
 
 
-@Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled=false)
+@Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled=true)
 public class BillingTrackerManagerContainerTest extends Arquillian {
 
     public static final String BILLING_TRACKER_TEST_FILENAME = new String("BillingTracker-ContainerTest.xlsx");
@@ -69,7 +69,7 @@ public class BillingTrackerManagerContainerTest extends Arquillian {
     }
 
 
-    @Test( enabled = false )
+    @Test( enabled = true )
     public void testImport() throws Exception {
 
         FileInputStream fis=null;
