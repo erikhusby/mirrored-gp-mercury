3aac7dca35a50f6c8eafdc0969028d266ce5b785
Phil Shapiro <pshapiro@broadinstitute.org>
2013-01-08 10:07:18 -0500

    GPLIM-545: Fix test disablement, add try/catch to handle error during test startup.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/boundary/billing/WorkCompleteMessageBeanTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/boundary/billing/WorkCompleteMessageBeanTest.java
@@ -28,7 +28,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-@Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled = false)
 public class WorkCompleteMessageBeanTest extends ContainerTest{
 
     public static final String TEST_PDO_NAME = "PDO-xxx";
@@ -58,11 +57,17 @@ public class WorkCompleteMessageBeanTest extends ContainerTest{
 
         utx.begin();
 
-        WorkCompleteMessageBean workCompleteMessageBean = new WorkCompleteMessageBean(workCompleteMessageDao);
-        workCompleteMessageBean.onMessage(createMessage());
+        try {
+            WorkCompleteMessageBean workCompleteMessageBean = new WorkCompleteMessageBean(workCompleteMessageDao);
+            workCompleteMessageBean.onMessage(createMessage());
 
-        workCompleteMessageDao.flush();
-        workCompleteMessageDao.clear();
+            workCompleteMessageDao.flush();
+            workCompleteMessageDao.clear();
+        } catch (Exception e) {
+            // Make sure we rollback if this code fails, otherwise we can cause other tests to fail.
+            utx.rollback();
+            throw e;
+        }
     }
 
     @AfterMethod(groups = TestGroups.EXTERNAL_INTEGRATION)
@@ -75,7 +80,7 @@ public class WorkCompleteMessageBeanTest extends ContainerTest{
         utx.rollback();
     }
 
-    @Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled = false)
+    @Test(groups = TestGroups.EXTERNAL_INTEGRATION)
     public void testOnMessage() throws Exception {
         List<WorkCompleteMessage> messages = workCompleteMessageDao.getNewMessages();
         Assert.assertTrue("Should be at least one message in new message queue", !messages.isEmpty());
@@ -90,7 +95,7 @@ public class WorkCompleteMessageBeanTest extends ContainerTest{
         Assert.assertTrue("Should find our message in message queue", found);
     }
 
-    @Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled = false)
+    @Test(groups = TestGroups.EXTERNAL_INTEGRATION)
     public void testOnMessageReadBack() throws Exception {
         AutomatedBiller automatedBiller = new AutomatedBiller(workCompleteMessageDao, productOrderSampleDao, productOrderDao, billingLedgerDao);
         automatedBiller.processMessages();
