13e114c888abb5a884d3bad25fd02415af190c9f
Hugh McCrory <mccrory@broadinstitute.org>
2013-01-14 12:14:14 -0500

    GPLIM-576 - cache fix. named bean scope.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPCohortSearchServiceProducer.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPCohortSearchServiceProducer.java
@@ -3,7 +3,7 @@ package org.broadinstitute.gpinformatics.infrastructure.bsp;
 
 import org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment;
 
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.New;
 import javax.enterprise.inject.Produces;
@@ -17,10 +17,9 @@ public class BSPCohortSearchServiceProducer {
     @Inject
     private Deployment deployment;
 
-
     @Produces
     @Default
-    @SessionScoped
+    @ApplicationScoped
     public BSPCohortSearchService produce(@New BSPCohortSearchServiceStub stub, @New BSPCohortSearchServiceImpl impl) {
 
         if ( deployment == STUBBY )
