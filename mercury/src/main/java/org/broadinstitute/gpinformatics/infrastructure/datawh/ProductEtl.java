7cbe27d24136e5434de67d621174cb846989871d
Eliot Polk <epolk@broadinstitute.org>
2013-01-17 09:11:05 -0500

    GPLIM-796_Warehouse_ProductFamilyName

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/datawh/ProductEtl.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/datawh/ProductEtl.java
@@ -95,7 +95,6 @@ public class ProductEtl  extends GenericEntityEtl {
                 format(entity.getGuaranteedCycleTimeSeconds()),
                 format(entity.getSamplesPerWeek()),
                 format(entity.isTopLevelProduct()),
-                format(entity.getWorkflowName()),
                 format(entity.getProductFamily() != null ? entity.getProductFamily().getName() : null)
         );
     }
