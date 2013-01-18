09e7d3ad4e7307995501240000ade5e02b71494e
Jon Thompson <thompson@broadinstitute.org>
2013-01-18 12:04:39 -0500

    GPLIM-712 starting JIRA update

---
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/vessel/JiraCommentUtilTest.java
@@ -0,0 +1,49 @@
+package org.broadinstitute.gpinformatics.mercury.control.vessel;
+
+import org.broadinstitute.gpinformatics.infrastructure.jira.JiraService;
+import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
+import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
+import org.broadinstitute.gpinformatics.mercury.entity.project.JiraTicket;
+import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
+import org.broadinstitute.gpinformatics.mercury.entity.vessel.TwoDBarcodedTube;
+import org.broadinstitute.gpinformatics.mercury.entity.workflow.LabBatch;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.TEST;
+
+/**
+ * Test sending messaging status updates to JIRA
+ */
+public class JiraCommentUtilTest extends Arquillian {
+
+    @Deployment
+    public static WebArchive buildMercuryWar() {
+        return DeploymentBuilder.buildMercuryWar(TEST);
+    }
+
+    @Inject
+    private JiraService jiraService;
+
+    @Test(enabled = false, groups = TestGroups.EXTERNAL_INTEGRATION)
+    public void testMessaging() {
+        TwoDBarcodedTube twoDBarcodedTube = new TwoDBarcodedTube("1234");
+
+        HashSet<LabVessel> starters = new HashSet<LabVessel>();
+        starters.add(twoDBarcodedTube);
+        LabBatch labBatch = new LabBatch("LCSET-2690", starters);
+        labBatch.setJiraTicket(new JiraTicket(jiraService, "LCSET-2690"));
+        twoDBarcodedTube.addLabBatch(labBatch);
+
+        List<LabVessel> labVessels = new ArrayList<LabVessel>();
+        labVessels.add(twoDBarcodedTube);
+        JiraCommentUtil.postUpdate("test", "test", labVessels);
+    }
+}
