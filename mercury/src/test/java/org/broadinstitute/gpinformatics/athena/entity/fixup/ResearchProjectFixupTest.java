5d3c7d85b5e96d63cbc0d0d58ca9fb5b78c9974f
hrafal <hrafal@broadinstitute.org>
2013-02-01 16:22:28 -0500

    GPLIM-872: fixup script for changing owner

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/entity/fixup/ResearchProjectFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/entity/fixup/ResearchProjectFixupTest.java
@@ -1,8 +1,10 @@
 package org.broadinstitute.gpinformatics.athena.entity.fixup;
 
 import junit.framework.Assert;
+import org.broadinstitute.bsp.client.users.BspUser;
 import org.broadinstitute.gpinformatics.athena.control.dao.ResearchProjectDao;
 import org.broadinstitute.gpinformatics.athena.entity.project.ResearchProject;
+import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPUserList;
 import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -25,6 +27,9 @@ public class ResearchProjectFixupTest extends Arquillian {
 
     @Inject
     private ResearchProjectDao rpDao;
+
+    @Inject
+    private BSPUserList bspUserList;
 
     @Deployment
     public static WebArchive buildMercuryWar() {
@@ -61,5 +66,40 @@ public class ResearchProjectFixupTest extends Arquillian {
         // The entity is already persistent, this call to persist is solely to begin and end a transaction, so the
         // change gets flushed.  This is an artifact of the test environment.
         rpDao.persistAll(rpListToPersist);
+    }
+
+    @Test(enabled = false, groups = TestGroups.EXTERNAL_INTEGRATION)
+    public void reassignRPUser() {
+        String[] jiraKeys = new String[]{
+                "RP-28",
+                "RP-30",
+                "RP-80",
+                "RP-81"
+        };
+
+        // at the time of this writing this resolves to the one user we want, but add some checks to make sure that
+        // remains the case
+        List<BspUser> bspUsers = bspUserList.find("Christine Stevens");
+
+        if (bspUsers == null || bspUsers.isEmpty()) {
+            throw new RuntimeException("No Christine Stevens Found!");
+        }
+
+        if (bspUsers.size() > 1) {
+            throw new RuntimeException("Too many Christine Stevens found!");
+        }
+
+        BspUser bspUser = bspUsers.get(0);
+        if (!bspUser.getFirstName().equals("Christine") || !bspUser.getLastName().equals("Stevens")) {
+            throw new RuntimeException("Wrong person found: " + bspUser);
+        }
+
+        for (String jiraKey : jiraKeys) {
+            ResearchProject researchProject = rpDao.findByBusinessKey(jiraKey);
+            researchProject.setCreatedBy(bspUser.getUserId());
+
+            rpDao.persist(researchProject);
+        }
+
     }
 }
