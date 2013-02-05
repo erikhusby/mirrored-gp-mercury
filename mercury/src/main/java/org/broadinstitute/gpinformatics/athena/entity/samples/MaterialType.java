8c83ba91d8e85a3fe6f6ebea030d1c24eaa5b3df
Phil Shapiro <pshapiro@broadinstitute.org>
2013-02-05 09:21:15 -0500

    GPLIM-1: Remove unused field.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/samples/MaterialType.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/samples/MaterialType.java
@@ -3,6 +3,7 @@ package org.broadinstitute.gpinformatics.athena.entity.samples;
 import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.broadinstitute.gpinformatics.athena.entity.project.Cohort;
 import org.hibernate.envers.Audited;
 
 import javax.annotation.Nonnull;
@@ -44,8 +45,6 @@ public class MaterialType implements Serializable, Comparable<MaterialType> {
             return builder.build();
         }
     };
-
-    private transient String fullName;
 
     /**
      * Package visible constructor for JPA
