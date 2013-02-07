187b0c571f8af8da11db1a2c7ca26479fff7a41c
Brian Reilly <breilly@broadinstitute.org>
2013-02-07 12:00:23 -0500

    GPLIM-907: added performance test for BSPUserList.getByBadgeId().

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPUserListTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPUserListTest.java
@@ -41,4 +41,24 @@ public class BSPUserListTest extends Arquillian {
         BspUser user2 = bspUserList.getById(user1.getUserId());
         Assert.assertTrue(user1.equals(user2));
     }
+
+    /**
+     * Test the performance of BSPUserList.getByBadgeId(). Like the existing implementation of getById(), the current
+     * implementation of getByBadgeId() is rather inefficient. However, it's good enough, as long as it consistently
+     * returns a result in 50ms or less. This test assumes that querying on a badge ID that does not exist is the worst
+     * case for performance.
+     *
+     * TODO: consider moving this to LimsQueryResourceTest instead, since the overall performance of the service is probably a more meaningful measure
+     */
+    @Test
+    public void testGetByBadgeIdPerformance() {
+        long start = System.nanoTime();
+        BspUser user = bspUserList.getByBadgeId("BSPUserListTest.testGetByBadgeIdPerformance");
+        long end = System.nanoTime();
+        long durationNano = end - start;
+        double durationMilli = durationNano / 1000000.0;
+        Assert.assertTrue("User should not have been found", user == null);
+        Assert.assertTrue("Query for unknown badge ID should take less than 200ms. Actually took " +
+                durationMilli + "ms", durationMilli < 50);
+    }
 }
