0adf12974cff98d5a8f720046ea3bcf89db7c7bc
Eliot Polk <epolk@broadinstitute.org>
2013-02-10 16:13:44 -0500

    GPLIM-929-revisitEventEtlCode

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/datawh/ExtractTransformDbFreeTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/datawh/ExtractTransformDbFreeTest.java
@@ -56,15 +56,15 @@ public class ExtractTransformDbFreeTest {
         datafileDir = System.getProperty("java.io.tmpdir");
         badDataDir = datafileDir + System.getProperty("file.separator") + nowMsec;
         extractTransform = new ExtractTransform();
-        createMocks();
-
+        auditReaderDao = createMock(AuditReaderDao.class);
+        extractTransform.setAuditReaderDao(auditReaderDao);
     }
 
     @BeforeMethod
     public void beforeMethod() throws Exception {
         ExtractTransform.setDatafileDir(datafileDir);
         EtlTestUtilities.deleteEtlFiles(datafileDir);
-        resetAll();
+        reset(auditReaderDao);
     }
 
     @AfterMethod
@@ -72,215 +72,83 @@ public class ExtractTransformDbFreeTest {
         EtlTestUtilities.deleteEtlFiles(datafileDir);
     }
 
-
-    private void createMocks() {
-        auditReaderDao = createMock(AuditReaderDao.class);
-        extractTransform.setAuditReaderDao(auditReaderDao);
-
-        productEtl = createMock(ProductEtl.class);
-        extractTransform.setProductEtl(productEtl);
-
-        productOrderEtl = createMock(ProductOrderEtl.class);
-        extractTransform.setProductOrderEtl(productOrderEtl);
-
-        productOrderSampleEtl = createMock(ProductOrderSampleEtl.class);
-        extractTransform.setProductOrderSampleEtl(productOrderSampleEtl);
-
-        productOrderSampleStatusEtl = createMock(ProductOrderSampleStatusEtl.class);
-        extractTransform.setProductOrderSampleStatusEtl(productOrderSampleStatusEtl);
-
-        productOrderStatusEtl = createMock(ProductOrderStatusEtl.class);
-        extractTransform.setProductOrderStatusEtl(productOrderStatusEtl);
-
-        priceItemEtl = createMock(PriceItemEtl.class);
-        extractTransform.setPriceItemEtl(priceItemEtl);
-
-        researchProjectEtl = createMock(ResearchProjectEtl.class);
-        extractTransform.setResearchProjectEtl(researchProjectEtl);
-
-        researchProjectStatusEtl = createMock(ResearchProjectStatusEtl.class);
-        extractTransform.setResearchProjectStatusEtl(researchProjectStatusEtl);
-
-        projectPersonEtl = createMock(ProjectPersonEtl.class);
-        extractTransform.setProjectPersonEtl(projectPersonEtl);
-
-        researchProjectIrbEtl = createMock(ResearchProjectIrbEtl.class);
-        extractTransform.setResearchProjectIrbEtl(researchProjectIrbEtl);
-
-        researchProjectFundingEtl = createMock(ResearchProjectFundingEtl.class);
-        extractTransform.setResearchProjectFundingEtl(researchProjectFundingEtl);
-
-        researchProjectCohortEtl = createMock(ResearchProjectCohortEtl.class);
-        extractTransform.setResearchProjectCohortEtl(researchProjectCohortEtl);
-
-        productOrderAddOnEtl = createMock(ProductOrderAddOnEtl.class);
-        extractTransform.setProductOrderAddOnEtl(productOrderAddOnEtl);
-
-        eventEtl = createMock(EventEtl.class);
-        extractTransform.setEventEtl(eventEtl);
-
-        workflowConfigEtl = createMock(WorkflowConfigEtl.class);
-        extractTransform.setWorkflowConfigEtl(workflowConfigEtl);
-
-        labBatchEtl = createMock(LabBatchEtl.class);
-        extractTransform.setLabBatchEtl(labBatchEtl);
-
-        labVesselEtl = createMock(LabVesselEtl.class);
-        extractTransform.setLabVesselEtl(labVesselEtl);
-    }
-
-    private void replayAll() {
+    public void testInvalidDir1() {
         replay(auditReaderDao);
-        replay(productEtl);
-        replay(productOrderEtl);
-        replay(productOrderSampleEtl);
-        replay(productOrderSampleStatusEtl);
-        replay(productOrderStatusEtl);
-        replay(priceItemEtl);
-        replay(researchProjectEtl);
-        replay(researchProjectStatusEtl);
-        replay(projectPersonEtl);
-        replay(researchProjectIrbEtl);
-        replay(researchProjectFundingEtl);
-        replay(researchProjectCohortEtl);
-        replay(productOrderAddOnEtl);
-        replay(eventEtl);
-        replay(workflowConfigEtl);
-        replay(labBatchEtl);
-        replay(labVesselEtl);
-    }
-
-    private void verifyAll() {
-        verify(auditReaderDao);
-        verify(productEtl);
-        verify(productOrderEtl);
-        verify(productOrderSampleEtl);
-        verify(productOrderSampleStatusEtl);
-        verify(productOrderStatusEtl);
-        verify(priceItemEtl);
-        verify(researchProjectEtl);
-        verify(researchProjectStatusEtl);
-        verify(projectPersonEtl);
-        verify(researchProjectIrbEtl);
-        verify(researchProjectFundingEtl);
-        verify(researchProjectCohortEtl);
-        verify(productOrderAddOnEtl);
-        verify(eventEtl);
-        verify(workflowConfigEtl);
-        verify(labBatchEtl);
-        verify(labVesselEtl);
-    }
-
-    private void resetAll() {
-        reset(auditReaderDao);
-        reset(productEtl);
-        reset(productOrderEtl);
-        reset(productOrderSampleEtl);
-        reset(productOrderSampleStatusEtl);
-        reset(productOrderStatusEtl);
-        reset(priceItemEtl);
-        reset(researchProjectEtl);
-        reset(researchProjectStatusEtl);
-        reset(projectPersonEtl);
-        reset(researchProjectIrbEtl);
-        reset(researchProjectFundingEtl);
-        reset(researchProjectCohortEtl);
-        reset(productOrderAddOnEtl);
-        reset(eventEtl);
-        reset(workflowConfigEtl);
-        reset(labBatchEtl);
-        reset(labVesselEtl);
-    }
-
-    /**
-     * Passes a blank and a non-existent directory for datafiles.
-     */
-    public void testInvalidDir() {
-        replayAll();
         ExtractTransform.setDatafileDir(null);
         Assert.assertEquals(-1, extractTransform.incrementalEtl());
         Assert.assertEquals(Response.Status.INTERNAL_SERVER_ERROR,
                 extractTransform.backfillEtl(ProductOrderSample.class.getName(), 0, Long.MAX_VALUE));
-        verifyAll();
+        verify(auditReaderDao);
+    }
 
-        resetAll();
-        replayAll();
+    public void testInvalidDir2() {
+        replay(auditReaderDao);
         ExtractTransform.setDatafileDir("");
         Assert.assertEquals(-1, extractTransform.incrementalEtl());
         Assert.assertEquals(Response.Status.INTERNAL_SERVER_ERROR,
                 extractTransform.backfillEtl(ProductOrderSample.class.getName(), 0, Long.MAX_VALUE));
-        verifyAll();
+        verify(auditReaderDao);
+    }
 
-        resetAll();
-        replayAll();
+    public void testInvalidDir3() {
+        replay(auditReaderDao);
         ExtractTransform.setDatafileDir(badDataDir);
         Assert.assertEquals(-1, extractTransform.incrementalEtl());
         Assert.assertEquals(Response.Status.INTERNAL_SERVER_ERROR,
                 extractTransform.backfillEtl(ProductOrderSample.class.getName(), 0, Long.MAX_VALUE));
-        verifyAll();
+        verify(auditReaderDao);
     }
 
-    /**
-     * Mathematically excludes changes by setting last ETL version impossibly high.
-     */
     public void testNoChanges() {
-        replayAll();
+        replay(auditReaderDao);
         extractTransform.writeLastEtlRun(Long.MAX_VALUE - 1);
         Assert.assertEquals(0, extractTransform.incrementalEtl());
         Assert.assertTrue(ExtractTransform.getIncrementalRunStartTime() >= 0);
-        verifyAll();
+        verify(auditReaderDao);
     }
 
-    public void testBadRange() {
-        replayAll();
+    public void testBadRange1() {
+        replay(auditReaderDao);
         Assert.assertEquals(Response.Status.BAD_REQUEST,
                 extractTransform.backfillEtl(ProductOrderSample.class.getName(), -1, Long.MAX_VALUE));
-        verifyAll();
+        verify(auditReaderDao);
+    }
 
-        resetAll();
-        replayAll();
+    public void testBadRange2() {
+        replay(auditReaderDao);
         Assert.assertEquals(Response.Status.BAD_REQUEST,
                 extractTransform.backfillEtl(ProductOrderSample.class.getName(), 1000, 999));
-        verifyAll();
+        verify(auditReaderDao);
     }
 
-    /**
-     * Must supply fully qualified class name
-     */
     public void testInvalidClassName() {
-        replayAll();
+        replay(auditReaderDao);
         Assert.assertEquals(Response.Status.NOT_FOUND,
                 extractTransform.backfillEtl("NoSuchClass_Ihope", 0, Long.MAX_VALUE));
-        verifyAll();
+        verify(auditReaderDao);
     }
 
-    /**
-     * Passes a non-existent directory for the last run file.
-     */
+    /** Passes a non-existent directory for the last run file. */
     public void testInvalidLastEtlBadDir() {
-        replayAll();
+        replay(auditReaderDao);
         extractTransform.setDatafileDir(badDataDir);
         Assert.assertEquals(0L, extractTransform.readLastEtlRun());
-        verifyAll();
+        verify(auditReaderDao);
     }
 
-    /**
-     * Writes an unparsable timestamp.
-     */
+    /** Writes an unparsable timestamp. */
     public void testReadLastEtlUnparsable() throws IOException {
-        replayAll();
+        replay(auditReaderDao);
         File file = new File(datafileDir, ExtractTransform.LAST_ETL_FILE);
         FileUtils.write(file, "abcedfg");
 
         Assert.assertEquals(0L, extractTransform.readLastEtlRun());
-        verifyAll();
+        verify(auditReaderDao);
     }
 
-    /**
-     * Takes the mutex, ETL cannot run.
-     */
+    /** Takes the mutex, ETL cannot run. */
     public void testMutex() {
-        replayAll();
+        replay(auditReaderDao);
         Assert.assertTrue(ExtractTransform.getMutex().tryAcquire());
         try {
             int recordCount = extractTransform.incrementalEtl();
@@ -288,7 +156,7 @@ public class ExtractTransformDbFreeTest {
         } finally {
             ExtractTransform.getMutex().release();
         }
-        verifyAll();
+        verify(auditReaderDao);
     }
 }
 
