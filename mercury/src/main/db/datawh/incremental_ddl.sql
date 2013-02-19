f0a69c630b6f17c010e4dc8c5f86280488ab3cc1
Eliot Polk <epolk@broadinstitute.org>
2013-02-19 11:09:39 -0500

    GPLIM-929-revisitEventEtlCode

--- mercury/src/main/db/datawh/incremental_ddl.sql
+++ mercury/src/main/db/datawh/incremental_ddl.sql
@@ -3,6 +3,7 @@
 -- For release 1.17
 -------------------------------------------------------
 TRUNCATE TABLE event_fact;
+TRUNCATE TABLE im_event_fact;
 ALTER TABLE event_fact DROP CONSTRAINT fk_event_lab_vessel;
 ALTER TABLE event_fact DROP CONSTRAINT fk_event_workflow_config;
 
@@ -14,6 +15,7 @@ ALTER TABLE event_fact DROP COLUMN sample_key;
 ALTER TABLE event_fact DROP COLUMN workflow_config_id;
 ALTER TABLE lab_batch DROP COLUMN due_date;
 ALTER TABLE lab_batch DROP COLUMN is_active;
+ALTER TABLE lab_batch DROP COLUMN created_on;
 
 ALTER TABLE im_event_fact ADD (process_id numeric(19) not null);
 ALTER TABLE im_event_fact ADD (sample_name VARCHAR(40));
@@ -23,6 +25,7 @@ ALTER TABLE im_event_fact DROP COLUMN sample_key;
 ALTER TABLE im_event_fact DROP COLUMN workflow_config_id;
 ALTER TABLE im_lab_batch DROP COLUMN due_date;
 ALTER TABLE im_lab_batch DROP COLUMN is_active;
+ALTER TABLE im_lab_batch DROP COLUMN created_on;
 
 DROP TABLE workflow_config;
 DROP TABLE im_workflow_config;
