7937cfa2ddb03b33bc0d6badc108808f5fa33c33
Miguel Covarrubias <mcovarr@broadinstitute.org>
2013-02-27 14:01:04 -0500

    GPLIM-1 remove length specification for date validation as it doesn't really make sense.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/products/ProductActionBean.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/products/ProductActionBean.java
@@ -82,7 +82,7 @@ public class ProductActionBean extends CoreActionBean {
         @Validate(field="productName", required = true, maxlength=255, on={SAVE_ACTION}, label = "Product Name"),
         @Validate(field="partNumber", required = true, maxlength=255, on={SAVE_ACTION}, label="Part Number"),
         @Validate(field="description", required = true, maxlength = 2000, on={SAVE_ACTION}, label = "Description"),
-        @Validate(field="availabilityDate", required = true, maxlength = 255, on={SAVE_ACTION}, label = "Availability Date")
+        @Validate(field="availabilityDate", required = true, on={SAVE_ACTION}, label = "Availability Date")
     })
     private Product editProduct;
 
