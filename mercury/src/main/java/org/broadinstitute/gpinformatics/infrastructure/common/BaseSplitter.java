4a0e23974739760e4e0a3561ebf8624aad258213
Miguel Covarrubias <mcovarr@broadinstitute.org>
2013-03-07 16:30:00 -0500

    GPLIM-1076 Missed files.

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/common/BaseSplitter.java
@@ -0,0 +1,126 @@
+package org.broadinstitute.gpinformatics.infrastructure.common;
+
+import java.util.*;
+
+@SuppressWarnings("UnusedDeclaration")
+public class BaseSplitter {
+
+    /**
+     * The maximum number of arguments for an Oracle IN clause.
+     */
+    protected static final int DEFAULT_SPLIT_SIZE = 1000;
+
+    /**
+     * Split the data into chunks of a certain size, then return a list of collections,
+     * where each collection is a 'chunk' of data, using the default split size of
+     * {@link #DEFAULT_SPLIT_SIZE}.
+     *
+     * @param collection the Collection to be split out
+     * @param <T> type of data
+     * @return list of collections of split data
+     */
+    public static <T> List<Collection<T>> split(Collection<T> collection) {
+        return BaseSplitter.split(collection, DEFAULT_SPLIT_SIZE);
+    }
+
+    /**
+     * Split the data into chunks of a certain size, then return a list of collections,
+     * where each collection is a 'chunk' of data.<p/>
+     * For example calling split(foo, 100) where
+     * foo is a List of Longs of size 375 will result in a Collection being returned
+     * containing four Collection<Long>, the first three with 100 elements each and the
+     * last with 75 elements.
+     *
+     * @param collection the collection to be split into pieces.
+     * @param size number of data in each split
+     * @param <T> type of data
+     * @return A list of collections of data that has been split.
+     */
+    public static <T> List<Collection<T>> split(Collection<T> collection, int size) {
+
+        if (collection.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        if (collection.size() <= size) {
+            return Collections.singletonList(collection);
+        }
+
+        // The full split up results
+        List<Collection<T>> result = new ArrayList<Collection<T>>(collection.size() / size);
+
+        // If the collection is a list, use List.subList() to avoid unnecessary allocation and copying.
+
+        if (collection instanceof List) {
+            List<T> list = (List<T>) collection;
+            for (int i = 0; i < list.size(); i += size) {
+                result.add(list.subList(i, Math.min(list.size(), i + size)));
+            }
+        } else {
+            // Each chunk is here
+            Collection<T> temp = new ArrayList<T>(size);
+
+            // Go through each item in the list and add it to the temp list
+            for (T d : collection) {
+
+                // If we have reached the chunk size, then add it to the chunked result and clear the temp
+                if (temp.size() == size) {
+                    result.add(new ArrayList<T>(temp));
+                    temp.clear();
+                }
+
+                // add the item
+                temp.add(d);
+            }
+
+            // Add the last chunk right in.
+            result.add(temp);
+        }
+        return result;
+    }
+
+    /**
+     * Split the data into chunks of a certain size, then return a list of arrays,
+     * where each array is a 'chunk' of data, using the default split size of
+     * {@link #DEFAULT_SPLIT_SIZE}.
+     *
+     * @param array the array to be split out
+     * @param <T> type of data
+     * @return list of arrays of split data
+     */
+    public static <T> List<T[]> split(T[] array) {
+        return BaseSplitter.split(array, DEFAULT_SPLIT_SIZE);
+    }
+
+    /**
+     * Split the data into chunks of a certain size, then return a list of arrays,
+     * where each array is a 'chunk' of data.<p/>
+     * For example calling split(foo, 100) where
+     * foo is an array of long of size 375 will result in a Collection being returned
+     * containing four long[], the first three with 100 elements each and the
+     * last with 75 elements.
+     *
+     * @param array the array to be split out
+     * @param size number of data in each split
+     * @param <T> type of data
+     * @return A list of arrays of data that has been split.
+     */
+    public static <T> List<T[]> split(T[] array, int size) {
+        if (array.length == 0) {
+            return Collections.emptyList();
+        }
+
+        if (array.length <= size) {
+            return Collections.singletonList(array);
+        }
+
+        // The full split up results
+        List<T[]> result = new ArrayList<T[]>(array.length / size);
+
+        for (int i = 0; i < array.length; i += size) {
+            result.add(Arrays.copyOfRange(array, i, Math.min(array.length, i + size)));
+        }
+        return result;
+    }
+
+}
---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/jpa/JPASplitter.java
@@ -0,0 +1,86 @@
+package org.broadinstitute.gpinformatics.infrastructure.jpa;
+
+
+import org.broadinstitute.gpinformatics.infrastructure.common.BaseSplitter;
+
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * Splits long lists into multiple queries. Oracle suffers from only being able
+ * to run "in" queries on items of up to length 1000.
+ * This splits data sets into sets, then queries each set individually, and and unions
+ * the data together.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class JPASplitter extends BaseSplitter {
+
+    /**
+     * A convenience method that will run a HQL query. The query must have all
+     * the methods except for the long parameterList set on the query. This will
+     * then split the long parameter list into chunks, and run the query on each
+     * chunk of the data.
+     *
+     * @param query         The HQL query to be run. Set all parameters (except the long parameter list) for this first!
+     * @param propertyName  The name of the parameter in the HQL query
+     * @param parameterList The data to put into the query in the place of the parameterListName
+     *
+     * @return A List of the results, which is created by running the HQL query
+     *         over each chunk of the data and adding those results together.
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    public static <SPLIT_DATA_TYPE, RETURN_DATA_TYPE> List<RETURN_DATA_TYPE> runQuery(
+            Query query, String propertyName, Collection<SPLIT_DATA_TYPE> parameterList) {
+
+        // if the splitting is not needed, run the original query.
+        if (parameterList.size() < BaseSplitter.DEFAULT_SPLIT_SIZE) {
+            return runQueryStandard(query, propertyName, parameterList);
+        }
+
+        if (parameterList.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<RETURN_DATA_TYPE> result = new ArrayList<RETURN_DATA_TYPE>();
+        List<Collection<SPLIT_DATA_TYPE>> temp = split(parameterList);
+        for (Collection<SPLIT_DATA_TYPE> data : temp) {
+            List<RETURN_DATA_TYPE> o = runQueryStandard(query, propertyName, data);
+            result.addAll(o);
+        }
+        return result;
+    }
+
+
+    /**
+     * Run the query in the standard way, without any special splits, etc.
+     *
+     * @param query         The HQL query - null if a criteria query
+     * @param propertyName  The name of the parameter in the HQL query. null if a criteria query
+     * @param parameterList The data to put into the query in the place of the
+     *                      parameterListName
+     *
+     * @return The list of the results, created by running the HQL query.
+     */
+    @SuppressWarnings("unchecked")
+    private static <SPLIT_DATA_TYPE, RETURN_DATA_TYPE>
+    List<RETURN_DATA_TYPE> runQueryStandard(Query query,
+                                            String propertyName,
+                                            Collection<SPLIT_DATA_TYPE> parameterList) {
+
+        if (parameterList.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        if (query != null) {
+            query.setParameter(propertyName, parameterList);
+            return query.getResultList();
+        }
+        return Collections.emptyList();
+    }
+
+}
+
