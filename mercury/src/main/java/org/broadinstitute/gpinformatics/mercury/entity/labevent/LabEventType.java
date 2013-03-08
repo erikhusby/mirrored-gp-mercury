c14a22f4f588449a08afe894bbf44fd7bd6f6aab
Brian Reilly <breilly@broadinstitute.org>
2013-03-08 07:58:13 -0500

    GPLIM-1046: Added DenatureToFlowcellTransfer lab event type.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/labevent/LabEventType.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/labevent/LabEventType.java
@@ -89,9 +89,15 @@ public enum LabEventType {
             PlasticToValidate.SOURCE),
     STRIP_TUBE_B_TRANSFER("StripTubeBTransfer", true, false, SystemOfRecord.SQUID, false,
             PlasticToValidate.SOURCE),
+
+    // HiSeq 2000
     FLOWCELL_TRANSFER("FlowcellTransfer", true, false, SystemOfRecord.SQUID, false,
             PlasticToValidate.SOURCE),
     FLOWCELL_LOADED("FlowcellLoaded", true, false, SystemOfRecord.SQUID, false, PlasticToValidate.SOURCE),
+
+    // For HiSeq 2500
+    DENATURE_TO_FLOWCELL_TRANSFER("DenatureToFlowcellTransfer", false, true, SystemOfRecord.MERCURY, false,
+            PlasticToValidate.SOURCE),
 
     // Sage
     SAGE_LOADING("SageLoading", true, false, SystemOfRecord.SQUID, false, PlasticToValidate.SOURCE),
