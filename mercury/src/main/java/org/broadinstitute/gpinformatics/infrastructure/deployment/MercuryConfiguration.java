288fc8ece766a80adbcc05b52f7f669c81fc6f86
Miguel Covarrubias <mcovarr@broadinstitute.org>
2013-03-12 10:54:32 -0400

    GPLIM-1085 Cleanup to use Annovention over copy/pasted DZone code, but does not work either in Arquilllian or standalone.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/deployment/MercuryConfiguration.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/deployment/MercuryConfiguration.java
@@ -1,5 +1,9 @@
 package org.broadinstitute.gpinformatics.infrastructure.deployment;
 
+import com.impetus.annovention.ClasspathDiscoverer;
+import com.impetus.annovention.Discoverer;
+import com.impetus.annovention.Filter;
+import com.impetus.annovention.listener.ClassAnnotationDiscoveryListener;
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.io.IOUtils;
 import org.yaml.snakeyaml.Yaml;
@@ -39,64 +43,6 @@ public class MercuryConfiguration {
     private static MercuryConfiguration instance;
 
     private static String MERCURY_BUILD_INFO;
-
-
-    /**
-     * Scans all classes accessible from the context class loader which belong to the given package and subpackages.
-     *
-     * Copied from http://dzone.com/snippets/get-all-classes-within-package.
-     *
-     * @param packageName The base package
-     *
-     * @return The classes
-     *
-     * @throws ClassNotFoundException
-     * @throws IOException
-     */
-    private static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<File>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        ArrayList<Class> classes = new ArrayList<Class>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes.toArray(new Class[classes.size()]);
-    }
-
-    /**
-     * Recursive method used to find all classes in a given directory and subdirs.
-     *
-     * Copied from http://dzone.com/snippets/get-all-classes-within-package.
-     *
-     * @param directory   The base directory
-     * @param packageName The package name for classes found inside the base directory
-     * @return The classes
-     * @throws ClassNotFoundException
-     */
-    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<Class>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-            }
-        }
-        return classes;
-    }
-
 
 
     private class ExternalSystems {
@@ -177,26 +123,31 @@ public class MercuryConfiguration {
     private synchronized Class<? extends AbstractConfig> getConfigClass(String configKey) {
         if (CONFIG_CLASSES == null) {
 
-            Set<Class<? extends AbstractConfig>> localConfigClasses = new HashSet<Class<? extends AbstractConfig>>();
-            try {
+            final Set<Class<? extends AbstractConfig>> localConfigClasses =
+                    new HashSet<Class<? extends AbstractConfig>>();
 
-                for (Class clazz : getClasses("org.broadinstitute.gpinformatics")) {
-                    if (AbstractConfig.class.isAssignableFrom(clazz) && !(AbstractConfig.class == clazz)) {
-                        for (Annotation annotation : clazz.getDeclaredAnnotations()) {
-                            if (annotation.annotationType().equals(ConfigKey.class)) {
-                                //noinspection unchecked
-                                localConfigClasses.add(clazz);
-                            }
+            Discoverer discoverer = new ClasspathDiscoverer();
+            discoverer.addAnnotationListener(new ClassAnnotationDiscoveryListener() {
+                @Override
+                public void discovered(String clazzName, String annotation) {
+                    try {
+                        Class clazz = Class.forName(clazzName);
+                        if (AbstractConfig.class.isAssignableFrom(clazz) && !(AbstractConfig.class == clazz)) {
+                            //noinspection unchecked
+                            localConfigClasses.add((Class<? extends AbstractConfig>) clazz);
                         }
+                    } catch (ClassNotFoundException e) {
+                        throw new RuntimeException(e);
                     }
                 }
-                CONFIG_CLASSES = localConfigClasses;
 
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+                @Override
+                public String[] supportedAnnotations() {
+                    return new String[]{ConfigKey.class.getName()};
+                }
+            });
+
+            CONFIG_CLASSES = localConfigClasses;
         }
 
         for (Class<? extends AbstractConfig> clazz : CONFIG_CLASSES) {
