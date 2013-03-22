117a7939ba456d2a998fae4c0aa67680fb077d77
Miguel Covarrubias <mcovarr@broadinstitute.org>
2013-03-22 17:53:07 -0400

    GPLIM-1 Cleanup.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/common/BusinessKeySorterTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/common/BusinessKeySorterTest.java
@@ -1,17 +1,21 @@
 package org.broadinstitute.gpinformatics.infrastructure.common;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.testng.Assert;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 @Test(groups = TestGroups.DATABASE_FREE)
 public class BusinessKeySorterTest {
 
     public void testSimple() {
 
-        Assert.assertTrue(BusinessKeySorter.sort("AAA-2", "AAA-100") < 0);
-        Assert.assertTrue(BusinessKeySorter.sort("AAA-100", "AAA-2") > 0);
-        Assert.assertTrue(BusinessKeySorter.sort("AAA-2", "AAA-2") == 0);
-
+        assertThat(BusinessKeySorter.sort("AAA-2", "AAA-100"), is(lessThan(0)));
+        assertThat(BusinessKeySorter.sort("AAA-100", "AAA-2"), is(greaterThan(0)));
+        assertThat(BusinessKeySorter.sort("AAA-2", "AAA-2"), is(equalTo(0)));
     }
 }
