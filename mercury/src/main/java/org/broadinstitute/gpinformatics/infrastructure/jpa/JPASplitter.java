94c58ef9a6e992f47c095ce0c4bcc4dfcf3a6e5f
hrafal <hrafal@broadinstitute.org>
2013-03-27 15:21:14 -0400

    GPLIM-1: remove println

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/jpa/JPASplitter.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/jpa/JPASplitter.java
@@ -96,9 +96,6 @@ public class JPASplitter extends BaseSplitter {
         List<Collection<SPLIT_DATA_TYPE>> temp = split(parameterList);
         for (Collection<SPLIT_DATA_TYPE> data : temp) {
             List<RETURN_DATA_TYPE> o = runCriteriaQuery(criteriaCreator, data);
-
-            System.out.println("input size: " + data.size() + " output size: " + o.size());
-
             result.addAll(o);
         }
 
