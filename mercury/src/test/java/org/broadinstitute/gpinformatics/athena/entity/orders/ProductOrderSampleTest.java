5a0a611a21158387221554713e3f9d565ccb4e46
Phil Shapiro <pshapiro@broadinstitute.org>
2013-04-05 14:07:49 -0400

    GPLIM-1: Update test to use DataProvider; use EnumMap.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/entity/orders/ProductOrderSampleTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/entity/orders/ProductOrderSampleTest.java
@@ -9,9 +9,10 @@ import org.broadinstitute.gpinformatics.athena.entity.products.RiskCriterion;
 import org.broadinstitute.gpinformatics.athena.entity.samples.MaterialType;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPSampleDTO;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPSampleSearchColumn;
+import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.broadinstitute.gpinformatics.infrastructure.test.dbfree.ProductOrderTestFactory;
 import org.broadinstitute.gpinformatics.infrastructure.test.dbfree.ProductTestFactory;
-import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
+import org.hamcrest.Matcher;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -19,7 +20,7 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -34,14 +35,22 @@ import static org.hamcrest.Matchers.*;
 @Test(groups = TestGroups.DATABASE_FREE)
 public class ProductOrderSampleTest {
 
+    @DataProvider(name = "testIsInBspFormat")
+    public Object[][] createIsInBspFormatData() {
+        return new Object[][] {
+                {"SM-2ACG", inBspFormat()},
+                {"SM-2ACG5", inBspFormat()},
+                {"SM-2ACG6", inBspFormat()},
+                {"Blahblahblah", not(inBspFormat())},
+                {"12345", not(inBspFormat())},
+                {"SM-ABC0", not(inBspFormat())},
+                {"SM-ABCO", inBspFormat()}
+        };
+    }
 
-    @Test
-    public void testIsInBspFormat() throws Exception {
-        assertThat("SM-2ACG", is(inBspFormat()));
-        assertThat("SM-2ACG5", is(inBspFormat()));
-        assertThat("SM-2ACG6", is(inBspFormat()));
-        assertThat("Blahblahblah", is(not(inBspFormat())));
-        assertThat("12345", is(not(inBspFormat())));
+    @Test(dataProvider = "testIsInBspFormat")
+    public void testIsInBspFormat(String sampleName, Matcher<String> matcher) throws Exception {
+        assertThat(sampleName, is(matcher));
     }
 
     static final org.broadinstitute.bsp.client.sample.MaterialType BSP_MATERIAL_TYPE =
@@ -65,12 +74,12 @@ public class ProductOrderSampleTest {
             addOn.setPrimaryPriceItem(new PriceItem("A", "B", "C", "D"));
             product.addAddOn(addOn);
 
-            Map<BSPSampleSearchColumn, String> dataMap = new HashMap<BSPSampleSearchColumn, String>() {{
+            Map<BSPSampleSearchColumn, String> dataMap = new EnumMap<BSPSampleSearchColumn, String>(BSPSampleSearchColumn.class) {{
                 put(BSPSampleSearchColumn.MATERIAL_TYPE, BSP_MATERIAL_TYPE.getFullName());
             }};
             sample1 = new ProductOrderSample("Sample1", new BSPSampleDTO(dataMap));
 
-            dataMap = new HashMap<BSPSampleSearchColumn, String>() {{
+            dataMap = new EnumMap<BSPSampleSearchColumn, String>(BSPSampleSearchColumn.class) {{
                 put(BSPSampleSearchColumn.MATERIAL_TYPE, "XXX:XXX");
             }};
             sample2 = new ProductOrderSample("Sample2", new BSPSampleDTO(dataMap));
