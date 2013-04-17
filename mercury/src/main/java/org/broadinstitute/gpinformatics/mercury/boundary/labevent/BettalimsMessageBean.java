75e46c958c8c3eeb7238790876a8ea42403a29b6
Phil Shapiro <pshapiro@broadinstitute.org>
2013-04-17 16:49:09 -0400

    GPLIM-1: Refactor code to use utility method to create a session context, to handle cases where a session context is not in scope.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/labevent/BettalimsMessageBean.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/labevent/BettalimsMessageBean.java
@@ -1,19 +1,17 @@
 package org.broadinstitute.gpinformatics.mercury.boundary.labevent;
 
+import org.broadinstitute.gpinformatics.infrastructure.common.SessionContextUtility;
 import org.jboss.weld.context.bound.BoundSessionContext;
-import org.jboss.weld.context.ejb.EjbRequestContext;
 
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
-import java.util.HashMap;
 
 /**
 * A Message Driven Bean to receive JMS messages from liquid handling decks.
@@ -39,6 +37,9 @@ public class BettalimsMessageBean implements MessageListener {
     @Inject
     private BettalimsMessageResource bettalimsMessageResource;
 
+    @Inject
+    private SessionContextUtility sessionContextUtility;
+
     public BettalimsMessageBean() {
     }
 
@@ -47,38 +48,26 @@ public class BettalimsMessageBean implements MessageListener {
      * on the file system, and email in case of failure, so the recipient of the email can resubmit messages manually.
      * @param message JMS message from deck
      */
-    @TransactionAttribute(value= TransactionAttributeType.NOT_SUPPORTED)
+    @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     @Override
-    public void onMessage(Message message) {
-        try {
-            // Need to associate this JMS message with a session, to allow injection of Session scoped beans, e.g.
-            // UserBean (if there is no session, the UserBean proxy will be injected, but calling any of its methods
-            // causes an exception).
-            sessionContext.associate(new HashMap<String, Object>());
-            sessionContext.activate();
-            // The deck side code is written in JavaScript, so it sends text messages, rather than object messages.
-            if (message instanceof TextMessage) {
-                //noinspection OverlyBroadCatchBlock
-                try {
-                    String text = ((TextMessage) message).getText();
-                    bettalimsMessageResource.storeAndProcess(text);
-                } catch (Exception e) {
+    public void onMessage(final Message message) {
+        sessionContextUtility.executeInContext(new SessionContextUtility.Function() {
+            @Override
+            public void apply() {
+                // The deck side code is written in JavaScript, so it sends text messages, rather than object messages.
+                if (message instanceof TextMessage) {
+                    //noinspection OverlyBroadCatchBlock
+                    try {
+                        String text = ((TextMessage) message).getText();
+                        bettalimsMessageResource.storeAndProcess(text);
+                    } catch (Exception e) {
+                        // todo jmt email LIMS oddities
+                    }
+                } else {
                     // todo jmt email LIMS oddities
+                    //"Expected TextMessage, received " + message.getClass().getName()
                 }
-            } else {
-                // todo jmt email LIMS oddities
-                //"Expected TextMessage, received " + message.getClass().getName()
             }
-        } finally {
-            // There seems to be a bug in JBoss AS 7.1.1 that causes Stateful RequestScoped beans not to be destroyed
-            // at the end of onMessage.  This leads to a memory leak of org.hibernate.internal.util.collections.IdentityMap.
-            // Until this bug is fixed, we manually end the Request scope.
-            EjbRequestContext context = (EjbRequestContext) beanManager.getContext(RequestScoped.class);
-            context.invalidate();
-            context.deactivate();
-
-            sessionContext.invalidate();
-            sessionContext.deactivate();
-        }
+        });
     }
 }
