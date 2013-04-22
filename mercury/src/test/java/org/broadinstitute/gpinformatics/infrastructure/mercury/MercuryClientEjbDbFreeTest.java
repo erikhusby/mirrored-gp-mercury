831658d473e384b0e74a0bf17d76da27349d2b5f
Eliot Polk <epolk@broadinstitute.org>
2013-04-22 15:32:29 -0400

    GPLIM-1238-updateBucketCriteria

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/mercury/MercuryClientEjbDbFreeTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/mercury/MercuryClientEjbDbFreeTest.java
@@ -8,7 +8,6 @@ import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPSampleSearchColumn
 import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPUserList;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.plating.BSPManagerFactoryProducer;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.plating.BSPManagerFactoryStub;
-import org.broadinstitute.gpinformatics.infrastructure.gap.Samples;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.broadinstitute.gpinformatics.infrastructure.test.dbfree.ProductOrderTestFactory;
 import org.broadinstitute.gpinformatics.mercury.boundary.bucket.BucketBean;
@@ -27,7 +26,6 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import javax.persistence.criteria.Root;
 import java.util.*;
 
 import static org.easymock.EasyMock.*;
@@ -72,33 +70,11 @@ public class MercuryClientEjbDbFreeTest {
         bucket = new Bucket(new WorkflowStepDef(PICO_PLATING_BUCKET));
         bspDtoMap = new HashMap<String, BSPSampleDTO>();
 
-        final int SAMPLE_SIZE = 2;
+        final int SAMPLE_SIZE = 5;
 
         pdo = ProductOrderTestFactory.buildExExProductOrder(SAMPLE_SIZE);
         pdo.setCreatedBy(BSPManagerFactoryStub.QA_DUDE_USER_ID);
         pdoCreator = bspUserList.getById(BSPManagerFactoryStub.QA_DUDE_USER_ID).getUsername();
-
-        int rackPosition = 1;
-        for (ProductOrderSample pdoSample : pdo.getSamples()) {
-            LabVessel labVessel = new TwoDBarcodedTube("R" + rackPosition++);
-
-            Map<BSPSampleSearchColumn, String> bspData = new HashMap<BSPSampleSearchColumn, String>();
-            bspData.put(BSPSampleSearchColumn.PRIMARY_DISEASE, "Cancer");
-            bspData.put(BSPSampleSearchColumn.LSID, "org.broad:" + pdoSample.getSampleName());
-            bspData.put(BSPSampleSearchColumn.MATERIAL_TYPE, "DNA:DNA Genomic");
-            bspData.put(BSPSampleSearchColumn.COLLABORATOR_SAMPLE_ID, pdoSample.getSampleName());
-            bspData.put(BSPSampleSearchColumn.SPECIES, "Homo Sapiens");
-            bspData.put(BSPSampleSearchColumn.PARTICIPANT_ID, "PT-2345");
-            bspData.put(BSPSampleSearchColumn.ROOT_SAMPLE, "SM-000");
-
-            BSPSampleDTO bspDto = new BSPSampleDTO(bspData);
-            MercurySample mercurySample = new MercurySample(pdo.getBusinessKey(), pdoSample.getSampleName(), bspDto);
-
-            labVessel.addSample(mercurySample);
-            labBatch.addLabVessel(labVessel);
-            labVessels.add(labVessel);
-            bspDtoMap.put(pdoSample.getSampleName(), bspDto);
-        }
 
         service = new MercuryClientEjb(bucketBean, bucketDao, workflowLoader, bspUserList, labVesselDao,
                 bspSampleDataFetcher);
@@ -106,6 +82,61 @@ public class MercuryClientEjbDbFreeTest {
 
 
     public void testSamplesToPicoBucket() throws Exception {
+        Collection<ProductOrderSample> expectedSamples = new ArrayList<ProductOrderSample>();
+
+        int rackPosition = 1;
+        for (ProductOrderSample pdoSample : pdo.getSamples()) {
+            LabVessel labVessel = new TwoDBarcodedTube("R" + rackPosition++);
+
+            boolean expectedInBucket;
+            Map<BSPSampleSearchColumn, String> bspData = new HashMap<BSPSampleSearchColumn, String>();
+            switch (rackPosition) {
+                case 1:
+                    // Unreceived root.
+                    bspData.put(BSPSampleSearchColumn.MATERIAL_TYPE, "DNA:DNA Genomic");
+                    bspData.put(BSPSampleSearchColumn.SAMPLE_ID, pdoSample.getSampleName());
+                    bspData.put(BSPSampleSearchColumn.ROOT_SAMPLE, pdoSample.getSampleName());
+                    bspData.put(BSPSampleSearchColumn.RECEIPT_DATE, null);
+                    expectedInBucket = false;
+                    break;
+
+                case 2:
+                    // Non-genomic material.
+                    bspData.put(BSPSampleSearchColumn.MATERIAL_TYPE, "Tissue:Blood");
+                    bspData.put(BSPSampleSearchColumn.SAMPLE_ID, pdoSample.getSampleName());
+                    bspData.put(BSPSampleSearchColumn.ROOT_SAMPLE, null);
+                    expectedInBucket = false;
+                    break;
+
+                case 3:
+                    // Received root.
+                    bspData.put(BSPSampleSearchColumn.MATERIAL_TYPE, "DNA:DNA Genomic");
+                    bspData.put(BSPSampleSearchColumn.ROOT_SAMPLE, pdoSample.getSampleName());
+                    bspData.put(BSPSampleSearchColumn.SAMPLE_ID, pdoSample.getSampleName());
+                    bspData.put(BSPSampleSearchColumn.RECEIPT_DATE, "2013-04-22");
+                    expectedInBucket = true;
+                    break;
+
+                default:
+                    // Non-root samples.
+                    bspData.put(BSPSampleSearchColumn.MATERIAL_TYPE, "DNA:DNA Genomic");
+                    bspData.put(BSPSampleSearchColumn.ROOT_SAMPLE, null);
+                    bspData.put(BSPSampleSearchColumn.SAMPLE_ID, pdoSample.getSampleName());
+                    expectedInBucket = true;
+                    break;
+            }
+            BSPSampleDTO bspDto = new BSPSampleDTO(bspData);
+            MercurySample mercurySample = new MercurySample(pdo.getBusinessKey(), pdoSample.getSampleName(), bspDto);
+
+            if (expectedInBucket) {
+                expectedSamples.add(pdoSample);
+            }
+
+            labVessel.addSample(mercurySample);
+            labBatch.addLabVessel(labVessel);
+            labVessels.add(labVessel);
+            bspDtoMap.put(pdoSample.getSampleName(), bspDto);
+        }
 
         expect(labVesselDao.findBySampleKeyList((List<String>) anyObject())).andReturn(labVessels);
         expect(bucketDao.findByName(PICO_PLATING_BUCKET)).andReturn(bucket);
@@ -117,21 +148,8 @@ public class MercuryClientEjbDbFreeTest {
 
         replay(mocks);
 
-        // Muck around to get sample lists to be sorted the same.
-        List<ProductOrderSample> addedSamples = new ArrayList<ProductOrderSample>(service.addFromProductOrder(pdo));
-        List<ProductOrderSample> pdoSamples = new ArrayList<ProductOrderSample>(pdo.getSamples());
-
-        Comparator<ProductOrderSample> pdoSampleComparator = new Comparator<ProductOrderSample>() {
-            @Override
-            public int compare(ProductOrderSample o1, ProductOrderSample o2) {
-                return o1.getSampleName().compareTo(o2.getSampleName());
-            }
-        };
-
-        Collections.sort(addedSamples, pdoSampleComparator);
-        Collections.sort(pdoSamples, pdoSampleComparator);
-
-        Assert.assertEquals(addedSamples, pdoSamples);
+        Collection<ProductOrderSample> addedSamples = service.addFromProductOrder(pdo);
+        Assert.assertEqualsNoOrder(addedSamples.toArray(), expectedSamples.toArray());
 
         verify(mocks);
     }
