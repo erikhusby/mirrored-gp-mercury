0cca73271ed34338320dfd326222971ac1200b7b
jcarey <pesmag529!>
2013-05-13 11:27:09 -0400

    GPLIM-1267 Remove local test setup.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/labevent/BettalimsMessageResourceTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/labevent/BettalimsMessageResourceTest.java
@@ -169,7 +169,7 @@ public class BettalimsMessageResourceTest extends Arquillian {
     /**
      * Sends messages for one PDO, then reworks two of those samples along with a second PDO.
      */
-    @Test(enabled = true, groups = EXTERNAL_INTEGRATION, timeOut = 0)
+    @Test(enabled = false, groups = EXTERNAL_INTEGRATION)
     public void testRework() throws ValidationException {
         // Set up one PDO / bucket / batch
         String testPrefix = testPrefixDateFormat.format(new Date());
