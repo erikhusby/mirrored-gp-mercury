fdf8c7d7b2970141c8c849557df327ca8f8e92e9
dinsmore <dinsmore@broadinstitute.org>
2013-05-20 16:58:36 -0400

    GPLIM-1325 split uses regex, so you have to escape first

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/dao/analysis/ReferenceSequenceDao.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/dao/analysis/ReferenceSequenceDao.java
@@ -1,9 +1,13 @@
 package org.broadinstitute.gpinformatics.mercury.control.dao.analysis;
 
+import org.broadinstitute.gpinformatics.infrastructure.jpa.BusinessKeyFinder;
 import org.broadinstitute.gpinformatics.infrastructure.jpa.GenericDao;
+import org.broadinstitute.gpinformatics.mercury.entity.analysis.Aligner;
+import org.broadinstitute.gpinformatics.mercury.entity.analysis.Aligner_;
 import org.broadinstitute.gpinformatics.mercury.entity.analysis.ReferenceSequence;
 import org.broadinstitute.gpinformatics.mercury.entity.analysis.ReferenceSequence_;
 
+import javax.ejb.LocalBean;
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.criteria.CriteriaBuilder;
@@ -16,15 +20,32 @@ import java.util.List;
  * Data Access for Analysis types.
  */
 @Stateful
+@LocalBean
 @RequestScoped
-public class ReferenceSequenceDao extends GenericDao {
+public class ReferenceSequenceDao extends GenericDao implements BusinessKeyFinder {
 
     public List<ReferenceSequence> findAll() {
         return findAll(ReferenceSequence.class);
     }
 
-    public ReferenceSequence findByBusinessKey(String value) {
-        String[] values = value.split("|");
+    /**
+     * Get all the current reference sequences.
+     *
+     * @return list of all the current {@link ReferenceSequence}s
+     */
+    public List<ReferenceSequence> findAllCurrent() {
+        CriteriaBuilder criteriaBuilder = getCriteriaBuilder();
+
+        final CriteriaQuery<ReferenceSequence> query = criteriaBuilder.createQuery(ReferenceSequence.class);
+        Root<ReferenceSequence> root = query.from(ReferenceSequence.class);
+        Predicate currentPredicate = criteriaBuilder.equal(root.get(ReferenceSequence_.isCurrent), true);
+        query.where(currentPredicate);
+        return getEntityManager().createQuery(query).getResultList();
+    }
+
+
+    public ReferenceSequence findByBusinessKey(String businessKey) {
+        String[] values = businessKey.split("\\|");
 
         CriteriaBuilder criteriaBuilder = getCriteriaBuilder();
         final CriteriaQuery<ReferenceSequence> query = criteriaBuilder.createQuery(ReferenceSequence.class);
