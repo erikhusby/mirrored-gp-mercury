aae5d924214a1d789b1cf5ccc28efe4d7ccd368e
Phil Shapiro <pshapiro@broadinstitute.org>
2013-05-23 17:09:38 -0400

    GPLIM-1: Fix test to match cleaned-up code.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/deployment/MercuryConfigurationTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/deployment/MercuryConfigurationTest.java
@@ -18,57 +18,59 @@ import java.util.Map;
 @Test(groups = TestGroups.DATABASE_FREE)
 public class MercuryConfigurationTest {
 
-    final String GLOBAL_YAML = "/config/test-mercury-config.yaml";
+    static final String GLOBAL_YAML = "/config/test-mercury-config.yaml";
 
-    final String LOCAL_YAML = "/config/test-mercury-config-local.yaml";
+    static final String LOCAL_YAML = "/config/test-mercury-config-local.yaml";
 
 
     public void testYaml() throws FileNotFoundException {
 
         URL url;
         FileInputStream is;
-        final Yaml yaml = new Yaml();
+        Yaml yaml = new Yaml();
 
         url = getClass().getResource(GLOBAL_YAML);
         is = new FileInputStream(url.getFile());
 
         @SuppressWarnings("unchecked")
-        final Map<String, Map> globalDoc = (Map<String, Map>) yaml.load(is);
+        Map<String, Map<String, Map<String, String>>> globalDoc =
+                (Map<String, Map<String, Map<String, String>>>) yaml.load(is);
 
         url = getClass().getResource(LOCAL_YAML);
         is = new FileInputStream(url.getFile());
 
         @SuppressWarnings("unchecked")
-        final Map<String, Map> localDoc = (Map<String, Map>) yaml.load(is);
+        Map<String, Map<String, Map<String, String>>> localDoc =
+                (Map<String, Map<String, Map<String, String>>>) yaml.load(is);
 
-        final MercuryConfiguration configuration = MercuryConfiguration.getInstance();
+        MercuryConfiguration configuration = MercuryConfiguration.getInstance();
 
         configuration.clear();
         configuration.load(globalDoc, localDoc);
 
-        final ThriftConfig qaThriftConfig = (ThriftConfig) configuration.getConfig(ThriftConfig.class, Deployment.QA);
+        ThriftConfig qaThriftConfig = (ThriftConfig) configuration.getConfig(ThriftConfig.class, Deployment.QA);
         Assert.assertEquals(qaThriftConfig.getHost(), "seqtest04");
 
-        final ThriftConfig devThriftConfig = (ThriftConfig) configuration.getConfig(ThriftConfig.class, Deployment.DEV);
+        ThriftConfig devThriftConfig = (ThriftConfig) configuration.getConfig(ThriftConfig.class, Deployment.DEV);
 
         // The Deployments passed to configReader are Mercury-centric, and both QA and DEV Mercurys point to QA thrift,
         // so these should actually be the same configs
-        Assert.assertTrue(qaThriftConfig == devThriftConfig);
+        Assert.assertEquals(qaThriftConfig, devThriftConfig);
 
         // Test override of parameters
 
-        final SquidConfig devSquidConfig = (SquidConfig) configuration.getConfig(SquidConfig.class, Deployment.DEV);
+        SquidConfig devSquidConfig = (SquidConfig) configuration.getConfig(SquidConfig.class, Deployment.DEV);
 
         Assert.assertEquals(devSquidConfig.getUrl(), "http://localhost:9090");
 
         // More parameter override, make sure we still have the non-local parameters set
-        final JiraConfig testJiraConfig = (JiraConfig) configuration.getConfig(JiraConfig.class, Deployment.TEST);
+        JiraConfig testJiraConfig = (JiraConfig) configuration.getConfig(JiraConfig.class, Deployment.TEST);
 
         Assert.assertEquals(testJiraConfig.getPort(), 8888);
         Assert.assertEquals(testJiraConfig.getHost(), "labopsjiratest.broadinstitute.org");
 
         // And Deployment mappings
-        final BSPConfig testBSPConfig = (BSPConfig) configuration.getConfig(BSPConfig.class, Deployment.TEST);
+        BSPConfig testBSPConfig = (BSPConfig) configuration.getConfig(BSPConfig.class, Deployment.TEST);
 
         Assert.assertEquals(testBSPConfig.getHost(), "bsp.broadinstitute.org");
     }
