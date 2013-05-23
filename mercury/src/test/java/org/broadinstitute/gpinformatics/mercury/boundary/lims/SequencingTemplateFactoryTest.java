ce9b9bc2daa0889c6bcb13b58dbebd2bef3b9f29
Brian Reilly <breilly@broadinstitute.org>
2013-05-23 15:26:23 -0400

    GPLIM-1309: Added some more asserts. Test is currently passing.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/lims/SequencingTemplateFactoryTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/lims/SequencingTemplateFactoryTest.java
@@ -4,22 +4,24 @@ import org.broadinstitute.gpinformatics.mercury.entity.labevent.LabEvent;
 import org.broadinstitute.gpinformatics.mercury.entity.labevent.VesselToSectionTransfer;
 import org.broadinstitute.gpinformatics.mercury.entity.run.IlluminaFlowcell;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
-import org.broadinstitute.gpinformatics.mercury.entity.vessel.SBSSection;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.TwoDBarcodedTube;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.VesselPosition;
+import org.broadinstitute.gpinformatics.mercury.limsquery.generated.SequencingTemplateLaneType;
 import org.broadinstitute.gpinformatics.mercury.limsquery.generated.SequencingTemplateType;
 import org.testng.annotations.Test;
 
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import static org.broadinstitute.gpinformatics.infrastructure.test.TestGroups.DATABASE_FREE;
 import static org.broadinstitute.gpinformatics.mercury.entity.labevent.LabEventType.DENATURE_TO_FLOWCELL_TRANSFER;
 import static org.broadinstitute.gpinformatics.mercury.entity.run.IlluminaFlowcell.FlowcellType.HiSeq2500Flowcell;
 import static org.broadinstitute.gpinformatics.mercury.entity.vessel.SBSSection.ALL2;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -39,14 +41,20 @@ public class SequencingTemplateFactoryTest {
                         new LabEvent(DENATURE_TO_FLOWCELL_TRANSFER, new Date(),
                                 "SequencingTemplateFactoryTest#testGetSequencingTemplate", 1L, 1L));
 
-        final List<VesselPosition> vesselPositions = SBSSection.ALL2.getWells();
-        Map<VesselPosition, LabVessel> labVessels=new HashMap<VesselPosition, LabVessel>();
-        for (VesselPosition vesselPosition : vesselPositions) {
-            labVessels.put(vesselPosition,denatureTube);
+        Map<VesselPosition, LabVessel> labVessels = new HashMap<VesselPosition, LabVessel>();
+        for (VesselPosition vesselPosition : ALL2.getWells()) {
+            labVessels.put(vesselPosition, denatureTube);
         }
 
         SequencingTemplateType template = factory.getSequencingTemplate(flowcell, labVessels);
         assertThat(template.getBarcode(), equalTo("flowcell_barcode"));
         assertThat(template.getLanes().size(), is(2));
+        Set<String> allLanes = new HashSet<String>();
+        for (SequencingTemplateLaneType lane : template.getLanes()) {
+            assertThat(lane.getLoadingVesselLabel(), equalTo("denature_tube_barcode"));
+            allLanes.add(lane.getLaneName());
+        }
+        assertThat(allLanes, hasItem("LANE1"));
+        assertThat(allLanes, hasItem("LANE2"));
     }
 }
