939a4ed94d199a2c9aa49274e985e8bff8985717
jcarey <jcarey@broadinstitute.org>
2013-06-05 14:08:26 -0400

    GPLIM-1373 Added action bean and jsp for the denature tube to reagent block pages.

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/workflow/LinkDenatureTubeToReagentBlockActionBean.java
@@ -0,0 +1,109 @@
+package org.broadinstitute.gpinformatics.mercury.presentation.workflow;
+
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.HandlesEvent;
+import net.sourceforge.stripes.action.RedirectResolution;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidationMethod;
+import org.apache.commons.lang3.StringUtils;
+import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrder;
+import org.broadinstitute.gpinformatics.infrastructure.athena.AthenaClientService;
+import org.broadinstitute.gpinformatics.mercury.control.dao.vessel.LabVesselDao;
+import org.broadinstitute.gpinformatics.mercury.entity.sample.SampleInstance;
+import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
+import org.broadinstitute.gpinformatics.mercury.entity.vessel.TwoDBarcodedTube;
+import org.broadinstitute.gpinformatics.mercury.presentation.CoreActionBean;
+
+import javax.inject.Inject;
+
+@UrlBinding("/workflow/LinkDenatureTubeToReagentBlock.action")
+public class LinkDenatureTubeToReagentBlockActionBean extends CoreActionBean {
+    private static String VIEW_PAGE = "/workflow/link_dtube_to_rb.jsp";
+
+    @Inject
+    private LabVesselDao labVesselDao;
+
+    @Inject
+    AthenaClientService athenaClientService;
+
+    @Validate(required = true, on = SAVE_ACTION)
+    private String denatureTubeBarcode;
+
+    @Validate(required = true, on = SAVE_ACTION)
+    private String reagentBlockBarcode;
+
+    private TwoDBarcodedTube denatureTube;
+
+    private String workflowName;
+
+    public String getReagentBlockBarcode() {
+        return reagentBlockBarcode;
+    }
+
+    public void setReagentBlockBarcode(String reagentBlockBarcode) {
+        this.reagentBlockBarcode = reagentBlockBarcode;
+    }
+
+    public String getDenatureTubeBarcode() {
+        return denatureTubeBarcode;
+    }
+
+    public void setDenatureTubeBarcode(String denatureTubeBarcode) {
+        this.denatureTubeBarcode = denatureTubeBarcode;
+    }
+
+    public LabVessel getDenatureTube() {
+        return denatureTube;
+    }
+
+    public void setDenatureTube(TwoDBarcodedTube denatureTube) {
+        this.denatureTube = denatureTube;
+    }
+
+    public String getWorkflowName() {
+        return workflowName;
+    }
+
+    public void setWorkflowName(String workflowName) {
+        this.workflowName = workflowName;
+    }
+
+    @DefaultHandler
+    @HandlesEvent(VIEW_ACTION)
+    public ForwardResolution showPage() {
+        return new ForwardResolution(VIEW_PAGE);
+    }
+
+    @HandlesEvent(SAVE_ACTION)
+    public Resolution save() {
+        addMessage("Denature Tube {0} associated with Reagent Block {1}", denatureTubeBarcode, reagentBlockBarcode);
+        return new RedirectResolution(VIEW_PAGE);
+    }
+
+    @ValidationMethod(on = SAVE_ACTION)
+    public void validateData() {
+        denatureTube = (TwoDBarcodedTube) labVesselDao.findByIdentifier(denatureTubeBarcode);
+        if (denatureTube == null) {
+            addValidationError("denatureTubeBarcode", "Could not find denature tube {0}", denatureTubeBarcode);
+        }
+    }
+
+    @HandlesEvent("denatureInfo")
+    public ForwardResolution denatureTubeInfo() {
+        denatureTube = (TwoDBarcodedTube) labVesselDao.findByIdentifier(denatureTubeBarcode);
+        if (denatureTube != null) {
+            for (SampleInstance sample : denatureTube.getAllSamples()) {
+                String productOrderKey = sample.getProductOrderKey();
+                if (StringUtils.isNotEmpty(productOrderKey)) {
+                    ProductOrder order = athenaClientService.retrieveProductOrderDetails(productOrderKey);
+                    workflowName = order.getProduct().getWorkflowName();
+                    break;
+                }
+            }
+        }
+        return new ForwardResolution("/workflow/denature_tube_info.jsp");
+    }
+}
