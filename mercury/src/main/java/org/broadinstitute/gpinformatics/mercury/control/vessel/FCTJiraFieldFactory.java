ef3686acde8dd18be4bd5961bcaa1f9221a94210
jcarey <jcarey@broadinstitute.org>
2013-06-20 13:11:27 -0400

    GPLIM-1434 Added FCT jira field factory.

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/vessel/FCTJiraFieldFactory.java
@@ -0,0 +1,39 @@
+package org.broadinstitute.gpinformatics.mercury.control.vessel;
+
+import org.broadinstitute.gpinformatics.infrastructure.jira.customfields.CustomField;
+import org.broadinstitute.gpinformatics.infrastructure.jira.customfields.CustomFieldDefinition;
+import org.broadinstitute.gpinformatics.mercury.entity.workflow.LabBatch;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Scott Matthews
+ *         Date: 1/28/13
+ *         Time: 5:02 PM
+ */
+public class FCTJiraFieldFactory extends AbstractBatchJiraFieldFactory {
+
+
+    public FCTJiraFieldFactory(@Nonnull LabBatch batch) {
+        super(batch);
+    }
+
+    @Override
+    public Collection<CustomField> getCustomFields(Map<String, CustomFieldDefinition> submissionFields) {
+        return null;
+    }
+
+    @Override
+    public String generateDescription() {
+        return null;
+    }
+
+    public String getSummary() {
+        StringBuilder summary = new StringBuilder();
+        summary.append("Denature Tube ");
+        //get denature tube from batch summary.append(batch);
+        return summary.toString();
+    }
+}
