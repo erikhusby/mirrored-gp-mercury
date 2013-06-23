262d2655c2b0755b923b3f9bee70abc745e35faf
hrafal <hrafal@broadinstitute.org>
2013-06-23 00:38:24 -0400

    GPLIM-1516: Shrink a few methods

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/billing/BillingTrackerProcessor.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/billing/BillingTrackerProcessor.java
@@ -221,23 +221,7 @@ public class BillingTrackerProcessor extends TableProcessor {
             // Check the latest date on the sample and make sure it is not after the date on the data row.
             Date uploadedTimestamp = (!StringUtils.isBlank(autoLedgerString)) ? DateUtils.parseDate(autoLedgerString) : null;
             Date currentSampleTimestamp = productOrderSample.getLatestAutoLedgerTimestamp();
-
-            boolean timestampsAreEqual;
-            if (doPersist) {
-                // If persisting, then we have cleared out the ledger entries, so there will be no timestamp to compare
-                // and validation has already been done, so the timestamp can be considered equal.
-                timestampsAreEqual = true;
-            } else if ((uploadedTimestamp == null) || (currentSampleTimestamp == null)) {
-                // If either are null, both must be null.
-                timestampsAreEqual = uploadedTimestamp == currentSampleTimestamp;
-            } else {
-                // The start of day values of the timestamps must be equal.
-                uploadedTimestamp = DateUtils.getStartOfDay(uploadedTimestamp);
-                currentSampleTimestamp = DateUtils.getStartOfDay(currentSampleTimestamp);
-                timestampsAreEqual = uploadedTimestamp.equals(currentSampleTimestamp);
-            }
-
-            if (!timestampsAreEqual) {
+            if (!areTimestampsEqual(uploadedTimestamp, currentSampleTimestamp)) {
                 String error = "Sample " + currentSampleName + " was auto billed after this tracker was downloaded. " +
                                "Download the tracker again to validate " + productOrderSample.getSampleName();
                 addDataMessage(error, dataRowIndex);
@@ -245,6 +229,32 @@ public class BillingTrackerProcessor extends TableProcessor {
         } catch (ParseException ex) {
             addDataMessage(ex.getMessage(), dataRowIndex);
         }
+    }
+
+    /**
+     * Check the spreadsheet timestamp against the ledger's timestamp.
+     *
+     * @param uploadedTimestamp The spreadsheet timestamp
+     * @param currentSampleTimestamp The ledger timestamp on the product order sample
+     *
+     * @return Are they 'equal' by rules set in the if-else logic documented here.
+     */
+    private boolean areTimestampsEqual(Date uploadedTimestamp, Date currentSampleTimestamp) {
+        boolean timestampsAreEqual;
+        if (doPersist) {
+            // If persisting, then we have cleared out the ledger entries, so there will be no timestamp to compare
+            // and validation has already been done, so the timestamp can be considered equal.
+            timestampsAreEqual = true;
+        } else if ((uploadedTimestamp == null) || (currentSampleTimestamp == null)) {
+            // If either are null, both must be null.
+            timestampsAreEqual = uploadedTimestamp == currentSampleTimestamp;
+        } else {
+            // The start of day values of the timestamps must be equal.
+            uploadedTimestamp = DateUtils.getStartOfDay(uploadedTimestamp);
+            currentSampleTimestamp = DateUtils.getStartOfDay(currentSampleTimestamp);
+            timestampsAreEqual = uploadedTimestamp.equals(currentSampleTimestamp);
+        }
+        return timestampsAreEqual;
     }
 
     private void addSummaryToChargesMap(int dataRowIndex, String rowPdoIdStr) {
@@ -276,6 +286,7 @@ public class BillingTrackerProcessor extends TableProcessor {
         }
     }
 
+    // Error messages.
     private static final String BILLED_IS_SAME =
             "Found a different billed quantity '%f' in the database for sample in %s in %s, price item '%s'. " +
             "The billed quantity in the spreadsheet is '%f', please download a recent copy of the " +
@@ -293,6 +304,12 @@ public class BillingTrackerProcessor extends TableProcessor {
             "Found quote ID ''{0}'' for updated sample ''{1}'' in ''{2}'', this differs from quote ''{4}'' " +
             "currently associated with ''{2}''.";
 
+    private static final String BAD_UPDATE_FILE =
+            "Found empty %s value for updated sample %s in %s, price item '%s'";
+
+    private static final String NEGATIVE_VALUE =
+            "Found negative new quantity '%f' for sample %s in %s, price item '%s'";
+
     private void parseRowForSummaryMap(Map<String, String> dataRow, int dataRowIndex, String rowPdoIdStr) {
         ProductOrderSample productOrderSample = currentSamples.get(sampleIndexInOrder);
 
@@ -304,7 +321,6 @@ public class BillingTrackerProcessor extends TableProcessor {
         for (BillableRef billableRef : currentBillableRefs) {
             PriceItem priceItem = currentPriceItemsByName.get(billableRef.getPriceItemName());
 
-            double newQuantity;
             double trackerBilled = 0;
 
             String billedKey = BillingTrackerHeader.getPriceItemHeader(billableRef) + " " + BillingTrackerHeader.BILLED;
@@ -332,67 +348,73 @@ public class BillingTrackerProcessor extends TableProcessor {
                 }
             }
 
-            String updateToKey = BillingTrackerHeader.getPriceItemHeader(billableRef) + " " +
-                              BillingTrackerHeader.UPDATE;
-            String updateTo = dataRow.get(updateToKey);
+            doUpdate(dataRow, dataRowIndex, productOrderSample, pdoSummaryStatsMap, billableRef, priceItem,
+                    trackerBilled, priceItemName);
+        }
+    }
 
-            if (!StringUtils.isBlank(updateTo)) {
-                newQuantity = Double.parseDouble(updateTo);
-                if (newQuantity < 0) {
-                    addDataMessage(String.format(
-                            "Found negative new quantity '%f' for sample %s in %s, price item '%s'",
-                            newQuantity, productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(),
-                            priceItemName), dataRowIndex);
+    private void doUpdate(Map<String, String> dataRow, int dataRowIndex, ProductOrderSample productOrderSample,
+                          Map<BillableRef, OrderBillSummaryStat> pdoSummaryStatsMap, BillableRef billableRef,
+                          PriceItem priceItem, double trackerBilled, String priceItemName) {
+
+        String updateToKey =
+                BillingTrackerHeader.getPriceItemHeader(billableRef) + " " + BillingTrackerHeader.UPDATE;
+        String updateTo = dataRow.get(updateToKey);
+
+        if (!StringUtils.isBlank(updateTo)) {
+            double newQuantity;
+            newQuantity = Double.parseDouble(updateTo);
+            if (newQuantity < 0) {
+                addDataMessage(String.format(NEGATIVE_VALUE, newQuantity, productOrderSample.getSampleKey(),
+                        currentProductOrder.getBusinessKey(), priceItemName), dataRowIndex);
+            }
+
+            double delta = newQuantity - trackerBilled;
+
+            if (delta != 0) {
+                String uploadedQuoteId = dataRow.get(BillingTrackerHeader.QUOTE_ID_HEADING.getText());
+                if (StringUtils.isBlank(uploadedQuoteId)) {
+                    addDataMessage(
+                            String.format(SAMPLE_EMPTY_VALUE, BillingTrackerHeader.QUOTE_ID_HEADING.getText(),
+                                    productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(),
+                                    priceItemName), dataRowIndex);
                 }
 
-                double delta = newQuantity - trackerBilled;
+                if (!productOrderSample.getProductOrder().getQuoteId().equals(uploadedQuoteId)) {
+                    addDataMessage(MessageFormat.format(QUOTE_MISMATCH, uploadedQuoteId,
+                            productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(),
+                            currentProduct.getPartNumber(), productOrderSample.getProductOrder().getQuoteId()),
+                            dataRowIndex);
+                }
 
-                if (delta != 0) {
-                    String uploadedQuoteId = dataRow.get(BillingTrackerHeader.QUOTE_ID_HEADING.getText());
-                    if (StringUtils.isBlank(uploadedQuoteId)) {
-                        addDataMessage(
-                                String.format(SAMPLE_EMPTY_VALUE, BillingTrackerHeader.QUOTE_ID_HEADING.getText(),
-                                        productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(),
-                                        priceItemName), dataRowIndex);
+                String workCompleteDateString = dataRow.get(BillingTrackerHeader.WORK_COMPLETE_DATE_HEADING.getText());
+                if (StringUtils.isBlank(workCompleteDateString)) {
+                    addDataMessage(String.format(BAD_UPDATE_FILE,
+                            BillingTrackerHeader.WORK_COMPLETE_DATE_HEADING.getText(),
+                            productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(), priceItemName),
+                            dataRowIndex);
+                }
+
+                OrderBillSummaryStat orderBillSummaryStat = pdoSummaryStatsMap.get(billableRef);
+                if (orderBillSummaryStat == null) {
+                    // Create a new stat obj and add it to the map.
+                    orderBillSummaryStat = new OrderBillSummaryStat();
+                    pdoSummaryStatsMap.put(billableRef, orderBillSummaryStat);
+                }
+
+                orderBillSummaryStat.applyDelta(delta);
+
+                try {
+                    // If there are no messages AND we are persisting, the do the persist.
+                    if (CollectionUtils.isEmpty(getMessages()) && doPersist) {
+                        Date workCompleteDate = DateUtils.parseDate(workCompleteDateString);
+                        productOrderSample.addLedgerItem(workCompleteDate, priceItem, delta);
                     }
-
-                    if (!productOrderSample.getProductOrder().getQuoteId().equals(uploadedQuoteId)) {
-                        addDataMessage(MessageFormat
-                                .format(QUOTE_MISMATCH, uploadedQuoteId, productOrderSample.getSampleKey(),
-                                        currentProductOrder.getBusinessKey(), currentProduct.getPartNumber(),
-                                        productOrderSample.getProductOrder().getQuoteId()), dataRowIndex);
-                    }
-
-                    String workCompleteDateString = dataRow.get(BillingTrackerHeader.WORK_COMPLETE_DATE_HEADING.getText());
-                    if (StringUtils.isBlank(workCompleteDateString)) {
-                        addDataMessage(String.format(
-                                "Found empty %s value for updated sample %s in %s, price item '%s'",
-                                BillingTrackerHeader.WORK_COMPLETE_DATE_HEADING.getText(),
-                                productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(), priceItemName),
-                                dataRowIndex);
-                    }
-
-                    OrderBillSummaryStat orderBillSummaryStat = pdoSummaryStatsMap.get(billableRef);
-                    if (orderBillSummaryStat == null) {
-                        // Create a new stat obj and add it to the map.
-                        orderBillSummaryStat = new OrderBillSummaryStat();
-                        pdoSummaryStatsMap.put(billableRef, orderBillSummaryStat);
-                    }
-
-                    orderBillSummaryStat.applyDelta(delta);
-
-                    try {
-                        // If there are no messages AND we are persisting, the do the persist.
-                        if (CollectionUtils.isEmpty(getMessages()) && doPersist) {
-                            Date workCompleteDate = DateUtils.parseDate(workCompleteDateString);
-                            productOrderSample.addLedgerItem(workCompleteDate, priceItem, delta);
-                        }
-                    } catch (ParseException e) {
-                        addDataMessage(MessageFormat.format(
-                                "Could not persist ledger for updated sample ''{0}'' in PDO ''{1}'' because of " +
-                                "error: {4}", productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(),
-                                currentProduct.getPartNumber(), e.getMessage()), dataRowIndex);
-                    }
+                } catch (ParseException e) {
+                    addDataMessage(MessageFormat.format(
+                            "Could not persist ledger for updated sample ''{0}'' in PDO ''{1}'' because of " +
+                            "error: {4}", productOrderSample.getSampleKey(), currentProductOrder.getBusinessKey(),
+                            currentProduct.getPartNumber(), e.getMessage()), dataRowIndex);
                 }
             }
         }
