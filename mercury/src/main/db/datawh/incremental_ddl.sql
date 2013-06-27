a23f4bca762bdc23ef00c1546a3311d01c1dee27
Phil Shapiro <pshapiro@broadinstitute.org>
2013-06-27 10:21:58 -0400

    GPLIM-1399: Add ETL for additional LedgerEntry fields

--- mercury/src/main/db/datawh/incremental_ddl.sql
+++ mercury/src/main/db/datawh/incremental_ddl.sql
@@ -1,3 +1,10 @@
 -------------------------------------------------------
 -- For release 1.27
 -------------------------------------------------------
+
+ALTER TABLE im_ledger_entry add  price_item_id           NUMERIC(19);
+ALTER TABLE im_ledger_entry add  price_item_type         VARCHAR2(50);
+ALTER TABLE im_ledger_entry add  quantity                NUMBER(15,4);
+ALTER TABLE im_ledger_entry add  billing_session_id      NUMERIC(19);
+ALTER TABLE im_ledger_entry add  billing_message         VARCHAR2(500);
+ALTER TABLE im_ledger_entry add  work_complete_date      DATE;
