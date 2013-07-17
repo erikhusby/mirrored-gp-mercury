68ad37380eaebeb3f833312ebd4a1cf27a99aa7d
Scott Matthews <scottmat@broadinstitute.org>
2013-07-17 17:01:56 -0400

    GPLIM-1807:  Added routing test for each of the newly entered effective versions.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/workflow/WorkflowValidator.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/workflow/WorkflowValidator.java
@@ -215,7 +215,12 @@ public class WorkflowValidator {
             for (SampleInstance sampleInstance : sampleInstances) {
                 String workflowName = sampleInstance.getWorkflowName();
                 LabBatch effectiveBatch = sampleInstance.getLabBatch();
-                if (workflowName != null) {
+
+                /*
+                    Not necessarily an ideal solution but validation should not necessarily cause a Null pointer which
+                    is what will happen if the batch is not found (Sample exists in multiple batches)
+                 */
+                if (workflowName != null && effectiveBatch != null) {
                     ProductWorkflowDefVersion workflowVersion = workflowLoader.load().getWorkflowByName(workflowName).
                             getEffectiveVersion(effectiveBatch.getCreatedOn());
                     if (workflowVersion != null) {
