2efc49f25eb113914f4c52edc5ff78dcaccfb2ac
Eliot Polk <epolk@broadinstitute.org>
2013-08-22 09:14:38 -0400

    GPLIM-1 reset warehouse incremental ddl

--- mercury/src/main/db/datawh/incremental_ddl.sql
+++ mercury/src/main/db/datawh/incremental_ddl.sql
@@ -1,5 +1,3 @@
 -------------------------------------------------------
--- For release 1.30
+-- For release 1.31
 -------------------------------------------------------
-alter table lab_metric add vessel_position  VARCHAR2(255);
-alter table im_lab_metric add vessel_position  VARCHAR2(255);
