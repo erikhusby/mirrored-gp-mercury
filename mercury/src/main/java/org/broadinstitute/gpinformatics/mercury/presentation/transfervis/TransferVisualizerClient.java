6ce3e04bccbe94517e321636d8645e48f98afca8
hrafal <hrafal@broadinstitute.org>
2013-08-26 16:21:40 -0400

    GPLIM-1902: Reformatted the code so I can read through it a bit.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/transfervis/TransferVisualizerClient.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/transfervis/TransferVisualizerClient.java
@@ -68,7 +68,9 @@ public class TransferVisualizerClient {
         return mxGraph;
     }
 
-    /** Which entity the user wants to search on */
+    /**
+     * Which entity the user wants to search on
+     */
     public enum SearchType {
         SEARCH_PLATE("Plate Barcode"),
         SEARCH_TUBE("Tube Barcode"),
@@ -88,22 +90,30 @@ public class TransferVisualizerClient {
         }
     }
 
-    public TransferVisualizerClient(String highlightBarcode, List<TransferVisualizer.AlternativeId> alternativeDisplayIds) {
+    public TransferVisualizerClient(String highlightBarcode,
+                                    List<TransferVisualizer.AlternativeId> alternativeDisplayIds) {
         createMxGraph();
         this.highlightBarcode = highlightBarcode;
         this.alternativeDisplayIds = alternativeDisplayIds;
     }
 
-    /** Implemented by vertices that do something when the user clicks on them */
+    /**
+     * Implemented by vertices that do something when the user clicks on them
+     */
     public interface HandlesClicks {
         String handleClick();
+
         boolean scrollTo();
+
         boolean deleteCell();
     }
 
-    /** Implemented by vertices that do something when the user right-clicks on them */
+    /**
+     * Implemented by vertices that do something when the user right-clicks on them
+     */
     public interface HandlesPopups {
         List<String> getPopupList();
+
         String handlePopup(String name);
     }
 
@@ -125,9 +135,9 @@ public class TransferVisualizerClient {
             try {
                 String tubeBarcode = null;
                 // todo jmt fold this into the enum
-                if(vertex.getIdType().equals(TransferVisualizer.IdType.TUBE_IN_RACK_ID_TYPE.toString())) {
+                if (vertex.getIdType().equals(TransferVisualizer.IdType.TUBE_IN_RACK_ID_TYPE.toString())) {
                     tubeBarcode = vertex.getTitle();
-                } else if(vertex.getIdType().equals(TransferVisualizer.IdType.RECEPTACLE_ID_TYPE.toString())) {
+                } else if (vertex.getIdType().equals(TransferVisualizer.IdType.RECEPTACLE_ID_TYPE.toString())) {
                     tubeBarcode = vertex.getId();
                 }
                 if (tubeBarcode != null) {
@@ -164,7 +174,7 @@ public class TransferVisualizerClient {
         public List<String> getPopupList() {
             List<String> popupList = new ArrayList<>();
             popupList.add(CellValue.COPY_BARCODE);
-            if(vertex.getChildVertices() != null) {
+            if (vertex.getChildVertices() != null) {
                 popupList.add(CellValue.COPY_TUBE_BARCODES);
             }
             return popupList;
@@ -200,7 +210,7 @@ public class TransferVisualizerClient {
                 clipboardService.setContents(contents);
             } catch (UnavailableServiceException e) {
 */
-                // Assume we're running as a non-JNLP application
+            // Assume we're running as a non-JNLP application
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(contents, contents);
 /*
@@ -220,7 +230,7 @@ public class TransferVisualizerClient {
                 stringBuilder.append("<html>");
                 stringBuilder.append(vertex.getTitle());
                 stringBuilder.append("<br/>");
-                for (Map.Entry<String, List<String>> entry: vertex.getAlternativeIds().entrySet()){
+                for (Map.Entry<String, List<String>> entry : vertex.getAlternativeIds().entrySet()) {
                     for (String id : entry.getValue()) {
                         stringBuilder.append(id);
                         stringBuilder.append("<br/>");
@@ -233,7 +243,9 @@ public class TransferVisualizerClient {
         }
     }
 
-    /** Contains a port for attaching edges.  Edges attached directly to child cells don't get laid out correctly. */
+    /**
+     * Contains a port for attaching edges.  Edges attached directly to child cells don't get laid out correctly.
+     */
     public static class ChildCellValue extends CellValue {
         private mxCell port;
         private static final long serialVersionUID = 20101104L;
@@ -248,7 +260,9 @@ public class TransferVisualizerClient {
         }
     }
 
-    /** A box that the user can click, to see more details about a rack or plate */
+    /**
+     * A box that the user can click, to see more details about a rack or plate
+     */
     public static class MoreDetails implements HandlesClicks {
 
         private Vertex vertex;
@@ -283,7 +297,9 @@ public class TransferVisualizerClient {
         }
     }
 
-    /** A box that the user can click to see more transfers */
+    /**
+     * A box that the user can click to see more transfers
+     */
     public class MoreTransfers implements HandlesClicks {
 
         private Vertex vertex;
@@ -329,7 +345,7 @@ public class TransferVisualizerClient {
 
     public void renderGraph() {
         for (Vertex vertex : graph.getMapIdToVertex().values()) {
-            if(vertex.getParentVertex() == null) {
+            if (vertex.getParentVertex() == null) {
                 renderVertex(vertex);
             }
         }
@@ -341,30 +357,36 @@ public class TransferVisualizerClient {
     private void renderEdge(Edge edge) {
         if (!edge.hasBeenRendered()) {
             mxCell sourceReceptacleCell = mapIdToMxVertex.get(edge.getSource().getId());
-            if(sourceReceptacleCell == null) {
+            if (sourceReceptacleCell == null) {
                 renderVertex(graph.getMapIdToVertex().get(edge.getSource().getId()));
                 sourceReceptacleCell = mapIdToMxVertex.get(edge.getSource().getId());
             }
+
             mxCell destinationReceptacleCell = mapIdToMxVertex.get(edge.getDestination().getId());
-            if(destinationReceptacleCell == null) {
+            if (destinationReceptacleCell == null) {
                 renderVertex(graph.getMapIdToVertex().get(edge.getDestination().getId()));
                 destinationReceptacleCell = mapIdToMxVertex.get(edge.getDestination().getId());
             }
+
             if (sourceReceptacleCell.getValue() instanceof ChildCellValue) {
                 sourceReceptacleCell = ((ChildCellValue) sourceReceptacleCell.getValue()).getPort();
             }
+
             if (destinationReceptacleCell.getValue() instanceof ChildCellValue) {
                 destinationReceptacleCell = ((ChildCellValue) destinationReceptacleCell.getValue()).getPort();
             }
+
             String label = edge.getLabel();
             // If the edge has both ends in child cells (e.g. cherry picks)
-            if(!sourceReceptacleCell.getParent().equals(mxGraph.getDefaultParent()) && !destinationReceptacleCell.getParent().equals(mxGraph.getDefaultParent())) {
+            if (!sourceReceptacleCell.getParent().equals(mxGraph.getDefaultParent()) && !destinationReceptacleCell
+                    .getParent().equals(mxGraph.getDefaultParent())) {
                 int sourceParentEdgeCount = sourceReceptacleCell.getParent().getEdgeCount();
                 // If we find another edge to the same destination parent, and it already has a label, don't label the
                 // current edge, because it would be very cluttered if all edges where labeled
-                for(int i = 0; i < sourceParentEdgeCount; i++) {
+                for (int i = 0; i < sourceParentEdgeCount; i++) {
                     mxCell edgeCell = (mxCell) sourceReceptacleCell.getParent().getEdgeAt(i);
-                    if(edgeCell.getTarget().equals(destinationReceptacleCell.getParent()) && edgeCell.getValue() != null) {
+                    if (edgeCell.getTarget().equals(destinationReceptacleCell.getParent())
+                        && edgeCell.getValue() != null) {
                         label = null;
                         break;
                     }
@@ -381,6 +403,7 @@ public class TransferVisualizerClient {
 
     /**
      * Insert a vertex for a plate, or a rack and tubes
+     *
      * @param vertex barcode and transfers
      */
     void renderVertex(Vertex vertex) {
@@ -390,22 +413,25 @@ public class TransferVisualizerClient {
             // todo jmt highlight GSSR barcodes and library names
             mxCell mxVertex = (mxCell) mxGraph.insertVertex(mxGraph.getDefaultParent(), null,
                     new CellValue(vertex), 0.0, 0.0, 160.0, 40.0,
-                    (vertex.getTitle().contains(highlightBarcode) ? "fillColor=#FFFF00;fontColor=#000000;" : "fillColor=#FFFFFF;fontColor=#000000;") +
-                            mxConstants.STYLE_VERTICAL_ALIGN + "=" + mxConstants.ALIGN_TOP +
-                            mxConstants.STYLE_VERTICAL_LABEL_POSITION + "=" + mxConstants.ALIGN_TOP);
+                    (vertex.getTitle().contains(highlightBarcode) ? "fillColor=#FFFF00;fontColor=#000000;" :
+                            "fillColor=#FFFFFF;fontColor=#000000;") +
+                    mxConstants.STYLE_VERTICAL_ALIGN + "=" + mxConstants.ALIGN_TOP +
+                    mxConstants.STYLE_VERTICAL_LABEL_POSITION + "=" + mxConstants.ALIGN_TOP);
             mxVertex.setConnectable(false);
             mxGraph.updateCellSize(mxVertex);
-            if(vertex.getTitle().contains(highlightBarcode)) {
+            if (vertex.getTitle().contains(highlightBarcode)) {
                 highlightVertex = mxVertex;
             }
+
             double rackHeight = mxVertex.getGeometry().getHeight();
-            if(vertex.getChildVertices() != null) {
+            if (vertex.getChildVertices() != null) {
                 mxCell[][] childCells = insertChildCells(vertex, mxVertex);
                 double maxChildCellHeight = arrangeChildCells(vertex, mxVertex, rackHeight, childCells);
                 rackHeight += maxChildCellHeight * (double) childCells.length;
             }
+
             // If there are self-referential plate events, draw a box for the user to click on, to see a list of these events
-            if(vertex.getDetails() != null && !vertex.getDetails().isEmpty()) {
+            if (vertex.getDetails() != null && !vertex.getDetails().isEmpty()) {
                 mxGraph.insertVertex(
                         mxVertex,
                         null,
@@ -413,8 +439,9 @@ public class TransferVisualizerClient {
                         0.0, rackHeight, mxVertex.getGeometry().getWidth(), (double) BUTTON_HEIGHT);
                 rackHeight += (double) BUTTON_HEIGHT;
             }
+
             // If there are more transfers, draw a box for the user to click on, to see the transfers
-            if(vertex.getHasMoreEdges()) {
+            if (vertex.getHasMoreEdges()) {
                 mxGraph.insertVertex(
                         mxVertex,
                         null,
@@ -430,16 +457,18 @@ public class TransferVisualizerClient {
 
     /**
      * Insert child cells, and resize, so they fit their titles
-     * @param vertex rack
+     *
+     * @param vertex   rack
      * @param mxVertex JGraph vertex
+     *
      * @return array of JGraph child cells
      */
     private mxCell[][] insertChildCells(Vertex vertex, mxCell mxVertex) {
         int numChildRows = vertex.getChildVertices().length;
         mxCell[][] childCells = new mxCell[vertex.getChildVertices().length][vertex.getChildVertices()[0].length];
-        for(int rowIndex = 0; rowIndex < numChildRows; rowIndex++) {
+        for (int rowIndex = 0; rowIndex < numChildRows; rowIndex++) {
             Vertex[] row = vertex.getChildVertices()[rowIndex];
-            for(int columnIndex = 0; columnIndex < row.length; columnIndex++) {
+            for (int columnIndex = 0; columnIndex < row.length; columnIndex++) {
                 Vertex childVertex = row[columnIndex];
                 if (childVertex != null) {
                     // The layout algorithm doesn't work when routing an edge between a nested cell (tube in
@@ -460,7 +489,7 @@ public class TransferVisualizerClient {
                     childMxVertex.setConnectable(false);
                     childCells[rowIndex][columnIndex] = childMxVertex;
                     mxGraph.updateCellSize(childMxVertex);
-                    if(childVertex.getTitle().contains(highlightBarcode)) {
+                    if (childVertex.getTitle().contains(highlightBarcode)) {
                         highlightVertex = childMxVertex;
                     }
 
@@ -473,10 +502,12 @@ public class TransferVisualizerClient {
 
     /**
      * arrange the resized cells in uniform rows and columns
-     * @param vertex rack
-     * @param mxVertex JGraph vertex
+     *
+     * @param vertex           rack
+     * @param mxVertex         JGraph vertex
      * @param rackHeaderHeight the height of the rack title bar
-     * @param childCells array of JGraph child vertices
+     * @param childCells       array of JGraph child vertices
+     *
      * @return the height of the tallest child cell
      */
     private double arrangeChildCells(Vertex vertex, mxCell mxVertex, double rackHeaderHeight, mxCell[][] childCells) {
@@ -491,16 +522,20 @@ public class TransferVisualizerClient {
                 }
             }
         }
+
         double totalCellsHeight = maxChildCellHeight * (double) childCells.length;
         double totalY = rackHeaderHeight + totalCellsHeight;
-        if(vertex.getDetails() != null && !vertex.getDetails().isEmpty()) {
+
+        if (vertex.getDetails() != null && !vertex.getDetails().isEmpty()) {
             totalY += (double) BUTTON_HEIGHT;
         }
-        if(vertex.getHasMoreEdges()) {
+
+        if (vertex.getHasMoreEdges()) {
             totalY += (double) BUTTON_HEIGHT;
         }
-        for(int rowIndex = 0; rowIndex < childCells.length; rowIndex++) {
-            for(int columnIndex = 0; columnIndex < childCells[0].length; columnIndex++) {
+
+        for (int rowIndex = 0; rowIndex < childCells.length; rowIndex++) {
+            for (int columnIndex = 0; columnIndex < childCells[0].length; columnIndex++) {
                 mxCell mxCell = childCells[rowIndex][columnIndex];
                 if (mxCell != null) {
                     mxGeometry mxGeometry = mxCell.getGeometry();
@@ -511,13 +546,15 @@ public class TransferVisualizerClient {
 
                     // Move the edge attachment port to the left of the cell
                     mxGeometry portGeometry = ((ChildCellValue) mxCell.getValue()).getPort().getGeometry();
-                    portGeometry.setX((maxChildCellWidth * (double) columnIndex) / (maxChildCellWidth * (double) numColumns));
+                    portGeometry.setX((maxChildCellWidth * (double) columnIndex) / (maxChildCellWidth
+                                                                                    * (double) numColumns));
                     double yRatio = (maxChildCellHeight * (double) rowIndex) / totalCellsHeight;
                     portGeometry.setY((yRatio * totalCellsHeight / totalY) + (rackHeaderHeight / totalY));
                 }
             }
         }
-        mxVertex.getGeometry().setWidth(Math.max(maxChildCellWidth * (double) numColumns, mxVertex.getGeometry().getWidth()));
+        mxVertex.getGeometry()
+                .setWidth(Math.max(maxChildCellWidth * (double) numColumns, mxVertex.getGeometry().getWidth()));
         return maxChildCellHeight;
     }
 
@@ -564,7 +601,9 @@ public class TransferVisualizerClient {
             // the remote view fully qualified class name
             final String viewClassName = TransferVisualizer.class.getName();
             // let's do the lookup (notice the ?stateful string as the last part of the jndi name for stateful bean lookup)
-            String name = "ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName + "?stateful";
+            String name =
+                    "ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName
+                    + "?stateful";
             return (TransferVisualizer) context.lookup(name);
         } catch (NamingException e) {
             throw new RuntimeException(e);
@@ -574,14 +613,14 @@ public class TransferVisualizerClient {
     public void renderAndLayoutGraph(Graph graph) {
         this.graph = graph;
         mxGraph.getModel().beginUpdate();
-        try{
+        try {
             mxGraph.selectAll();
             mxGraph.removeCells();
         } finally {
             mxGraph.getModel().endUpdate();
         }
         mxGraph.getModel().beginUpdate();
-        try{
+        try {
             renderGraph();
             layout();
         } finally {
@@ -589,27 +628,28 @@ public class TransferVisualizerClient {
         }
     }
 
-    public Graph fetchGraph(String barcode, SearchType searchType, List<TransferVisualizer.AlternativeId> alternativeIds) throws RemoteException {
+    public Graph fetchGraph(String barcode, SearchType searchType,
+                            List<TransferVisualizer.AlternativeId> alternativeIds) throws RemoteException {
         TransferVisualizer transferVisualizer = getServer();
         // todo jmt fold this code into the enum
-        switch(searchType) {
-            case SEARCH_PLATE:
-                graph = transferVisualizer.forPlate(barcode, alternativeIds);
-                break;
-            case SEARCH_TUBE:
-                graph = transferVisualizer.forTube(barcode, alternativeIds);
-                break;
-            case SEARCH_CONTAINER:
-                graph = transferVisualizer.forContainer(barcode, alternativeIds);
-                break;
+        switch (searchType) {
+        case SEARCH_PLATE:
+            graph = transferVisualizer.forPlate(barcode, alternativeIds);
+            break;
+        case SEARCH_TUBE:
+            graph = transferVisualizer.forTube(barcode, alternativeIds);
+            break;
+        case SEARCH_CONTAINER:
+            graph = transferVisualizer.forContainer(barcode, alternativeIds);
+            break;
 //            case SEARCH_GSSR_SAMPLE:
 //                graph = transferVisualizer.forGssrBarcode(barcode, alternativeIds);
 //                break;
 //            case SEARCH_LIBRARY_NAME:
 //                graph = transferVisualizer.forLibraryName(barcode, alternativeIds);
 //                break;
-            default:
-                throw new RuntimeException("Unknown searchType " + searchType);
+        default:
+            throw new RuntimeException("Unknown searchType " + searchType);
         }
         return graph;
     }
