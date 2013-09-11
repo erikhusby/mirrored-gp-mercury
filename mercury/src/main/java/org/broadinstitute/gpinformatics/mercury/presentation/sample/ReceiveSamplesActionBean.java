e94d3a08e2598a0ad51a84690e1b06c11ed5b1ea
pdunlea <pdunlea@broadinstitute.org>
2013-09-11 16:36:53 -0400

    GPLIM-2030 ActionBeans which will be utilized

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/sample/ReceiveSamplesActionBean.java
@@ -0,0 +1,48 @@
+package org.broadinstitute.gpinformatics.mercury.presentation.sample;
+
+import net.sourceforge.stripes.action.DefaultHandler;
+import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.HandlesEvent;
+import net.sourceforge.stripes.action.Resolution;
+import net.sourceforge.stripes.action.UrlBinding;
+import org.broadinstitute.gpinformatics.mercury.boundary.sample.ReceiveSamplesEjb;
+import org.broadinstitute.gpinformatics.mercury.presentation.CoreActionBean;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@UrlBinding(ReceiveSamplesActionBean.URL)
+public class ReceiveSamplesActionBean extends CoreActionBean {
+
+    public static final String URL = "/sample/receiveSamples.action";
+    public static final String SHOW_RECEIPT_JSP = "/sample/sample_receipt.jsp";
+    public static final String SHOW_RECEIPT_ACTION = "showRecept";
+    public static final String RECEIVE_SAMPLES_ACTION = "receiveSamples";
+
+    private List<String> samplesToReceive;
+
+    @Inject
+    private ReceiveSamplesEjb receiveSamplesEjb;
+
+    @DefaultHandler
+    @HandlesEvent(SHOW_RECEIPT_ACTION)
+    public Resolution showReceipt() {
+        return new ForwardResolution(SHOW_RECEIPT_JSP);
+    }
+
+    @HandlesEvent(RECEIVE_SAMPLES_ACTION)
+    public Resolution receiveSamples() {
+
+
+
+        return showReceipt();
+    }
+
+    public List<String> getSamplesToReceive() {
+        return samplesToReceive;
+    }
+
+    public void setSamplesToReceive(List<String> samplesToReceive) {
+        this.samplesToReceive = samplesToReceive;
+    }
+}
---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/vessel/RackScanActionBean.java
@@ -0,0 +1,12 @@
+package org.broadinstitute.gpinformatics.mercury.presentation.vessel;
+
+import net.sourceforge.stripes.action.UrlBinding;
+import org.broadinstitute.gpinformatics.mercury.presentation.CoreActionBean;
+
+@UrlBinding(RackScanActionBean.URL)
+public class RackScanActionBean extends CoreActionBean {
+
+    public static final String URL = "/view/rackScan.action";
+
+
+}
