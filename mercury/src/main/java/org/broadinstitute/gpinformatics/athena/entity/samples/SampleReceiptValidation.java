86e6c4d01305113f7f023ba17c33c2b180897f3d
Scott Matthews <scottmat@broadinstitute.org>
2013-09-16 16:27:34 -0400

    GPLIM-2036: Code Review changes for GPI-455

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/samples/SampleReceiptValidation.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/samples/SampleReceiptValidation.java
@@ -26,62 +26,29 @@ import java.util.Date;
 @Table(name = "sample_receipt_validation", schema = "athena")
 public class SampleReceiptValidation {
 
-    public enum SampleValidationReason {
-        SAMPLE_NOT_IN_BSP("The sample is not recognized in BSP"),
-        MISSING_SAMPLE_FROM_SAMPLE_KIT("Not all of the samples for the sample kit came back"),
-        SAMPLES_FROM_MULTIPLE_KITS("The samples being received span multiple sample kits");
-
-        private final String reasonMessage;
-
-        private SampleValidationReason(String reasonMessage) {
-            this.reasonMessage = reasonMessage;
-        }
-
-        public String getReasonMessage() {
-            return reasonMessage;
-        }
-    }
-
-    public enum SampleValidationType {
-        WARNING, BLOCKING;
-    }
-
-    public enum SampleValidationStatus {
-        PENDING, CLEARED;
-    }
-
     @Id
     @SequenceGenerator(name = "SEQ_SAMP_REC_VALIDATION", schema = "athena", sequenceName = "SEQ_SAMP_REC_VALIDATION")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_SAMP_REC_VALIDATION")
     private Long validationId;
-
     @ManyToOne(cascade = {CascadeType.PERSIST})
     private ProductOrderSample productOrderSample;
-
     @Column(name = "validation_type", length = 255, nullable = false)
     @Enumerated(EnumType.STRING)
     private SampleValidationType validationType;
-
     @Column(name = "status", length = 255, nullable = false)
     @Enumerated(EnumType.STRING)
     private SampleValidationStatus status;
-
     @Column(name = "CREATED_DATE", nullable = false)
     private Date createdDate;
-
     @Column(name = "CREATED_BY", nullable = false)
     private Long createdBy;
-
     @Column(name = "MODIFIED_DATE")
     private Date modifiedDate;
-
     @Column(name = "MODIFIED_BY")
     private long modifiedBy;
-
     @Column(name = "REASON", nullable = false)
     @Enumerated(EnumType.STRING)
     private SampleValidationReason reason;
-
 
     protected SampleReceiptValidation() {
     }
@@ -94,8 +61,9 @@ public class SampleReceiptValidation {
     public SampleReceiptValidation(ProductOrderSample productOrderSample, @Nonnull Long createdBy,
                                    @Nonnull SampleValidationType validationType,
                                    @Nonnull SampleValidationReason reason) {
-        this(productOrderSample,createdBy,SampleValidationStatus.PENDING,validationType, reason);
+        this(productOrderSample, createdBy, SampleValidationStatus.PENDING, validationType, reason);
     }
+
 
     public SampleReceiptValidation(ProductOrderSample productOrderSample, @Nonnull Long createdBy,
                                    @Nonnull SampleValidationStatus status,
@@ -118,28 +86,56 @@ public class SampleReceiptValidation {
         return productOrderSample;
     }
 
+    public void setProductOrderSample(ProductOrderSample productOrderSample) {
+        this.productOrderSample = productOrderSample;
+    }
+
     public SampleValidationType getValidationType() {
         return validationType;
+    }
+
+    public void setValidationType(SampleValidationType validationType) {
+        this.validationType = validationType;
     }
 
     public SampleValidationStatus getStatus() {
         return status;
     }
 
+    public void setStatus(SampleValidationStatus status) {
+        this.status = status;
+    }
+
     public Date getCreatedDate() {
         return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
     }
 
     public Long getCreatedBy() {
         return createdBy;
     }
 
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
     public Date getModifiedDate() {
         return modifiedDate;
     }
 
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
     public long getModifiedBy() {
         return modifiedBy;
+    }
+
+    public void setModifiedBy(long modifiedBy) {
+        this.modifiedBy = modifiedBy;
     }
 
     public SampleValidationReason getReason() {
@@ -150,31 +146,26 @@ public class SampleReceiptValidation {
         this.reason = reason;
     }
 
-    public void setProductOrderSample(ProductOrderSample productOrderSample) {
-        this.productOrderSample = productOrderSample;
+    public enum SampleValidationReason {
+        SAMPLE_NOT_IN_BSP("The sample is not recognized in BSP"),
+        MISSING_SAMPLE_FROM_SAMPLE_KIT("Not all of the samples for the sample kit came back"),
+        SAMPLES_FROM_MULTIPLE_KITS("The samples being received span multiple sample kits");
+        private final String reasonMessage;
+
+        private SampleValidationReason(String reasonMessage) {
+            this.reasonMessage = reasonMessage;
+        }
+
+        public String getReasonMessage() {
+            return reasonMessage;
+        }
     }
 
-    public void setValidationType(SampleValidationType validationType) {
-        this.validationType = validationType;
+    public enum SampleValidationType {
+        WARNING, BLOCKING;
     }
 
-    public void setStatus(SampleValidationStatus status) {
-        this.status = status;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public void setCreatedBy(Long createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public void setModifiedDate(Date modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public void setModifiedBy(long modifiedBy) {
-        this.modifiedBy = modifiedBy;
+    public enum SampleValidationStatus {
+        PENDING, CLEARED;
     }
 }
