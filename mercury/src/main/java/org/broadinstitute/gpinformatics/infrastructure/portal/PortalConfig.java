eef72f5f6acb0562fbd5259c22e2ac76f609e22a
Scott Matthews <scottmat@broadinstitute.org>
2013-10-09 16:55:03 -0400

    GPLIM-2062:  Calling Portal Clinician service to fill in Clinician Information.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/portal/PortalConfig.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/portal/PortalConfig.java
@@ -3,6 +3,7 @@ package org.broadinstitute.gpinformatics.infrastructure.portal;
 import org.broadinstitute.gpinformatics.infrastructure.deployment.AbstractConfig;
 import org.broadinstitute.gpinformatics.infrastructure.deployment.ConfigKey;
 import org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment;
+import org.broadinstitute.gpinformatics.mercury.control.LoginAndPassword;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
@@ -13,8 +14,17 @@ import java.io.Serializable;
  */
 @SuppressWarnings("UnusedDeclaration")
 @ConfigKey("portal")
-public class PortalConfig extends AbstractConfig implements Serializable {
-    private String urlBase;
+public class PortalConfig extends AbstractConfig  implements LoginAndPassword, Serializable  {
+
+    public static final String CRSP_PORTAL_NAME = "CRSP";
+    private String login;
+
+    private String password;
+
+    private String host;
+
+    private int port;
+
 
     @Inject
     public PortalConfig(@Nonnull Deployment deployment) {
@@ -22,14 +32,47 @@ public class PortalConfig extends AbstractConfig implements Serializable {
     }
 
     public String getUrlBase() {
-        return urlBase;
+        return String.format("http://%s:%d/portal", getHost(), getPort());
     }
 
-    public void setUrlBase(String urlBaseIn) {
-        urlBase = urlBaseIn;
+    public String getWsUrl(String portalName) {
+        return String.format("http://%s:%d/portal/%s/ws/portals/private/", getHost(), getPort(), portalName);
     }
+
 
     public static PortalConfig produce(Deployment deployment) {
         return produce(PortalConfig.class, deployment);
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
     }
 }
--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/sample/CrspInfoResource.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/sample/CrspInfoResource.java
@@ -2,9 +2,15 @@ package org.broadinstitute.gpinformatics.mercury.boundary.sample;
 
 import com.sun.jersey.api.client.Client;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.broadinstitute.gpinformatics.athena.control.dao.orders.ProductOrderDao;
+import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrder;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPConfig;
+import org.broadinstitute.gpinformatics.infrastructure.portal.PortalConfig;
 import org.broadinstitute.gpinformatics.mercury.boundary.ResourceException;
 import org.broadinstitute.gpinformatics.mercury.control.AbstractJerseyClientService;
+import org.broadinstitute.gpinformatics.mercury.control.LoginAndPassword;
 import org.broadinstitute.gpinformatics.mercury.crsp.generated.CrspPhiDTO;
 import org.broadinstitute.gpinformatics.mercury.crsp.generated.CrspPhiInfo;
 import org.xml.sax.InputSource;
@@ -12,9 +18,7 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
-import javax.annotation.security.DenyAll;
 import javax.annotation.security.PermitAll;
-import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -42,22 +46,60 @@ import java.util.Map;
 public class CrspInfoResource extends AbstractJerseyClientService {
 
     public static final String SEARCH_CRSP_PHI = "sample/getcrspphenotypes";
+    public static final String SEARCH_CRSP_CLINICIAN_INFO = "getclinicianinfo";
+
+    private static final Log logger = LogFactory.getLog(CrspInfoResource.class);
 
     @Inject
     private BSPConfig bspConfig;
+
+    @Inject
+    private PortalConfig portalConfig;
+
+    @Inject
+    private ProductOrderDao productOrderDao;
+
+
+    private LoginAndPassword currentConfig;
 
     @GET
     @Path("/getCrspPhiInfo")
 //    @RolesAllowed({"CRSP-Mercury-ProjectManagers", "CRSP-Mercury-Developers"})
     @PermitAll
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getCrspPhiInfo(@QueryParam("sampleIds") List<String> sampleIds) {
+    public Response getCrspPhiInfo(@QueryParam("sampleIds") List<String> sampleIds,
+                                   @QueryParam("reqId") String reqId) {
 
+        currentConfig = portalConfig;
         if (sampleIds == null || sampleIds.isEmpty()) {
             throw new ResourceException("At least one sample ID is needed", Response.Status.BAD_REQUEST);
         }
 
+        if(StringUtils.isBlank(reqId)) {
+            throw new ResourceException("A Requisition identifier is needed to find Clinician information",
+                    Response.Status.BAD_REQUEST);
+        }
+
+        ProductOrder order = productOrderDao.findByBusinessKey(reqId);
+
+        String clinicianQueryResponse =
+                getJerseyClient().resource(portalConfig.getWsUrl(PortalConfig.CRSP_PORTAL_NAME)+SEARCH_CRSP_CLINICIAN_INFO)
+                .queryParam("requisition_identifier", order.getRequisitionKey()).get(String.class);
+
+        CrspPhiInfo clinicianInfo = null;
+        try {
+            clinicianInfo = parseCrspPhiResponse(clinicianQueryResponse);
+        } catch (JAXBException | SAXException e) {
+            logger.error("Error parsing information retrieved for clinician information");
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+
+        CrspPhiDTO clinicianData = clinicianInfo.getPhiData().iterator().next();
+
         Map<String, CrspPhiDTO> resourceResults = new HashMap<>();
+
+        clearClient();
+        currentConfig = bspConfig;
 
         String queryResponse = getJerseyClient().resource(bspConfig.getWSUrl(SEARCH_CRSP_PHI))
                 .queryParam("sample_ids", StringUtils.join(sampleIds, ","))
@@ -65,34 +107,54 @@ public class CrspInfoResource extends AbstractJerseyClientService {
 
         Response foundResponse;
         try {
-            JAXBContext queryContext = JAXBContext.newInstance(CrspPhiInfo.class, CrspPhiDTO.class);
-            Unmarshaller unmarshaller = queryContext.createUnmarshaller();
-            XMLReader reader = XMLReaderFactory.createXMLReader();
-
-            //Prepare the input
-            InputSource is = new InputSource(new StringReader(queryResponse));
-
-            //Create a SAXSource specifying the filter
-            SAXSource source = new SAXSource(is);
-
-            CrspPhiInfo queryInfo = (CrspPhiInfo) unmarshaller.unmarshal(source);
+            CrspPhiInfo queryInfo = parseCrspPhiResponse(queryResponse);
 
             if (queryInfo != null) {
                 for (CrspPhiDTO phiDTO : queryInfo.getPhiData()) {
+
+                    phiDTO.setClinicianName(clinicianData.getClinicianName());
+
+                    phiDTO.setClinicianPhone(clinicianData.getClinicianPhone());
+                    phiDTO.setClinicianFax(clinicianData.getClinicianFax());
+                    phiDTO.setClinicianEmail(clinicianData.getClinicianEmail());
+
+                    phiDTO.setClinicianAddress1(clinicianData.getClinicianAddress1());
+                    phiDTO.setClinicianAddress2(clinicianData.getClinicianAddress2());
+                    phiDTO.setClinicianCity(clinicianData.getClinicianCity());
+                    phiDTO.setClinicianState(clinicianData.getClinicianState());
+                    phiDTO.setClinicianZipCode(clinicianData.getClinicianZipCode());
+                    phiDTO.setClinicianCountry(clinicianData.getClinicianCountry());
+
                     resourceResults.put(phiDTO.getSampleID(), phiDTO);
                 }
             }
-            foundResponse = Response.ok().entity(resourceResults).build();
         } catch (JAXBException | SAXException e) {
-            foundResponse = Response.status(Response.Status.BAD_REQUEST).build();
             e.printStackTrace();
+            logger.error("Error getting data from BSP call",e);
+            return Response.status(Response.Status.BAD_REQUEST).build();
         }
+
+        foundResponse = Response.ok().entity(resourceResults).build();
 
         return foundResponse;
     }
 
+    private CrspPhiInfo parseCrspPhiResponse(String queryResponse) throws JAXBException, SAXException {
+        JAXBContext queryContext = JAXBContext.newInstance(CrspPhiInfo.class, CrspPhiDTO.class);
+        Unmarshaller unmarshaller = queryContext.createUnmarshaller();
+        XMLReader reader = XMLReaderFactory.createXMLReader();
+
+        //Prepare the input
+        InputSource is = new InputSource(new StringReader(queryResponse));
+
+        //Create a SAXSource specifying the filter
+        SAXSource source = new SAXSource(is);
+
+        return (CrspPhiInfo) unmarshaller.unmarshal(source);
+    }
+
     @Override
     protected void customizeClient(Client client) {
-        specifyHttpAuthCredentials(client, bspConfig);
+        specifyHttpAuthCredentials(client, currentConfig);
     }
 }
