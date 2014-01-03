41dfd8257008502ee293fe6977a1a1c0511d2c4d
andrew <andrew@broadinstitute.org>
2014-01-03 13:30:19 -0500

    https://gpinfojira.broadinstitute.org:8443/jira/browse/BSP-811.  Cleaned up integration test.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/athena/boundary/orders/PDOSampleBillingStatusResourceTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/boundary/orders/PDOSampleBillingStatusResourceTest.java
@@ -1,12 +1,7 @@
 package org.broadinstitute.gpinformatics.athena.boundary.orders;
 
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.json.JSONConfiguration;
-import org.broadinstitute.gpinformatics.infrastructure.test.ContainerTest;
+import junit.framework.Assert;
 import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
 import org.broadinstitute.gpinformatics.mercury.integration.RestServiceContainerTest;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -16,16 +11,12 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.JAXBContext;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.AUTO_BUILD;
 import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV;
-import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.STUBBY;
 import static org.broadinstitute.gpinformatics.infrastructure.test.TestGroups.EXTERNAL_INTEGRATION;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PDOSampleBillingStatusResourceTest extends RestServiceContainerTest {
@@ -63,21 +54,49 @@ public class PDOSampleBillingStatusResourceTest extends RestServiceContainerTest
     public void testPDOSampleBilling(@ArquillianResource URL baseUrl) throws Exception {
         // todo arz check the base url for 0.0.0.0 or localhost
         List<PDOSamplePair> pdoSamplesList = new ArrayList<>();
-        pdoSamplesList.add(new PDOSamplePair("PDO-123", "SM-456",null));
+        PDOSamplePair pdoSample1 = new PDOSamplePair("PDO-872", "SM-47KKU",null);
+        PDOSamplePair pdoSample2 = new PDOSamplePair("PDO-1133","0113404606",null);
+        PDOSamplePair pdoSample3 = new PDOSamplePair("PDO-ONE_BILLION","DooDoo",null);
+        pdoSamplesList.add(pdoSample1);
+        pdoSamplesList.add(pdoSample2);
+        pdoSamplesList.add(pdoSample3);
+
         PDOSamplePairs pdoSamplePairs = new PDOSamplePairs();
         pdoSamplePairs.setPdoSamplePairs(pdoSamplesList);
 
         // todo arz figure out why 0.0.0.0 barfs
         URL fixedUrl = new URL(baseUrl.getProtocol(),"127.0.0.1",baseUrl.getPort(),baseUrl.getFile());
 
-
-        // todo arz put in assertions
-        PDOSamplePairs samplePairs = makeWebResource(fixedUrl,"pdoSampleBillingStatus")
+        PDOSamplePairs returnedPdoSamples = makeWebResource(fixedUrl,"pdoSampleBillingStatus")
                 .type(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
                 .entity(pdoSamplePairs)
                 .post(PDOSamplePairs.class);
-        System.out.println("got it");
+
+        boolean foundPdoSample1 = false;
+        boolean foundPdoSample2 = false;
+        boolean foundPdoSample3 = false;
+        for (PDOSamplePair pdoSamplePair : returnedPdoSamples.getPdoSamplePairs()) {
+            if (pdoSamplePair.getSampleName().equals(pdoSample1.getSampleName())) {
+                foundPdoSample1 = true;
+                Assert.assertTrue(pdoSamplePair.isHasPrimaryPriceItemBeenBilled());
+            }
+            if (pdoSamplePair.getSampleName().equals(pdoSample2.getSampleName())) {
+                foundPdoSample2 = true;
+                Assert.assertFalse(pdoSamplePair.isHasPrimaryPriceItemBeenBilled());
+            }
+            if (pdoSamplePair.getSampleName().equals(pdoSample3.getSampleName())) {
+                foundPdoSample3 = true;
+                Assert.assertNull(pdoSamplePair.isHasPrimaryPriceItemBeenBilled());
+            }
+        }
+
+        Assert.assertTrue(foundPdoSample1);
+        Assert.assertTrue(foundPdoSample2);
+        Assert.assertTrue(foundPdoSample3);
+
+        Assert.assertEquals(returnedPdoSamples.getErrors().size(),1);
+
     }
 
 
