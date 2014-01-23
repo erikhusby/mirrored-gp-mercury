5ae141d9b8f72b4cf3020bb2a592a2ccbaa45559
Jon Thompson <thompson@broadinstitute.org>
2014-01-23 12:28:28 -0500

    GPLIM-2203 more test coverage

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/zims/IlluminaRunResourceLiveTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/zims/IlluminaRunResourceLiveTest.java
@@ -33,13 +33,8 @@ public class IlluminaRunResourceLiveTest extends Arquillian {
 
     @Test(dataProvider = Arquillian.ARQUILLIAN_DATA_PROVIDER, groups = EXTERNAL_INTEGRATION)
     @RunAsClient
-    public void testZimsOverHttp(@ArquillianResource URL baseUrl) throws Exception {
-        String url = baseUrl.toExternalForm() + IlluminaRunResourceTest.WEBSERVICE_URL;
-        DefaultClientConfig clientConfig = new DefaultClientConfig();
-        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-        ZimsIlluminaRun run = Client.create(clientConfig).resource(url)
-                .queryParam("runName", "140108_SL-HDJ_0272_BFCH7A84ADXX")
-                .accept(MediaType.APPLICATION_JSON).get(ZimsIlluminaRun.class);
+    public void testMercury(@ArquillianResource URL baseUrl) throws Exception {
+        ZimsIlluminaRun run = getZimsIlluminaRun(baseUrl, "140108_SL-HDJ_0272_BFCH7A84ADXX");
 
         Assert.assertEquals(run.getLanes().size(), 2, "Wrong number of lanes");
         ZimsIlluminaChamber zimsIlluminaChamber = run.getLanes().iterator().next();
@@ -47,7 +42,31 @@ public class IlluminaRunResourceLiveTest extends Arquillian {
 
         Assert.assertEquals(zimsIlluminaChamber.getLibraries().size(), 5, "Wrong number of libraries");
         LibraryBean libraryBean = zimsIlluminaChamber.getLibraries().iterator().next();
-        Assert.assertEquals(libraryBean.getLibrary(), "0154850237_Illumina_P5-Wizap_P7-Jatod");
+        Assert.assertEquals(libraryBean.getLibrary(), "0154850236_Illumina_P5-Yahih_P7-Zepon");
         Assert.assertEquals(libraryBean.getLibraryCreationDate(), "12/17/2013 12:50");
+    }
+
+    @Test(dataProvider = Arquillian.ARQUILLIAN_DATA_PROVIDER, groups = EXTERNAL_INTEGRATION)
+    @RunAsClient
+    public void testThrift(@ArquillianResource URL baseUrl) throws Exception {
+        ZimsIlluminaRun run = getZimsIlluminaRun(baseUrl, "120910_SL-HBL_0218_BFCD15B6ACXX");
+
+        Assert.assertEquals(run.getLanes().size(), 8, "Wrong number of lanes");
+        ZimsIlluminaChamber zimsIlluminaChamber = run.getLanes().iterator().next();
+        Assert.assertEquals(zimsIlluminaChamber.getLoadingConcentration(), 14.5);
+
+        Assert.assertEquals(zimsIlluminaChamber.getLibraries().size(), 94, "Wrong number of libraries");
+        LibraryBean libraryBean = zimsIlluminaChamber.getLibraries().iterator().next();
+        Assert.assertEquals(libraryBean.getLibrary(), "Pond-176198");
+        Assert.assertEquals(libraryBean.getLibraryCreationDate(), "08/22/2012 12:40");
+    }
+
+    private ZimsIlluminaRun getZimsIlluminaRun(URL baseUrl, String runName) {
+        String url = baseUrl.toExternalForm() + IlluminaRunResourceTest.WEBSERVICE_URL;
+        DefaultClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        return Client.create(clientConfig).resource(url)
+                .queryParam("runName", runName)
+                .accept(MediaType.APPLICATION_JSON).get(ZimsIlluminaRun.class);
     }
 }
