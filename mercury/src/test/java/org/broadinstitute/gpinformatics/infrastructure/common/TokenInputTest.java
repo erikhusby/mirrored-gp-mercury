2b6a34266f0f3137efb242cb380d73778446319d
Dennis Ryan <dryan@broadinstitute.org>
2014-01-27 09:43:22 -0500

    GPLIM-2435 Use mock instead of creating dummy subclass.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/common/TokenInputTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/common/TokenInputTest.java
@@ -12,16 +12,25 @@
 package org.broadinstitute.gpinformatics.infrastructure.common;
 
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
+import org.easymock.EasyMock;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Collection;
 
 @Test(groups = TestGroups.DATABASE_FREE)
 public class TokenInputTest {
+    private TokenInput tokenInput;
+
+    @BeforeMethod
+    private void setUp() {
+        tokenInput = EasyMock.createMockBuilder(TokenInput.class).createMock();
+    }
+
     public void testExtractSearchTermsWithInput() throws Exception {
         String queryString = "foo bar baz foo-bar";
-        Collection extractSearchTerms = getTestTokenInput().extractSearchTerms(queryString);
+        Collection extractSearchTerms = tokenInput.extractSearchTerms(queryString);
 
         Assert.assertEquals(extractSearchTerms.size(), 4);
         Assert.assertTrue(extractSearchTerms.contains("foo"));
@@ -31,37 +40,12 @@ public class TokenInputTest {
     }
 
     public void testExtractSearchTermsNullInput() throws Exception {
-        Collection extractSearchTerms = getTestTokenInput().extractSearchTerms(null);
+        Collection extractSearchTerms = tokenInput.extractSearchTerms(null);
         Assert.assertTrue(extractSearchTerms.isEmpty());
     }
 
     public void testExtractSearchTermsEmptyInput() throws Exception {
-            Collection extractSearchTerms = getTestTokenInput().extractSearchTerms("");
-            Assert.assertTrue(extractSearchTerms.isEmpty());
-        }
-
-    private TokenInput getTestTokenInput(){
-        return new TokenInput(true) {
-            @Override
-            protected String getTokenId(Object o) {
-                return null;
-            }
-
-            @Override
-            protected String getTokenName(Object o) {
-                return null;
-            }
-
-            @Override
-            protected String formatMessage(String messageString, Object o) {
-                return null;
-            }
-
-            @Override
-            protected Object getById(String key) {
-                return null;
-            }
-        };
-
+        Collection extractSearchTerms = tokenInput.extractSearchTerms("");
+        Assert.assertTrue(extractSearchTerms.isEmpty());
     }
 }
