432c3f1d22d55486fbb305d64b153166e1b0a122
Eliot Polk <epolk@broadinstitute.org>
2014-01-31 15:29:58 -0500

    GPLIM-2230 Mercury support for automated daughter plate creation

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/BSPRestClient.java
@@ -0,0 +1,54 @@
+/*
+ * The Broad Institute
+ * SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ * This software and its documentation are copyright 2013 by the
+ * Broad Institute/Massachusetts Institute of Technology. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support
+ * whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ * use, misuse, or functionality.
+ */
+
+package org.broadinstitute.gpinformatics.mercury;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPConfig;
+import org.broadinstitute.gpinformatics.mercury.control.AbstractJerseyClientService;
+
+import javax.inject.Inject;
+
+/**
+ * This contains common code used by all clients of BSP rest, ie: non-broadcore) services.
+ */
+public class BSPRestClient extends AbstractJerseyClientService {
+
+    private static final long serialVersionUID = 5472586820069306030L;
+
+    @Inject
+    private BSPConfig bspConfig;
+
+    public BSPRestClient() {
+    }
+
+    public BSPRestClient(BSPConfig bspConfig) {
+        this.bspConfig = bspConfig;
+    }
+
+    public String getUrl(String urlSuffix) {
+        return bspConfig.getUrl("rest/" + urlSuffix);
+    }
+
+    public BSPConfig getBspConfig() {
+        return bspConfig;
+    }
+
+    @Override
+    protected void customizeClient(Client client) {
+        specifyHttpAuthCredentials(client, bspConfig);
+    }
+
+    public WebResource getWebResource(String urlString) {
+        return getJerseyClient().resource(urlString);
+    }
+}
