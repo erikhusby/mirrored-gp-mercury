aafa1b33610062f89808cc6cea06a99998ea059b
Jon Thompson <thompson@broadinstitute.org>
2014-03-24 10:51:11 -0400

    GPLIM-2628 fixup

---
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/run/SequencingRunFixupTest.java
@@ -0,0 +1,37 @@
+package org.broadinstitute.gpinformatics.mercury.entity.run;
+
+import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
+import org.broadinstitute.gpinformatics.mercury.control.dao.run.IlluminaSequencingRunDao;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+
+import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV;
+
+/**
+ * Fixups to the SequencingRun entity.
+ */
+public class SequencingRunFixupTest extends Arquillian {
+
+    @Inject
+    private IlluminaSequencingRunDao illuminaSequencingRunDao;
+
+    @Deployment
+    public static WebArchive buildMercuryWar() {
+        return DeploymentBuilder.buildMercuryWar(DEV, "prod");
+    }
+
+    @Test(enabled = true)
+    public void fixupGplim2628() {
+        // storeRunReadStructure is supplying run barcode, but there are two runs with same barcode, so change
+        // the unwanted one
+        IlluminaSequencingRun illuminaSequencingRun =
+                illuminaSequencingRunDao.findByRunName("140321_SL-MAD_0181_FC000000000-A7LC2");
+        illuminaSequencingRun.setRunBarcode("x" + illuminaSequencingRun.getRunBarcode());
+        illuminaSequencingRunDao.flush();
+    }
+
+}
