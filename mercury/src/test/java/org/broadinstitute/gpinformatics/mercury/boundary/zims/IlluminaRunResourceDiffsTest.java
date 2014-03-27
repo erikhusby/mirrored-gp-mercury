685e9ad525a22ae6bdec1a8b7f915cf7bb702568
Jon Thompson <thompson@broadinstitute.org>
2014-03-27 11:32:58 -0400

    GPLIM-2635 draft test to diff JSON

---
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/zims/IlluminaRunResourceDiffsTest.java
@@ -0,0 +1,57 @@
+package org.broadinstitute.gpinformatics.mercury.boundary.zims;
+
+import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
+import org.broadinstitute.gpinformatics.mercury.control.dao.run.IlluminaSequencingRunDao;
+import org.broadinstitute.gpinformatics.mercury.entity.reagent.ImportFromSquidTest;
+import org.broadinstitute.gpinformatics.mercury.entity.run.IlluminaSequencingRun;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.testng.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV;
+import static org.broadinstitute.gpinformatics.infrastructure.test.TestGroups.EXTERNAL_INTEGRATION;
+
+/**
+ * A test to compare IlluminaRunResource output from different deployments, e.g. to verify a change.
+ */
+public class IlluminaRunResourceDiffsTest extends Arquillian {
+
+    @Inject
+    private IlluminaSequencingRunDao illuminaSequencingRunDao;
+
+    @Deployment
+    public static WebArchive buildMercuryWar() {
+        return DeploymentBuilder.buildMercuryWar(DEV, "dev");
+    }
+
+    @Test(groups = EXTERNAL_INTEGRATION)
+    public void testMercury() {
+        try {
+            List<IlluminaSequencingRun> illuminaSequencingRuns = illuminaSequencingRunDao.findAll(
+                    IlluminaSequencingRun.class);
+            for (IlluminaSequencingRun illuminaSequencingRun : illuminaSequencingRuns) {
+                // Exclude runs created by tests
+                if (!illuminaSequencingRun.getRunName().contains("Flowcell")) {
+                    String localRun = IlluminaRunResourceLiveTest.getZimsIlluminaRunString(
+                            new URL(ImportFromSquidTest.TEST_MERCURY_URL + "/"),
+                            illuminaSequencingRun.getRunName());
+                    String referenceRun = IlluminaRunResourceLiveTest.getZimsIlluminaRunString(
+                            new URL("http://mercurydev:8080/Mercury/"),
+                            illuminaSequencingRun.getRunName());
+                    JSONAssert.assertEquals(referenceRun, localRun, JSONCompareMode.LENIENT);
+                }
+            }
+        } catch (MalformedURLException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
