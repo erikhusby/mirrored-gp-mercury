b75fbaaabbf720f75729f85717053378520843f1
Scott Matthews <scottmat@broadinstitute.org>
2014-05-01 14:50:35 -0400

    GPLIM-2625, GPLIM-2711, GPLIM-2708:  Updating Fixup test to be disabled and pointed to prod

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/bucket/BucketEntryFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/bucket/BucketEntryFixupTest.java
@@ -60,7 +60,7 @@ public class BucketEntryFixupTest extends Arquillian {
          * If the need comes to utilize this fixup in production, change the buildMercuryWar parameters accordingly
          */
         return DeploymentBuilder.buildMercuryWar(
-                org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV, "dev");
+                org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.PROD, "prod");
     }
 
     @BeforeMethod(groups = TestGroups.EXTERNAL_INTEGRATION)
@@ -129,11 +129,11 @@ public class BucketEntryFixupTest extends Arquillian {
         bucket.removeEntry(bucketEntry);
     }
 
-    @Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled = true)
+    @Test(groups = TestGroups.EXTERNAL_INTEGRATION, enabled = false)
     public void setProductOrderReferences() throws Exception {
 
-        List<BucketEntry> bucketEntriesToFix = bucketEntryDao.findList(BucketEntry.class, BucketEntry_.productOrder,
-                                                                       null);
+        List<BucketEntry> bucketEntriesToFix =
+                bucketEntryDao.findList(BucketEntry.class, BucketEntry_.productOrder, null);
         int counter = 0;
 
         Set<String> badKeySet = new HashSet<>();
@@ -162,8 +162,6 @@ public class BucketEntryFixupTest extends Arquillian {
                 }
                 if (counter == 100) {
                     counter = 0;
-//                    productOrderDao.flush();
-//                    productOrderDao.clear();
                     utx.commit();
                     utx.begin();
                     bucketEntriesToFix = bucketEntryDao.findList(BucketEntry.class, BucketEntry_.productOrder, null);
