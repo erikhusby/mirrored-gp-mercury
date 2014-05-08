7d4604147528a088b641116bf2cc984964c0b534
Dennis Ryan <dryan@broadinstitute.org>
2014-05-08 11:06:51 -0400

    GPLIM-1517: negative test case

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPSampleSearchColumnTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/bsp/BSPSampleSearchColumnTest.java
@@ -14,15 +14,34 @@ package org.broadinstitute.gpinformatics.infrastructure.bsp;
 import org.apache.commons.collections.CollectionUtils;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
 
 @Test(groups = TestGroups.DATABASE_FREE)
 public class BSPSampleSearchColumnTest {
+    @DataProvider(name = "searchColumnDataProvider")
+    public Object[][] searchColumnDataProvider() {
+        EnumSet<BSPSampleSearchColumn> uniqueSearchColumns =
+                EnumSet.copyOf(Arrays.asList(BSPSampleSearchColumn.PDO_SEARCH_COLUMNS));
+
+        BSPSampleSearchColumn[] arrayWithDupes =
+                {BSPSampleSearchColumn.COLLABORATOR_NAME, BSPSampleSearchColumn.COLLABORATOR_NAME,
+                        BSPSampleSearchColumn.COLLABORATOR_NAME, BSPSampleSearchColumn.COLLABORATOR_NAME,
+                        BSPSampleSearchColumn.COLLABORATOR_NAME};
+        EnumSet<BSPSampleSearchColumn> searchColumnsFromDupeArray = EnumSet.copyOf(Arrays.asList(arrayWithDupes));
+
+        return new Object[][]{
+                {uniqueSearchColumns, BSPSampleSearchColumn.PDO_SEARCH_COLUMNS, true},
+                // This failing case is really here to test my test
+                {searchColumnsFromDupeArray, arrayWithDupes, false}
+        };
+    }
+
     /**
      * Test whether or not there are duplicate entries in BSPSampleSearchColumn.PDO_SEARCH_COLUMNS
      * <p/>
@@ -31,13 +50,14 @@ public class BSPSampleSearchColumnTest {
      *
      * @see BSPSampleSearchColumn#PDO_SEARCH_COLUMNS
      */
-    public void testPdoSearchColumns() {
-        List<BSPSampleSearchColumn> bspSampleSearchColumnList = Arrays.asList(BSPSampleSearchColumn.PDO_SEARCH_COLUMNS);
-        Set<BSPSampleSearchColumn> bspSampleSearchColumns = new HashSet<>(bspSampleSearchColumnList);
+    @Test(dataProvider = "searchColumnDataProvider")
+    public void testPdoSearchColumns(Set<BSPSampleSearchColumn> testSet, BSPSampleSearchColumn[] testArray,
+                                     boolean willPass) {
+        List<BSPSampleSearchColumn> bspSampleSearchColumnList = Arrays.asList(testArray);
 
-        Assert.assertEquals(bspSampleSearchColumnList.size(), bspSampleSearchColumns.size(),
+        Assert.assertEquals(testSet.size() == testArray.length, willPass,
                 "Duplicate entries found in BSPSampleSearchColumn.PDO_SEARCH_COLUMNS: " + CollectionUtils
-                        .disjunction(bspSampleSearchColumnList, bspSampleSearchColumns)
+                        .disjunction(bspSampleSearchColumnList, testSet)
         );
     }
 
