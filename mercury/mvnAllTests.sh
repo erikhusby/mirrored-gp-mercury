223414d115c6c2dd392fe2710c6eedff6a0da860
Erik Husby <mhusby@broadinstitute.org>
2014-07-10 10:54:38 -0400

    Arquillian Suite Implementation in Bamboo

--- mercury/mvnAllTests.sh
+++ mercury/mvnAllTests.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -l
 #
 # Run all the unit tests using each of the several profiles.
 #
+use Maven-3.0
 #mvn clean | tee tests.log
 OPTIONS="-PArquillian-JBossAS7-Remote -Djava.awt.headless=true --batch-mode -Dmaven.download.meter=silent"
 for PROFILE in Tests.ArqSuite.Standard Tests.ArqSuite.Stubby Tests.DatabaseFree Tests.ExternalIntegration Tests.Alternatives
