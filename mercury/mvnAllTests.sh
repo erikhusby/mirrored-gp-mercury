59e7ced1fda847db64f5793e6e65a25a15b0d893
Erik Husby <mhusby@broadinstitute.org>
2014-07-22 15:20:15 -0400

    GPLIM-2843 - Tweaking mvnAllTests.sh settings

--- mercury/mvnAllTests.sh
+++ mercury/mvnAllTests.sh
@@ -4,9 +4,13 @@
 #
 use Maven-3.0
 #mvn clean | tee tests.log
+JBOSS_HOME=/prodinfolocal/jboss-as-7.1.1.Final/
+MAVEN_OPTS="-Xms4g -XX:MaxPermSize=1g"
 OPTIONS="-PArquillian-JBossAS7-Remote,BUILD -Djava.awt.headless=true --batch-mode -Dmaven.download.meter=silent"
 PROFILES="Tests.ArqSuite.Standard Tests.ArqSuite.Stubby Tests.DatabaseFree Tests.ExternalIntegration Tests.Alternatives"
 #PROFILES="Tests.ArqSuite.Standard Tests.ArqSuite.Stubby"
+
+rm tests.log
 for PROFILE in $PROFILES
 do
     echo "Using profile $PROFILE"
