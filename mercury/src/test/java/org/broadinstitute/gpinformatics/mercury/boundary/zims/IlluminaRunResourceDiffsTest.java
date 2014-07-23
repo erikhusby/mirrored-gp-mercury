a5ff224298631ab3b21347337dca3f94a1fe8937
Jon Thompson <thompson@broadinstitute.org>
2014-07-23 12:19:30 -0400

    GPLIM-2635 disabled test

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/zims/IlluminaRunResourceDiffsTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/zims/IlluminaRunResourceDiffsTest.java
@@ -32,7 +32,7 @@ public class IlluminaRunResourceDiffsTest extends Arquillian {
         return DeploymentBuilder.buildMercuryWar(DEV, "dev");
     }
 
-    @Test(groups = EXTERNAL_INTEGRATION, enabled = true)
+    @Test(groups = EXTERNAL_INTEGRATION, enabled = false)
     public void testCompareAll() {
         List<IlluminaSequencingRun> illuminaSequencingRuns = illuminaSequencingRunDao.findAll(
                 IlluminaSequencingRun.class);
