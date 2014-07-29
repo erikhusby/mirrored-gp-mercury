c8b8096242685f265da20fcb704262f07a7aefe0
hrafal <hrafal@broadinstitute.org>
2014-07-29 13:50:51 -0400

    PORTAL-164: Formula for sample count so web service is not halted on non-sample web service

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/orders/ProductOrderData.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/orders/ProductOrderData.java
@@ -118,7 +118,7 @@ public class ProductOrderData {
             // includeSamples = false.  Is the JAXB behavior with an empty List undesirable?
             samples = null;
         }
-        numberOfSamples = productOrder.getSamples().size();
+        numberOfSamples = productOrder.getSampleCount();
     }
 
     private static List<String> getSampleList(List<ProductOrderSample> productOrderSamples) {
