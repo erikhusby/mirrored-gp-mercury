45ccf4673433ef3f80c87239b2ff4a64550eaf92
Dennis Ryan <dryan@broadinstitute.org>
2014-08-18 15:41:10 -0400

    GPLIM-2858: add page number, and date expires to PDF download footer. date expires corresponds to end of current quarter (cherry picked from commit faa6abb)

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/widget/daterange/OneQuarterDateRange.java
@@ -0,0 +1,50 @@
+/*
+ * The Broad Institute
+ * SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ * This software and its documentation are copyright 2014 by the
+ * Broad Institute/Massachusetts Institute of Technology. All rights are reserved.
+ *
+ * This software is supplied without any warranty or guaranteed support
+ * whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ * use, misuse, or functionality.
+ */
+
+package org.broadinstitute.gpinformatics.infrastructure.widget.daterange;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class OneQuarterDateRange extends DateRange.ComputeStartAndStopDate {
+    public OneQuarterDateRange() {
+        this(new Date());
+    }
+
+    public OneQuarterDateRange(Date currentDate) {
+        Calendar calendar = GregorianCalendar.getInstance();
+        calendar.setTime(DateUtils.getStartOfDay(currentDate));
+        setStartCalendar(calendar);
+    }
+
+    @Override
+    public void calcDate(Calendar startCalendar, Calendar stop) {
+        int month = startCalendar.get(Calendar.MONTH);
+        if ((month >= Calendar.JANUARY) && (month <= Calendar.MARCH)) {
+            startCalendar.set(Calendar.MONTH, Calendar.JANUARY);
+        } else if ((month >= Calendar.APRIL) && (month <= Calendar.JUNE)) {
+            startCalendar.set(Calendar.MONTH, Calendar.APRIL);
+        } else if ((month >= Calendar.JULY) && (month <= Calendar.SEPTEMBER)) {
+            startCalendar.set(Calendar.MONTH, Calendar.JULY);
+        } else {
+            startCalendar.set(Calendar.MONTH, Calendar.OCTOBER);
+        }
+        startCalendar.set(Calendar.DAY_OF_MONTH, 1);
+        startCalendar.setTime(DateUtils.getStartOfDay(startCalendar.getTime()));
+
+        stop.set(Calendar.MONTH, startCalendar.get(Calendar.MONTH) + 2);
+        stop.set(Calendar.DAY_OF_MONTH, stop.getActualMaximum(Calendar.DAY_OF_MONTH));
+        stop.setTime(DateUtils.getEndOfDay(stop.getTime()));
+        setStopCalendar(stop);
+    }
+
+}
