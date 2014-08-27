4dcd34fe5d2a3d5c2c7bb7806c8337166474542d
Scott Matthews <scottmat@broadinstitute.org>
2014-08-27 17:06:37 -0400

    GPLIM-2885:  TDD Development of validations.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/sample/ManifestRecordTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/sample/ManifestRecordTest.java
@@ -11,6 +11,11 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
 /**
  * Database free test of ManifestRecords, entities representing individual samples within a Buick manifest used for
  * sample registration.
@@ -18,7 +23,7 @@ import org.testng.annotations.Test;
 @Test(groups = TestGroups.DATABASE_FREE)
 public class ManifestRecordTest {
 
-    private static final String VALUE_1 = "value1";
+    private static final String COLLABORATOR_SAMPLE_ID_1 = "COLLABORATOR_SAMPLE_ID_1";
     private static final String VALUE_2 = "value2";
     private static final String VALUE_3 = "value3";
     private static final ManifestRecord.Status NEW_STATUS = ManifestRecord.Status.ABANDONED;
@@ -28,8 +33,13 @@ public class ManifestRecordTest {
 
     @BeforeMethod
     public void setUp() throws Exception {
-        testRecord = ManifestTestFactory.buildManifestRecord(
-                ImmutableMap.of(Metadata.Key.SAMPLE_ID, VALUE_1, Metadata.Key.GENDER, VALUE_2, Metadata.Key.PATIENT_ID, VALUE_3));
+        testRecord = buildManifestRecord(COLLABORATOR_SAMPLE_ID_1);
+    }
+
+    private ManifestRecord buildManifestRecord(String sampleId) {
+        return ManifestTestFactory.buildManifestRecord(
+                ImmutableMap.of(Metadata.Key.SAMPLE_ID, sampleId, Metadata.Key.GENDER, VALUE_2, Metadata.Key.PATIENT_ID,
+                        VALUE_3));
     }
 
     /**
@@ -40,7 +50,7 @@ public class ManifestRecordTest {
         // Test with no specified Status or ErrorStatus.
 
         // Basic sanity check of retrieving Metadata by key.
-        Assert.assertEquals(testRecord.getMetadataByKey(Metadata.Key.SAMPLE_ID).getValue(), VALUE_1);
+        Assert.assertEquals(testRecord.getMetadataByKey(Metadata.Key.SAMPLE_ID).getValue(), COLLABORATOR_SAMPLE_ID_1);
         Assert.assertEquals(testRecord.getMetadataByKey(Metadata.Key.GENDER).getValue(), VALUE_2);
         Assert.assertEquals(testRecord.getMetadataByKey(Metadata.Key.PATIENT_ID).getValue(), VALUE_3);
         // Default status should be UPLOADED.
@@ -56,22 +66,36 @@ public class ManifestRecordTest {
         Assert.assertEquals(testRecord.getErrorStatus(), NEW_ERROR_STATUS);
     }
 
-    public void testValidationDuplicateInManifest() throws Exception {
+    public void validManifest() {
+        ManifestSession testSession = buildTestSession();
 
-        ResearchProject testProject = ResearchProjectTestFactory.createTestResearchProject("RP-334");
+        String COLLABORATOR_SAMPLE_ID_2 = "COLLABORATOR_SAMPLE_ID_2";
+        ManifestRecord secondManifestRecord = buildManifestRecord(COLLABORATOR_SAMPLE_ID_2);
+        testSession.addRecord(secondManifestRecord);
 
-        ManifestSession testSession = new ManifestSession(testProject, "DUPLICATE_TEST", new BSPUserList.QADudeUser("LU", 33L));
+        Assert.assertTrue(testSession.isManifestValid());
 
-        testSession.addRecord(testRecord);
+        Assert.assertEquals(testSession.getLogEntries().size(), 0);
+        assertThat(testRecord.getErrorStatus(), is(nullValue()));
+        assertThat(secondManifestRecord.getErrorStatus(), is(nullValue()));
+
+        assertThat(testRecord.getLogEntries(), is(empty()));
+        assertThat(secondManifestRecord.getLogEntries(), is(empty()));
+    }
+
+
+    public void duplicateInManifest() throws Exception {
+
+        ManifestSession testSession = buildTestSession();
 
         ManifestRecord testRecordWithDupe =
-                ManifestTestFactory.buildManifestRecord(ImmutableMap.of(Metadata.Key.SAMPLE_ID, VALUE_1,
+                ManifestTestFactory.buildManifestRecord(ImmutableMap.of(Metadata.Key.SAMPLE_ID,
+                        COLLABORATOR_SAMPLE_ID_1,
                         Metadata.Key.GENDER, "F", Metadata.Key.PATIENT_ID, VALUE_3));
 
         testSession.addRecord(testRecordWithDupe);
 
         Assert.assertFalse(testSession.isManifestValid());
-
 
         Assert.assertEquals(testSession.getLogEntries().size(), 2);
         Assert.assertEquals(testRecord.getErrorStatus(), ManifestRecord.ErrorStatus.DUPLICATE_SAMPLE_ID);
@@ -79,6 +103,15 @@ public class ManifestRecordTest {
 
         Assert.assertEquals(testRecord.getLogEntries().size(), 1);
         Assert.assertEquals(testRecordWithDupe.getLogEntries().size(), 1);
+    }
+
+    public ManifestSession buildTestSession() {
+        ResearchProject testProject = ResearchProjectTestFactory.createTestResearchProject("RP-334");
+
+        ManifestSession testSession = new ManifestSession(testProject, "DUPLICATE_TEST", new BSPUserList.QADudeUser("LU", 33L));
+
+        testSession.addRecord(testRecord);
+        return testSession;
     }
 
 }
