d9087265ff5229c16c4b36a08972cbb3b6935bb7
Erik Husby <mhusby@broadinstitute.org>
2014-09-24 11:36:42 -0400

    GPLIM-2947, Improved scripts according to code rewview, update to latest version of jgitflow-maven-plugin

--- mercury/fixVersions.sh
+++ mercury/fixVersions.sh
@@ -2,6 +2,10 @@
 #
 # Fix the versions of limsThrift and bspclient for the release. Assumes we are on the release branch.
 #
+# This procedure is used in the Bamboo builds that run as user releng where the default
+# versions are not correct for the Mercury environment. In addition, the order is important
+# because of problems in the dot kit that screws up the PATH after the use Maven-3.1 command.
+#
 unuse Maven-2.2
 unuse Java-1.6-Prodinfo
 unuse Git-2.0
@@ -11,7 +15,12 @@ use Git-1.8
 
 mvn -DincludesList=lims:limsThrift:jar::1.0-SNAPSHOT versions:lock-snapshots versions:resolve-ranges
 
-BRANCH=`git branch | grep release`
+#
+# Find current release branch which by convention is release/x.xx
+# The gitflow process as implemented by the maven-jgitflow plugin insures that there is only one
+# release branch in process at a time.
+#
+BRANCH=`git branch | grep release/`
 git branch
 git status
 git commit -m "REL-714 Fixing versions of limsThrift and bspclient." pom.xml
