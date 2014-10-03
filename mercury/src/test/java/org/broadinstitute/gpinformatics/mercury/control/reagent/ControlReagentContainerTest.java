0cefd4f04af5bc0787c88681e5a84b343f4aa2cf
Jon Thompson <thompson@broadinstitute.org>
2014-10-03 17:43:12 -0400

    GPLIM-2980 container test passing

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/reagent/ControlReagentContainerTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/reagent/ControlReagentContainerTest.java
@@ -9,8 +9,11 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.broadinstitute.bsp.client.util.MessageCollection;
 import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
+import org.broadinstitute.gpinformatics.mercury.control.dao.vessel.BarcodedTubeDao;
 import org.broadinstitute.gpinformatics.mercury.control.vessel.VarioskanParserTest;
+import org.broadinstitute.gpinformatics.mercury.entity.reagent.ControlReagent;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.BarcodedTube;
+import org.broadinstitute.gpinformatics.mercury.test.BaseEventTest;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
@@ -40,6 +43,9 @@ public class ControlReagentContainerTest extends Arquillian {
     @Inject
     private ControlReagentFactory controlReagentFactory;
 
+    @Inject
+    private BarcodedTubeDao barcodedTubeDao;
+
     @Deployment
     public static WebArchive buildMercuryWar() {
         return DeploymentBuilder.buildMercuryWar(DEV, "dev");
@@ -51,13 +57,13 @@ public class ControlReagentContainerTest extends Arquillian {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
         String timestamp = simpleDateFormat.format(new Date());
         Map<String, String> mapControlToLot = new HashMap<>();
-        mapControlToLot.put("NA12878", "SK-P" + timestamp);
+        mapControlToLot.put(BaseEventTest.POSITIVE_CONTROL, "SK-P" + timestamp);
         mapControlToLot.put("NO_TEMPLATE_CONTROL", "SK-N" + timestamp);
-        // replace tube barcode and lot with timestamps, to avoid unique constraint
         try {
+            // replace tube barcode and lot with timestamps, to avoid unique constraint
             Workbook workbook = WorkbookFactory.create(testSpreadSheetInputStream);
             Sheet sheet = workbook.getSheet("Sheet1");
-            for (int i = 1; i < sheet.getLastRowNum(); i++) {
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
                 Row row = sheet.getRow(i);
                 if (row != null) {
                     Cell tubeCell = row.getCell(0);
@@ -71,10 +77,18 @@ public class ControlReagentContainerTest extends Arquillian {
             }
             File tempFile = File.createTempFile("ControlReagents", ".xlsx");
             workbook.write(new FileOutputStream(tempFile));
+
             MessageCollection messageCollection = new MessageCollection();
             List<BarcodedTube> barcodedTubes = controlReagentFactory.buildTubesFromSpreadsheet(
                     new FileInputStream(tempFile), messageCollection);
+
             Assert.assertFalse(messageCollection.hasErrors());
+            barcodedTubeDao.flush();
+            barcodedTubeDao.clear();
+            BarcodedTube barcodedTube = barcodedTubeDao.findByBarcode(barcodedTubes.get(0).getLabel());
+            Assert.assertEquals(barcodedTube.getReagentContents().size(), 1);
+            ControlReagent controlReagent = (ControlReagent) barcodedTube.getReagentContents().iterator().next();
+            Assert.assertEquals(controlReagent.getControl().getCollaboratorSampleId(), BaseEventTest.POSITIVE_CONTROL);
         } catch (IOException | InvalidFormatException e) {
             throw new RuntimeException(e);
         }
