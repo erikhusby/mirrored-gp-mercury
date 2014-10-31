777d0f7940500820bc327be670cc49c9db793ec5
Eliot Polk <epolk@broadinstitute.org>
2014-10-31 16:17:15 -0400

    GPLIM-3132,3136 fixup reagent lot and date

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/ReagentFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/ReagentFixupTest.java
@@ -3,12 +3,21 @@ package org.broadinstitute.gpinformatics.mercury.entity.reagent;
 import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.broadinstitute.gpinformatics.mercury.control.dao.reagent.ReagentDesignDao;
+import org.broadinstitute.gpinformatics.mercury.entity.envers.FixupCommentary;
+import org.broadinstitute.gpinformatics.mercury.presentation.UserBean;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV;
 
@@ -20,6 +29,9 @@ public class ReagentFixupTest extends Arquillian {
 
     @Inject
     private ReagentDesignDao reagentDesignDao;
+
+    @Inject
+    private UserBean userBean;
 
     @Deployment
     public static WebArchive buildMercuryWar() {
@@ -34,4 +46,50 @@ public class ReagentFixupTest extends Arquillian {
         reagentDesign.setDesignName("Buick_v6_0_2014");
         reagentDesignDao.flush();
     }
+
+    @Test(enabled = false)
+    public void gplim3136fixupLot() {
+        userBean.loginOSUser();
+        Map<Long, String> map = new HashMap<Long, String> () {{
+            put(631019L, "000017279828");
+            put(631020L, "WO0000109974644");
+        }};
+        for (Long id : map.keySet()) {
+            Reagent reagent = reagentDesignDao.findById(Reagent.class, id);
+            if (reagent == null) {
+                throw new RuntimeException("cannot find " + id);
+            }
+            System.out.println("Reagent lot " + reagent.getLot());
+            reagent.setLot(map.get(id));
+            System.out.println("   updated to " + reagent.getLot());
+        }
+        reagentDesignDao.persist(new FixupCommentary(
+                "GPLIM-3136 Masterform did not allow other than 10 character lot identifier."));
+        reagentDesignDao.flush();
+    }
+
+
+    @Test(enabled = false)
+    public void gplim3132fixupLotAndDate() throws Exception {
+        userBean.loginOSUser();
+
+        // Lot 14D24A0015 is the wrong and should be 14D24A0013 with an expiration date of 2/17/15.
+        // Existing lot 14D24A0013 should be set to the same expiration date.
+        String correctLot = "14D24A0013";
+        Date correctDate = (new SimpleDateFormat("MM/dd/yyyy")).parse("02/17/2015");
+        List<Reagent> reagents = reagentDesignDao.findListByList(Reagent.class, Reagent_.lot,
+                Arrays.asList(new String[]{"14D24A0015", correctLot}));
+        Assert.assertEquals(2, reagents.size());
+        for (Reagent reagent : reagents) {
+            System.out.println("Reagent lot " + reagent.getLot());
+            if (!correctLot.equals(reagent.getLot())) {
+                reagent.setLot(correctLot);
+            }
+            reagent.setExpiration(correctDate);
+            System.out.println("   updated to " + reagent.getLot() + " expiring " + reagent.getExpiration());
+        }
+        reagentDesignDao.persist(new FixupCommentary("GPLIM-3132 Incorrect lot information was entered."));
+        reagentDesignDao.flush();
+    }
+
 }
