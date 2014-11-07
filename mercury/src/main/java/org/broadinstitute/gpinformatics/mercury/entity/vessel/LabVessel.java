d7b4a527dea8dd6253d333157a2de2f24121bf2a
Scott Matthews <scottmat@broadinstitute.org>
2014-11-07 14:24:50 -0500

    GPLIM-3005 changing labVessel getter for mercurySamples to return a Set instead of a List. (cherry picked from commit 08fb32d) (cherry picked from commit 18f63f2)

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/LabVessel.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/LabVessel.java
@@ -181,7 +181,7 @@ public abstract class LabVessel implements Serializable {
     // todo jmt separate role for sample holder?
     @ManyToMany(cascade = CascadeType.PERSIST)
     @BatchSize(size = 100)
-    private List<MercurySample> mercurySamples = new ArrayList<>();
+    private Set<MercurySample> mercurySamples = new HashSet<>();
 
     // todo jmt set these fields db-free
     @OneToMany(mappedBy = "sourceVessel", cascade = CascadeType.PERSIST)
@@ -1179,7 +1179,7 @@ public abstract class LabVessel implements Serializable {
         }
     }
 
-    public List<MercurySample> getMercurySamples() {
+    public Set<MercurySample> getMercurySamples() {
         return mercurySamples;
     }
 
@@ -1911,5 +1911,4 @@ public abstract class LabVessel implements Serializable {
         }
         return values.toArray(new String[values.size()]);
     }
-
 }
