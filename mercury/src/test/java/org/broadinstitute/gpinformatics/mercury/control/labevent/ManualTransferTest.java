e274337288727e4c9406ad76b4b9bd3b372ae1c4
Jon Thompson <thompson@broadinstitute.org>
2015-01-07 13:24:11 -0500

    GPLIM-3301 sketch of test

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/labevent/ManualTransferTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/labevent/ManualTransferTest.java
@@ -6,22 +6,46 @@ package org.broadinstitute.gpinformatics.mercury.control.labevent;
 public class ManualTransferTest {
 
     public void testSectionTransfer() {
-        // choose lab event type (set mode mercury)
-        // add source lab vessel
-        // choose section
-        // set volume
-        // add destination lab vessel
-        // choose section
-        // set volume
-        // enter date, machine etc.
-        // record transfer
+        ManualTransferFactory manualTransferFactory = new ManualTransferFactory();
+        // Should the web page use AJAX to build dot notation that is created by Stripes on final POST,
+        // or do POSTS for each action, and accumulate results in JAXB DTO?.
+        //      DTO minimizes amount of JavaScript, but the repaint may confuse the user.
+        //      DTO would still have to be constructed by Stripes, unless stored in session.
+        //      Could store DTO in preference, which would allow saving, and avoiding losing data due to session timeout.
+        //      User defined search uses AJAX, but it deals with SearchTerm only, whereas this page would deal with
+        //      ~10 types, which would lead to a proliferation of JSP fragments
+        //           plate, positionMap, receptacle, reagent, metadata, cherry pick source
+        //      these fragments may be necessary anyway, to reduce duplication
+
+        // ignore multiple events per message for now
+
+        // StationEvent
+        // ReceptacleEvent
+        // PlateEvent
+        // PlateTransferEvent
+        // PlateCherryPickEvent
+        // ReceptaclePlateTransferEvent
+
+        // GET: enter page
+        // render drop down of lab event types
+        // user chooses lab event type (set mode mercury), clicks button
+        // POST: render skeleton of section message (or cherry, tube to tube) including machine names, default reagents
+        // user scans rack barcode
+        // user clicks rack scan button (manual alternative?)
+        // AJAX: render tube barcodes in geometry, with volume text box
+        // user chooses source section
+        // user sets volumes (need bulk change)
+        // user scans destination plate
+        // user chooses section
+        // set volume on plate wells
+        // user enters date, machine etc.
+        // AJAX: add / remove reagents
+        // AJAX: add / remove metadata
+        // user clicks transfer button
+        // POST to action bean
         // BettaLimsMessageResource processMessage or storeAndProcess
+
+        // Prototype a JSP to render a JAXB DTO and rebuild it on POST
     }
 
-    // StationEvent
-    // ReceptacleEvent
-    // PlateEvent
-    // PlateTransferEvent
-    // PlateCherryPickEvent
-    // ReceptaclePlateTransferEvent
 }
