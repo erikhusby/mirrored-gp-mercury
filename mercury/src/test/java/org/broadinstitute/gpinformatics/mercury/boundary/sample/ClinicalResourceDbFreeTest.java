2f0d17992f2781f6af5028b0edb50ee86866b4fd
Dennis Ryan <dryan@broadinstitute.org>
2015-02-17 09:49:24 -0500

    GPLIM-3328 move commented out tests from ClinicalResourceTest to ClinicalResourceDbFreeTest

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/sample/ClinicalResourceDbFreeTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/sample/ClinicalResourceDbFreeTest.java
@@ -4,10 +4,14 @@ import org.broadinstitute.bsp.client.users.BspUser;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
 import org.broadinstitute.gpinformatics.mercury.boundary.UnknownUserException;
 import org.broadinstitute.gpinformatics.mercury.boundary.manifest.ManifestSessionEjb;
+import org.broadinstitute.gpinformatics.mercury.entity.sample.ManifestSession;
 import org.broadinstitute.gpinformatics.mercury.presentation.UserBean;
 import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @Test(groups = TestGroups.DATABASE_FREE)
 public class ClinicalResourceDbFreeTest {
@@ -15,12 +19,14 @@ public class ClinicalResourceDbFreeTest {
     private ClinicalResource clinicalResource;
     private UserBean userBean;
     private BspUser testUser;
+    private ManifestSessionEjb manifestSessionEjb;
 
     @BeforeMethod
     public void setUp() throws Exception {
         testUser = new BspUser();
         userBean = Mockito.mock(UserBean.class);
-        clinicalResource = new ClinicalResource(userBean, null); // TODO: provide a (mock?) ManifestSessionEjb
+        manifestSessionEjb = Mockito.mock(ManifestSessionEjb.class);
+        clinicalResource = new ClinicalResource(userBean, manifestSessionEjb);
     }
 
     /**
@@ -41,5 +47,57 @@ public class ClinicalResourceDbFreeTest {
     @Test(expectedExceptions = UnknownUserException.class)
     public void testCreateSessionUnknownUser() {
         clinicalResource.createAccessioningSession("unknown_user", "test manifest", "RP-1", Boolean.TRUE);
+    }
+
+    /**
+     * Test that the call fails if isFromSampleKit is not passed in.
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateManifestWithIsFromSampleKitNull() {
+        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
+        clinicalResource.createAccessioningSession("test_user", "test manifest", "RP-1", null);
+    }
+
+    /**
+     * Test that the call fails if a null manifest name is provided.
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateManifestNullManifestName() {
+        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
+        clinicalResource.createAccessioningSession("test user", null, "RP-1", Boolean.TRUE);
+    }
+
+    /**
+     * Test that the call fails if an empty manifest name is provided.
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testCreateManifestEmptyManifestName() {
+        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
+        clinicalResource.createAccessioningSession("test user", "", "RP-1", Boolean.TRUE);
+    }
+
+    /**
+     * Test that a valid request returns the ID for a new manifest manifest.
+     */
+    public void testValidRequest() {
+        String username = "test_user";
+        BspUser user = new BspUser();
+        user.setUserId(1L);
+        user.setUsername(username);
+        Mockito.when(userBean.getBspUser()).thenReturn(user);
+        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
+        long expectedManifestId = 1L;
+        ManifestSession manifestSession = new ManifestSession(expectedManifestId);
+        String researchProjectKey = "RP-1";
+        String manifestName = "test manifest";
+
+        Mockito.when(manifestSessionEjb.createManifestSession(researchProjectKey, manifestName, user)).thenReturn(
+                manifestSession);
+
+        long manifestId =
+                clinicalResource.createAccessioningSession(username, manifestName, researchProjectKey, Boolean.TRUE);
+
+        Mockito.verify(userBean).login(username);
+        assertThat(manifestId, equalTo(expectedManifestId));
     }
 }
--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/sample/ClinicalResourceTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/sample/ClinicalResourceTest.java
@@ -63,7 +63,7 @@ public class ClinicalResourceTest extends RestServiceContainerTest {
     @Test(dataProvider = Arquillian.ARQUILLIAN_DATA_PROVIDER)
     @RunAsClient
     public void testWebService(@ArquillianResource URL baseUrl) throws Exception {
-        Map<String,Object> postBody = new HashMap<String,Object>();
+        Map<String, Object> postBody = new HashMap<>();
         postBody.put("username", QA_DUDE_PM);
         postBody.put("manifestId", MANIFEST_ID);
 
@@ -79,7 +79,8 @@ public class ClinicalResourceTest extends RestServiceContainerTest {
         MultivaluedMap formData = new MultivaluedMapImpl();
         formData.add("name1", "val1");
         formData.add("name2", "val2");
-        ClientResponse response = resource.type("application/x-www-form-urlencoded").post(ClientResponse.class, formData);
+        ClientResponse response =
+                resource.type("application/x-www-form-urlencoded").post(ClientResponse.class, formData);
 
         resource.entity(postBody).post(new GenericType<List<Sample>>() { });
 
@@ -94,8 +95,7 @@ public class ClinicalResourceTest extends RestServiceContainerTest {
                 ClinicalSampleFactory.createCrspSample(ImmutableMap
                         .of(Metadata.Key.PATIENT_ID, "004-002", Metadata.Key.SAMPLE_ID, "03101231193")),
                 ClinicalSampleFactory.createCrspSample(ImmutableMap
-                                .of(Metadata.Key.PATIENT_ID, "001-001", Metadata.Key.SAMPLE_ID, "03101067213")
-                ));
+                        .of(Metadata.Key.PATIENT_ID, "001-001", Metadata.Key.SAMPLE_ID, "03101067213")));
 
         clinicalResource.addSamplesToManifest(QA_DUDE_PM, MANIFEST_ID, samples);
 
@@ -110,111 +110,3 @@ public class ClinicalResourceTest extends RestServiceContainerTest {
         return "clinical";
     }
 }
-
-
-//package org.broadinstitute.gpinformatics.mercury.boundary.sample;
-//
-//        import org.broadinstitute.bsp.client.users.BspUser;
-//        import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
-//        import org.broadinstitute.gpinformatics.mercury.boundary.UnknownUserException;
-//        import org.broadinstitute.gpinformatics.mercury.boundary.manifest.ManifestSessionEjb;
-//        import org.broadinstitute.gpinformatics.mercury.entity.sample.ManifestSession;
-//        import org.broadinstitute.gpinformatics.mercury.presentation.UserBean;
-//        import org.mockito.Mockito;
-//        import org.testng.annotations.BeforeMethod;
-//        import org.testng.annotations.Test;
-//
-//        import static org.hamcrest.CoreMatchers.equalTo;
-//        import static org.hamcrest.MatcherAssert.assertThat;
-//
-//@Test(groups = TestGroups.DATABASE_FREE)
-//public class ClinicalResourceTest {
-//
-//    private ClinicalResource clinicalResource;
-//    private UserBean userBean;
-//    private ManifestSessionEjb manifestSessionEjb;
-//
-//    @BeforeMethod
-//    public void setUp() throws Exception {
-//        userBean = Mockito.mock(UserBean.class);
-//        manifestSessionEjb = Mockito.mock(ManifestSessionEjb.class);
-//        clinicalResource = new ClinicalResource(userBean, manifestSessionEjb);
-//    }
-//
-//    /**
-//     * Test that passing in <code>false</code> for isFromSampleKit throws an unsupported operation exception. This is
-//     * intended for future use for Buick-like projects. For now, we only support <code>true</code>, which is for the
-//     * workflow we're supporting at this time for clinical diagnostics.
-//     */
-//    @Test(expectedExceptions = UnsupportedOperationException.class)
-//    public void testCreateManifestNotFromSampleKit() {
-//        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
-//        clinicalResource.createAccessioningSession("test_user", "test manifest", "RP-1", Boolean.FALSE);
-//    }
-//
-//    /**
-//     * Test that passing in an unknown user throws an UnknownUserException. As tested elsewhere, this will result in a
-//     * 403 response.
-//     */
-//    @Test(expectedExceptions = UnknownUserException.class)
-//    public void testCreateManifestUnknownUser() {
-//        /*
-//         * Using default Mockito behavior of returning "false" for non-stubbed methods, in this case
-//         * BspUser.isValidBspUser()
-//         */
-//
-//        clinicalResource.createAccessioningSession("unknown_user", "test manifest", "RP-1", Boolean.TRUE);
-//    }
-//
-//    /**
-//     * Test that the call fails if isFromSampleKit is not passed in.
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void testCreateManifestWithIsFromSampleKitNull() {
-//        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
-//        clinicalResource.createAccessioningSession("test_user", "test manifest", "RP-1", null);
-//    }
-//
-//    /**
-//     * Test that the call fails if a null manifest name is provided.
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void testCreateManifestNullManifestName() {
-//        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
-//        clinicalResource.createAccessioningSession("test user", null, "RP-1", Boolean.TRUE);
-//    }
-//
-//    /**
-//     * Test that the call fails if an empty manifest name is provided.
-//     */
-//    @Test(expectedExceptions = IllegalArgumentException.class)
-//    public void testCreateManifestEmptyManifestName() {
-//        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
-//        clinicalResource.createAccessioningSession("test user", "", "RP-1", Boolean.TRUE);
-//    }
-//
-//    /**
-//     * Test that a valid request returns the ID for a new manifest manifest.
-//     */
-//    public void testValidRequest() {
-//        String username = "test_user";
-//        BspUser user = new BspUser();
-//        user.setUserId(1L);
-//        user.setUsername(username);
-//        Mockito.when(userBean.getBspUser()).thenReturn(user);
-//        Mockito.when(userBean.isValidBspUser()).thenReturn(true);
-//        long expectedManifestId = 1L;
-//        ManifestSession manifestSession = new ManifestSession(expectedManifestId);
-//        String researchProjectKey = "RP-1";
-//        String manifestName = "test manifest";
-//
-//        Mockito.when(manifestSessionEjb.createManifestSession(researchProjectKey, manifestName, user)).thenReturn(
-//                manifestSession);
-//
-//        long manifestId =
-//                clinicalResource.createAccessioningSession(username, manifestName, researchProjectKey, Boolean.TRUE);
-//
-//        Mockito.verify(userBean).login(username);
-//        assertThat(manifestId, equalTo(expectedManifestId));
-//    }
-//}
