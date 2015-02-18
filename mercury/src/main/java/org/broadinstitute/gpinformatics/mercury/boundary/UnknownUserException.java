e4e876195e0fba0221601c3b10a6b28b935837d7
Brian Reilly <breilly@broadinstitute.org>
2015-02-18 14:23:18 -0500

    GPLIM-3328 Added javadoc for UnknownUserException, including a note about the choice of FORBIDDEN over UNAUTHORIZED.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/UnknownUserException.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/UnknownUserException.java
@@ -3,13 +3,16 @@ package org.broadinstitute.gpinformatics.mercury.boundary;
 import javax.ws.rs.core.Response;
 
 /**
+ * An exception to be thrown from a web service when a user requesting an operation is not known. This will map to an
+ * HTTP 403 "FORBIDDEN" response, indicating that the user, whoever they are, is not allowed access to the web service.
+ * Note that the other option, 401 "UNAUTHORIZED", would imply that we know who the user is and we are denying access
+ * for that user.
  */
 public class UnknownUserException extends ResourceException {
 
     private final String username;
 
     public UnknownUserException(String username) {
-        //todo: FORBIDDEN or UNAUTHORIZED?
         super(Response.Status.FORBIDDEN);
         this.username = username;
     }
