dc0f4e5ec0cbb60bd7757849a522bddd113cbc80
Brian Reilly <breilly@broadinstitute.org>
2015-02-20 16:28:37 -0500

    GPLIM-3328 Revived some changes that I'd made to Metadata.Key that were previously lost in a merge.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/Metadata.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/Metadata.java
@@ -176,14 +176,68 @@ public class Metadata {
     }
 
     public enum Key implements Displayable {
-        // The Category.SAMPLE keys are currently all used for uploads of the "modified" (edited) manifest during
-        // Buick sample registration.
-        GENDER(Category.SAMPLE, DataType.STRING, "Gender", Visibility.USER),
-        PATIENT_ID(Category.SAMPLE, DataType.STRING, "Patient ID", Visibility.USER),
-        SAMPLE_TYPE(Category.SAMPLE, DataType.STRING, "Sample Type", Visibility.USER),
-        TUMOR_NORMAL(Category.SAMPLE, DataType.STRING, "Tumor/Normal", Visibility.USER),
-        BUICK_COLLECTION_DATE(Category.SAMPLE, DataType.STRING, "Collection Date", Visibility.USER),
+
+        /**
+         * The collaborator-assigned sample ID.
+         */
         SAMPLE_ID(Category.SAMPLE, DataType.STRING, "Sample ID", Visibility.USER),
+
+        /**
+         * The Broad-assigned sample ID (e.g., an SM- ID).
+         */
+        BROAD_SAMPLE_ID(Category.SAMPLE, DataType.STRING, "Broad Sample ID", Visibility.SYSTEM),
+
+        /**
+         * The manufacturer barcode on the sample's container.
+         */
+        BROAD_2D_BARCODE(Category.SAMPLE, DataType.STRING, "2D Barcode", Visibility.SYSTEM),
+
+        /**
+         * The sample's material type, e.g. DNA, Blood, FFPE, etc.
+         */
+        MATERIAL_TYPE(Category.SAMPLE, DataType.STRING, "Material Type", Visibility.USER),
+
+        /**
+         * The type of material from which the sample was derived.
+         */
+        ORIGINAL_MATERIAL_TYPE(Category.SAMPLE, DataType.STRING, "Original Material Type", Visibility.USER),
+
+        /**
+         * A unique ID for the patient. Should not contain PHI.
+         */
+        PATIENT_ID(Category.SAMPLE, DataType.STRING, "Patient ID", Visibility.USER),
+
+        /**
+         * The gender of the patient.
+         */
+        GENDER(Category.SAMPLE, DataType.STRING, "Gender", Visibility.USER),
+
+        /**
+         * The type of sample, tumor or normal. Sometimes called "Sample Type" in other systems.
+         */
+        TUMOR_NORMAL(Category.SAMPLE, DataType.STRING, "Tumor/Normal", Visibility.USER),
+
+        /**
+         * The estimated % of tumor in the sample.
+         */
+        PERCENT_TUMOR(Category.SAMPLE, DataType.STRING, "Estimated % Tumor", Visibility.USER),
+
+        /**
+         * The date that the sample was collected.
+         */
+        COLLECTION_DATE(Category.SAMPLE, DataType.STRING, "Collection Date", Visibility.USER), // DataType.DATE?
+
+        /**
+         * The date that the sample was shipped to Broad.
+         */
+        SHIPMENT_DATE(Category.SAMPLE, DataType.STRING, "Shipment Date", Visibility.USER), // DataType.DATE?
+
+        /**
+         * BUICK_COLLECTION_DATE is stored separately from COLLECTION_DATE because we intend to not use this data for
+         * any reason except to pass-through to the final report. Since this is something that the collaborator asked us
+         * to store and repeat back, there is no guarantee that it has the same meaning as our own COLLECTION_DATE.
+         */
+        BUICK_COLLECTION_DATE(Category.SAMPLE, DataType.STRING, "Collection Date", Visibility.USER),
         BUICK_VISIT(Category.SAMPLE, DataType.STRING, "Visit", Visibility.USER),
 
         CORRELATION_COEFFICIENT_R2(Category.LAB_METRIC_RUN, DataType.STRING, "R Squared Correlation Coefficient",
@@ -191,9 +245,7 @@ public class Metadata {
         INSTRUMENT_NAME(Category.LAB_METRIC_RUN, DataType.STRING, "Instrument Name", Visibility.USER),
         INSTRUMENT_SERIAL_NUMBER(Category.LAB_METRIC_RUN, DataType.STRING, "Serial Number", Visibility.USER),
 
-        TOTAL_NG(Category.LAB_METRIC, DataType.NUMBER, "Total ng", Visibility.USER),
-        BROAD_SAMPLE_ID(Category.SAMPLE, DataType.STRING, "SM ID", Visibility.SYSTEM),
-        BROAD_2D_BARCODE(Category.SAMPLE, DataType.STRING, "2D Barcode", Visibility.SYSTEM);
+        TOTAL_NG(Category.LAB_METRIC, DataType.NUMBER, "Total ng", Visibility.USER);
 
         private final Category category;
         private final DataType dataType;
