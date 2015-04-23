50fbfe7157474c8b410141b3be9b0495b03a9f53
Scott Matthews <scottmat@broadinstitute.org>
2015-04-23 09:20:46 -0400

    SUPPORT-563, GPLIM-2719 Removing duplicate unnecessary stub class.  adding extra test cases to cover added price item lists

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/integration/quotes/PriceListCacheContainerTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/integration/quotes/PriceListCacheContainerTest.java
@@ -1,6 +1,7 @@
 package org.broadinstitute.gpinformatics.mercury.integration.quotes;
 
 import org.broadinstitute.gpinformatics.infrastructure.quote.PriceListCache;
+import org.broadinstitute.gpinformatics.infrastructure.quote.QuotePlatformType;
 import org.broadinstitute.gpinformatics.infrastructure.quote.QuotePriceItem;
 import org.broadinstitute.gpinformatics.infrastructure.quote.QuoteServiceProducer;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
@@ -22,6 +23,9 @@ public class PriceListCacheContainerTest {
         Assert.assertNotNull(quotePriceItems);
         Assert.assertTrue(quotePriceItems.size() > 10);
 
+        int crspItemCount = 0;
+        int externalListCount = 0;
+
         for (QuotePriceItem quotePriceItem : quotePriceItems) {
             Assert.assertNotNull(quotePriceItem.getPlatformName(), quotePriceItem.toString());
             // category actually can be null
@@ -29,6 +33,15 @@ public class PriceListCacheContainerTest {
             Assert.assertNotNull(quotePriceItem.getName(), quotePriceItem.toString());
             Assert.assertNotNull(quotePriceItem.getSubmittedDate(), quotePriceItem.toString());
             Assert.assertNotNull(quotePriceItem.getEffectiveDate(), quotePriceItem.toString());
+            if(quotePriceItem.getPlatformName().equals(QuotePlatformType.CRSP.getPlatformName())) {
+                crspItemCount++;
+            } else if(quotePriceItem.getPlatformName().equals(QuotePlatformType.GSP.getPlatformName())) {
+                externalListCount++;
+            }
         }
+        System.out.println("The crsp item count is " + crspItemCount);
+        System.out.println("The external item count is " + externalListCount);
+        Assert.assertTrue(crspItemCount > 0);
+        Assert.assertTrue(externalListCount > 0);
     }
 }
