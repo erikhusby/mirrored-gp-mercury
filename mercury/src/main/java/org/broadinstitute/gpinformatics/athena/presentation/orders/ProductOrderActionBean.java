7740b27c670e52574f7788c348afee2ef3b9698a
Brian Reilly <breilly@broadinstitute.org>
2015-05-05 15:58:53 -0400

    GPLIM-3550 Cause a validation error when saving a pending PDO where the regulatory information is being skipped but no reason has been supplied.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/orders/ProductOrderActionBean.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/orders/ProductOrderActionBean.java
@@ -599,6 +599,9 @@ public class ProductOrderActionBean extends CoreActionBean {
         }
 
         requireField(researchProject, "a Research Project", action);
+
+        validateRegulatoryInformation(action);
+
         if (!ApplicationInstance.CRSP.isCurrent()) {
             validateQuoteOptions(action);
         }
@@ -686,13 +689,11 @@ public class ProductOrderActionBean extends CoreActionBean {
         doValidation(action);
         if (!hasErrors()) {
             doOnRiskUpdate();
-        }
-        if (!hasErrors()) {
-            validateRegulatoryInformation(action);
-        }
 
-        if (!hasErrors()) {
-            updateFromInitiationTokenInputs();
+            // doOnRiskUpdate() can add errors, so check again.
+            if (!hasErrors()) {
+                updateFromInitiationTokenInputs();
+            }
         }
     }
 
