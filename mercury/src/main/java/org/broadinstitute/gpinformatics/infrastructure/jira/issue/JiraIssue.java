d27d12939dd2f49742fdc7150ab27c09db1dba35
Scott Matthews <scottmat@broadinstitute.org>
2015-06-03 21:31:32 -0400

    GPLIM-3487 Code review comments.  Forgot to set created.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/jira/issue/JiraIssue.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/jira/issue/JiraIssue.java
@@ -118,6 +118,7 @@ public class JiraIssue implements Serializable {
         summary = tempIssue.getSummary();
         description = tempIssue.getDescription();
         dueDate = tempIssue.getDueDate();
+        created = tempIssue.getCreated();
         reporter = tempIssue.getReporter();
     }
 
