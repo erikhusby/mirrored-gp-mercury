f5200b1e880493ea7f72bc238644541e100f3513
Erik Husby <mhusby@broadinstitute.org>
2015-07-20 15:46:46 -0400

    Revert "Merge branch 'GPLIM-3645_PlatePositionMap' into release/1.62"
    This reverts commit 2acdc549e963c264fd1b876776063ff4b74a6c4c, reversing
    changes made to 3c3dcb2b6052adbceef0566350b788fe164578bd.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/ReagentFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/ReagentFixupTest.java
@@ -561,37 +561,4 @@ public class ReagentFixupTest extends Arquillian {
         genericReagentDao.persist(new FixupCommentary("GPLIM-3588 change reagent used on Ice Capture 2 event."));
         genericReagentDao.flush();
     }
-
-    @Test(enabled = false)
-    public void fixupGplim3629() {
-        userBean.loginOSUser();
-        // Changes the reagent used on labEvents that were determined from sql queries (see ticket).
-        Map<Long, Long> eventIdToUndesiredReagentIds = new HashMap<Long, Long>() {{
-            put(939935L, 959960L);
-            put(939937L, 959960L);
-            put(939938L, 959960L);
-            put(939940L, 959960L);
-            put(939943L, 959960L);
-            put(939944L, 959960L);
-            put(939948L, 959965L);
-        }};
-        Long desiredReagentId = 954982L;
-
-        Reagent desired = genericReagentDao.findById(Reagent.class, desiredReagentId);
-        Assert.assertNotNull(desired, "reagent " + desiredReagentId);
-        for (Map.Entry<Long, Long> entry : eventIdToUndesiredReagentIds.entrySet()) {
-            LabEvent labEvent = labEventDao.findById(LabEvent.class, entry.getKey());
-            Assert.assertNotNull(labEvent, "event " + entry.getKey());
-            Reagent undesired = genericReagentDao.findById(Reagent.class, entry.getValue());
-            Assert.assertNotNull(undesired, "reagent " + entry.getValue());
-            // Removes undesired reagent and add desired reagent.
-            Assert.assertTrue(labEvent.getReagents().remove(undesired),
-                    "event " + entry.getKey() + " reagent " + entry.getValue());
-            labEvent.addReagent(desired);
-        }
-
-        genericReagentDao.persist(new FixupCommentary("GPLIM-3629 change reagent used with flowcell events."));
-        genericReagentDao.flush();
-    }
-
 }
