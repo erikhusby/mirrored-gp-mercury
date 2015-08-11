61065132be8840359508161728f63c5a0c6a2200
Scott Matthews <scottmat@broadinstitute.org>
2015-08-11 15:29:33 -0400

    GPLIM-3696 beggining to add service to push products to Salesforce.

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/salesforce/SalesforceService.java
@@ -0,0 +1,15 @@
+package org.broadinstitute.gpinformatics.infrastructure.salesforce;
+
+import com.sun.jersey.api.client.Client;
+import org.broadinstitute.gpinformatics.mercury.control.AbstractJerseyClientService;
+import org.broadinstitute.gpinformatics.mercury.control.AbstractJsonJerseyClientService;
+
+/**
+ * TODO scottmat fill in javadoc!!!
+ */
+public class SalesforceService extends AbstractJsonJerseyClientService {
+    @Override
+    protected void customizeClient(Client client) {
+
+    }
+}
