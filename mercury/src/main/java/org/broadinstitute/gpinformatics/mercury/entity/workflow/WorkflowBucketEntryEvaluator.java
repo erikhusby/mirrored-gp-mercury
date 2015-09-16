f095fb5f9891687474a9e2cf77302847bda44d40
Dennis Ryan <dryan@broadinstitute.org>
2015-09-16 11:49:25 -0400

    GPLIM-3736 fix some workflows

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/workflow/WorkflowBucketEntryEvaluator.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/workflow/WorkflowBucketEntryEvaluator.java
@@ -13,7 +13,6 @@ package org.broadinstitute.gpinformatics.mercury.entity.workflow;
 
 import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrder;
 import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrderAddOn;
-import org.broadinstitute.gpinformatics.athena.entity.products.Product;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -51,51 +50,21 @@ public class WorkflowBucketEntryEvaluator implements Serializable {
     }
 
     private boolean productAddOnsHaveWorkflow(ProductOrder productOrder) {
-        if (workflows.isEmpty()) {
+        if (workflows.isEmpty()){
             return true;
         }
-        for (ProductOrderAddOn orderAddon : productOrder.getAddOns()) {
-            if (productHasWorkflow(orderAddon.getAddOn())) {
-                return true;
+        for (ProductOrderAddOn productOrderAddOn : productOrder.getAddOns()) {
+            Workflow addOnWorkflow = productOrderAddOn.getAddOn().getWorkflow();
+            if (addOnWorkflow != Workflow.NONE) {
+                if (workflows.contains(addOnWorkflow)) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    public Workflow findMatchingWorkflow(ProductOrder productOrder) {
-        for (ProductOrderAddOn productOrderAddOn : productOrder.getAddOns()) {
-            Workflow addonWorkflow = findMatchingWorkflow(productOrderAddOn.getAddOn());
-            if (addonWorkflow != Workflow.NONE) {
-                return addonWorkflow;
-            }
-        }
-        return Workflow.NONE;
-    }
-
-    public Workflow findMatchingWorkflow(Product product) {
-        Workflow productWorkflow = product.getWorkflow();
-        if (productWorkflow != Workflow.NONE) {
-            if (workflows.contains(productWorkflow)) {
-                return productWorkflow;
-            }
-        }
-        return Workflow.NONE;
-    }
-
-    public boolean productHasWorkflow(Product product) {
-        if (workflows.isEmpty()){
-            return true;
-        }
-        return findMatchingWorkflow(product)!=Workflow.NONE;
-    }
-
     public boolean invoke(LabVessel labVessel, ProductOrder productOrder) {
         return productAddOnsHaveWorkflow(productOrder) && materialTypeMatches(labVessel);
     }
-
-
-    public boolean invoke(LabVessel labVessel, Product product) {
-        return productHasWorkflow(product) && materialTypeMatches(labVessel);
-    }
-
 }
