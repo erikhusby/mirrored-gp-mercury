ab24f07b8611a9215969d828cad91e4994e82f81
Jon Thompson <thompson@broadinstitute.org>
2015-09-21 13:41:56 -0400

    GPLIM-3707 replace composite key with sequence number, required for audit tables

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/labevent/LabEventReagent.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/labevent/LabEventReagent.java
@@ -3,12 +3,17 @@ package org.broadinstitute.gpinformatics.mercury.entity.labevent;
 import org.broadinstitute.gpinformatics.mercury.entity.reagent.Reagent;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.EmbeddedId;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.math.BigDecimal;
 
 /**
@@ -16,19 +21,23 @@ import java.math.BigDecimal;
  */
 @Entity
 @Audited
-@Table(schema = "mercury", name = "lab_event_reagents")
+@Table(schema = "mercury", name = "lab_event_reagents",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"LAB_EVENT", "REAGENTS"})
+)
 public class LabEventReagent {
 
-    @EmbeddedId
-    private LabEventReagentKey labEventReagentKey = new LabEventReagentKey();
+    @SuppressWarnings("unused")
+    @Id
+    @SequenceGenerator(name = "SEQ_LAB_EVENT_REAGENT", schema = "mercury", sequenceName = "SEQ_LAB_EVENT_REAGENT")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_LAB_EVENT_REAGENT")
+    @Column(name = "LAB_EVENT_REAGENT_ID")
+    private Long labEventReagentId;
 
-    @MapsId("labEvent")
     @ManyToOne
     @JoinColumn(name = "LAB_EVENT")
     private LabEvent labEvent;
 
-    @MapsId("reagents")
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(name = "REAGENTS")
     private Reagent reagent;
 
