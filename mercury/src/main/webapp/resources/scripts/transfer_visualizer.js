49f377587f7442e36dd4fe9d5f7b8fe3406678cf
Jon Thompson <thompson@broadinstitute.org>
2015-10-29 16:49:12 -0400

    GPLIM-3682 wrap edge labels

--- mercury/src/main/webapp/resources/scripts/transfer_visualizer.js
+++ mercury/src/main/webapp/resources/scripts/transfer_visualizer.js
@@ -236,7 +236,36 @@ function renderJson(json, svg) {
                 return (d.targetY + (d.sourceY - d.targetY)/2);
             }
         })
+        .attr("dx", 0)
+        .attr("dy", 0)
         .text(function(d) {
             return d.label;
         });
+    svg.selectAll(".graphEdgeLabel")
+        .call(wrap,  150);
+
+    function wrap(text, width) {
+        text.each(function() {
+            var text = d3.select(this),
+                words = text.text().split(/\s+/).reverse(),
+                word,
+                line = [],
+                lineNumber = 0,
+                lineHeight = 1.1, // ems
+                x = text.attr("x"),
+                y = text.attr("y"),
+                dy = parseFloat(text.attr("dy")),
+                tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+            while (word = words.pop()) {
+                line.push(word);
+                tspan.text(line.join(" "));
+                if (tspan.node().getComputedTextLength() > width) {
+                    line.pop();
+                    tspan.text(line.join(" "));
+                    line = [word];
+                    tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                }
+            }
+        });
+    }
 }
