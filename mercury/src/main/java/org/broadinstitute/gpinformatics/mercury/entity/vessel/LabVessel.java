9fcd351f5f9a53c6f02d85a156ccd4b69a749c6f
Dennis Ryan <dryan@broadinstitute.org>
2015-11-06 15:04:07 -0500

    GPLIM-3803: bulk populate bsp sample with metadata once

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/LabVessel.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/LabVessel.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.broadinstitute.bsp.client.users.BspUser;
 import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrder;
+import org.broadinstitute.gpinformatics.infrastructure.SampleData;
 import org.broadinstitute.gpinformatics.infrastructure.SampleDataFetcher;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPSampleSearchColumn;
 import org.broadinstitute.gpinformatics.infrastructure.common.MathUtils;
@@ -286,7 +287,10 @@ public abstract class LabVessel implements Serializable {
                 sampleNames.put(mercurySample.getSampleKey(), mercurySample);
             }
         }
-        sampleDataFetcher.fetchSampleDataForMercurySamples(sampleNames, BSPSampleSearchColumn.BUCKET_PAGE_COLUMNS);
+        Map<String, SampleData> sampleDataMap = sampleDataFetcher.fetchSampleDataForMercurySamples(sampleNames, BSPSampleSearchColumn.BUCKET_PAGE_COLUMNS);
+        for (Map.Entry<String, SampleData> sampleDataEntry : sampleDataMap.entrySet()) {
+            sampleNames.get(sampleDataEntry.getKey()).setSampleData(sampleDataEntry.getValue());
+        }
     }
 
     /**
