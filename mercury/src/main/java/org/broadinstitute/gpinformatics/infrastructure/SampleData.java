f9ea40a8f3409527dab648fa0981a1167667fd99
Brian Reilly <breilly@broadinstitute.org>
2015-11-19 15:59:55 -0500

    GPLIM-3886 Made samples with sample data in Mercury valid for RIN risk checks.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/SampleData.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/SampleData.java
@@ -11,12 +11,36 @@ import java.util.Date;
 public interface SampleData {
     boolean hasData();
 
+    /**
+     * Determines whether or not this sample data contains a RIN score that is a valid number that can be used in a risk
+     * calculation. Returning false indicates that there is an intent to have a RIN score but no meaningful checks can
+     * be made against it. In that case, in order to avoid silently allowing an out-of-spec sample from being processed,
+     * work on the sample should probably not proceed.
+     *
+     * @return true if there is a valid RIN score (including no score); false otherwise
+     */
     boolean canRinScoreBeUsedForOnRiskCalculation();
 
     Date getPicoRunDate();
 
+    /**
+     * Returns a string representing the RIN score. This is character rather than numeric data because it is sometimes
+     * recorded as a range.
+     *
+     * @return the raw RIN score
+     */
     String getRawRin();
 
+    /**
+     * Get the RIN value for the sample. Returns null if no RIN value is set. If the RIN value is expressed as a range,
+     * the lower value is returned. Throws NumberFormatException if the value cannot be parsed as a single value or as a
+     * range.
+     *
+     * @return the (lowest) RIN value from BSP; null if no value is set
+     * @throws NumberFormatException if the value cannot be parsed as a single value or a range
+     *
+     * @see #getRawRin() to get the unmodified rin value as a string.
+     */
     Double getRin();
 
     Double getRqs();
