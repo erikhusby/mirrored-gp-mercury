6d08736f3d1d1989b128d91ae061c907dd797c42
jowalsh <jowalsh@broadinstitute.org>
2015-12-08 14:29:06 -0500

    GPLIM-3917 fixup incorrect P5 adapter expiration

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/ReagentFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/reagent/ReagentFixupTest.java
@@ -780,4 +780,38 @@ public class ReagentFixupTest extends Arquillian {
         genericReagentDao.flush();
         utx.commit();
     }
+
+    @Test(enabled = false)
+    public void fixupGplim3917date() throws Exception {
+        userBean.loginOSUser();
+
+        // Replaces wrong expiration for P5 Indexed Adapter Plate reagent on event 1121392
+        // with a new reagent having barcode 000001818323, exp 05/21/16
+        LabEvent labEvent = genericReagentDao.findById(LabEvent.class, 1121392L);
+        Assert.assertNotNull(labEvent);
+
+        String kitType = "P5 Indexed Adapter Plate";
+        String barcode = "000001818323";
+        Reagent undesired = null;
+        for (Reagent reagent : labEvent.getReagents()) {
+            if (reagent.getName().equals(kitType)) {
+                Assert.assertNull(undesired);
+                undesired = reagent;
+            }
+        }
+        Assert.assertNotNull(undesired);
+        Assert.assertEquals(barcode, undesired.getLot());
+
+        Date expiration = new GregorianCalendar(2016, Calendar.MAY, 21).getTime();
+        Assert.assertNull(genericReagentDao.findByReagentNameLotExpiration(kitType, barcode, expiration));
+        Reagent desired = new GenericReagent(kitType, barcode, expiration);
+        System.out.println("Created reagent " + kitType + " lot " + barcode + " expiration " + expiration);
+        Assert.assertTrue(labEvent.getReagents().remove(undesired));
+        labEvent.getReagents().add(desired);
+        System.out.println("Reagent " + undesired.getReagentId() + " removed and " + desired.getReagentId() +
+                           " added to event " + labEvent.getLabEventId());
+
+        genericReagentDao.persist(new FixupCommentary("GPLIM-3917 fixup incorrect P5 adapter expiration"));
+        genericReagentDao.flush();
+    }
 }
