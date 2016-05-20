5fe1014f5fb717dd9f09e61fff79c3351211deb8
Dennis Ryan <dryan@broadinstitute.org>
2016-05-20 14:52:22 -0400

    GPLIM-4021: remove duplicate copywright notice

--- mercury/src/main/webapp/resources/scripts/dataTables-filterColumn.js
+++ mercury/src/main/webapp/resources/scripts/dataTables-filterColumn.js
@@ -1,17 +1,6 @@
 /*
  * The Broad Institute
  * SOFTWARE COPYRIGHT NOTICE AGREEMENT
- * This software and its documentation are copyright 2016 by the 
- * Broad Institute/Massachusetts Institute of Technology. All rights are reserved.
- *
- * This software is supplied without any warranty or guaranteed support 
- * whatsoever. Neither the Broad Institute nor MIT can be responsible for its 
- * use, misuse, or functionality.
- */
-
-/*
- * The Broad Institute
- * SOFTWARE COPYRIGHT NOTICE AGREEMENT
  * This software and its documentation are copyright 2016 by the
  * Broad Institute/Massachusetts Institute of Technology. All rights are reserved.
  *
