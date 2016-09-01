b7a9118ea30ec492f3c1292ee597c249227bdb9c
Brian Reilly <breilly@broadinstitute.org>
2016-09-01 10:45:29 -0400

    GPLIM-4248 Added OrspProject entity to exclude list for HibernateMetadataTest.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/test/HibernateMetadataTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/test/HibernateMetadataTest.java
@@ -12,6 +12,7 @@
 package org.broadinstitute.gpinformatics.infrastructure.test;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.broadinstitute.gpinformatics.infrastructure.cognos.entity.OrspProject;
 import org.broadinstitute.gpinformatics.infrastructure.cognos.entity.SampleCoverageFirstMet;
 import org.broadinstitute.gpinformatics.infrastructure.metrics.entity.Aggregation;
 import org.broadinstitute.gpinformatics.infrastructure.metrics.entity.AggregationAlignment;
@@ -59,9 +60,10 @@ public class HibernateMetadataTest extends ContainerTest {
             AggregationHybridSelection.class.getName(),
             AggregationReadGroup.class.getName(),
             AggregationWgs.class.getName(),
-            SampleCoverageFirstMet.class.getName(),
+            OrspProject.class.getName(),
             PicardAnalysis.class.getName(),
-            PicardFingerprint.class.getName()
+            PicardFingerprint.class.getName(),
+            SampleCoverageFirstMet.class.getName()
     };
 
     /**
