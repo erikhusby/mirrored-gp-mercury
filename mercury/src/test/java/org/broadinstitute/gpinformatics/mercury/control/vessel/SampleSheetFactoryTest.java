8244b9a78ffd3cb14f7ade560fcbc6b4ce090a6b
Jon Thompson <thompson@broadinstitute.org>
2016-10-12 15:29:25 -0400

    GPLIM-4341 clean up test

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/vessel/SampleSheetFactoryTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/control/vessel/SampleSheetFactoryTest.java
@@ -1,10 +1,7 @@
 package org.broadinstitute.gpinformatics.mercury.control.vessel;
 
-//import com.jprofiler.api.agent.Controller;
-
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.lang3.tuple.Pair;
-import org.broadinstitute.bsp.client.util.MessageCollection;
 import org.broadinstitute.gpinformatics.athena.control.dao.orders.ProductOrderDao;
 import org.broadinstitute.gpinformatics.athena.entity.orders.ProductOrder;
 import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
@@ -15,6 +12,7 @@ import org.broadinstitute.gpinformatics.mercury.entity.vessel.VesselPosition;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.inject.Inject;
@@ -45,23 +43,14 @@ public class SampleSheetFactoryTest extends Arquillian {
 
     @Test
     public void testBasics() {
-//        Controller.startCPURecording(true);
-//        Controller.startProbeRecording(Controller.PROBE_NAME_JDBC, true);
         ProductOrder productOrder = productOrderDao.findByBusinessKey("PDO-6743");
         List<Pair<LabVessel, VesselPosition>> vesselPositionPairs = sampleSheetFactory.loadByPdo(
                 productOrder);
-        MessageCollection messageCollection = new MessageCollection();
+        Assert.assertEquals(vesselPositionPairs.size(), 288);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        List<Pair<LabVessel, VesselPosition>> vesselPositionPairs = new ArrayList<>();
-//        StaticPlate chip = staticPlateDao.findByBarcode("200598720152");
-//        for (VesselPosition vesselPosition : chip.getVesselGeometry().getVesselPositions()) {
-//            vesselPositionPairs.add(new ImmutablePair<LabVessel, VesselPosition>(chip, vesselPosition));
-//        }
         sampleSheetFactory.write(new PrintStream(byteArrayOutputStream), vesselPositionPairs,
                 productOrder.getResearchProject());
         System.out.println(byteArrayOutputStream.toString());
         // todo jmt asserts
-//        Controller.stopProbeRecording(Controller.PROBE_NAME_JDBC);
-//        Controller.stopCPURecording();
     }
 }
