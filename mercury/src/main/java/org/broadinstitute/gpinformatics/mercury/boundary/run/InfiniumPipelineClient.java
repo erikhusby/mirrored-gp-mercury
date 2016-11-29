39fec933df4126a3b9e3afb6e10b7eb3c2eebf6b
jsacco <jsacco@broadinstitute.org>
2016-11-29 11:20:41 -0500

    GPLIM-3937 Add CDI annotation to injected class

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/run/InfiniumPipelineClient.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/run/InfiniumPipelineClient.java
@@ -1,16 +1,11 @@
 package org.broadinstitute.gpinformatics.mercury.boundary.run;
 
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.broadinstitute.gpinformatics.infrastructure.deployment.InfiniumStarterConfig;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.StaticPlate;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.VesselPosition;
-import org.apache.activemq.artemis.api.core.TransportConfiguration;
-import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient;
-import org.apache.activemq.artemis.api.jms.JMSFactoryType;
-import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
-import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants;
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
@@ -18,7 +13,6 @@ import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.JMSException;
-import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
@@ -26,6 +20,7 @@ import javax.jms.Session;
 /**
  * Forward finished Infinium chip wells to the pipeline for analysis.
  */
+@Dependent
 public class InfiniumPipelineClient {
 
     private static final Log log = LogFactory.getLog(InfiniumPipelineClient.class);
@@ -45,7 +40,6 @@ public class InfiniumPipelineClient {
             String url = String.format(
                     "tcp://%s:%d", infiniumStarterConfig.getJmsHost(), infiniumStarterConfig.getJmsPort());
             ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
-            connectionFactory.setUseAsyncSend(false);
             connection = connectionFactory.createQueueConnection();
 
             connection.start();
