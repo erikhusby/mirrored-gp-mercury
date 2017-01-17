93c97d79f7abe3fb0fe2701a55871efac4ee9b86
Jon Thompson <thompson@broadinstitute.org>
2017-01-17 13:26:44 -0500

    SUPPORT-2461 fixup bucket entries for Mercury pilot, which are being topped off in Squid.

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/workflow/LabBatchFixUpTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/workflow/LabBatchFixUpTest.java
@@ -1231,4 +1231,14 @@ public class LabBatchFixUpTest extends Arquillian {
         labBatchDao.persist(new FixupCommentary("GPLIM-4491 add Jira links from LCSET to FCT"));
         labBatchDao.flush();
     }
+
+    @Test(enabled = false)
+    public void fixupSupport2461() throws Exception{
+        List<String> tubeBarcodes = Arrays.asList(
+                "1147584526",
+                "1147584542",
+                "1147584541");
+
+        changeBucketEntriesToAliquots(tubeBarcodes, 1716867L, "LCSET-10218", "SUPPORT-2461");
+    }
 }
