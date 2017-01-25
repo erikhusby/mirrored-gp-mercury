edb78edfc47a753e94e66e4082838802f5993460
Dennis Ryan <dryan@broadinstitute.org>
2017-01-25 12:56:52 -0500

    GPLIM-4598 Add Paging to View PDO Page

--- mercury/src/main/webapp/resources/scripts/bootstrap-dt.js
+++ mercury/src/main/webapp/resources/scripts/bootstrap-dt.js
@@ -36,6 +36,8 @@ function enableDefaultPagingOptions(){
  *  Set the defaults for DataTables initialization
  */
 $j.extend(true, $j.fn.dataTable.defaults, {
+    'sDom': "<'row-fluid'<'span6'f><'span4'T><'span2'ir>>t<'row-fluid'<'span6'l><'span6'p>>",
+$j.extend( true, $j.fn.dataTable.defaults, {
     "sDom": "<'row-fluid'<'span8'f><'span4'B>r>t<'row-fluid'<'span6'i><'span6'p>>",
     "bAutoWidth": false,
     "bInfo": false,
@@ -50,7 +52,7 @@ $j.extend(true, $j.fn.dataTable.defaults, {
 });
 if (isLegacyDataTables()) {
     $j.extend(true, $j.fn.dataTable.defaults, {
-        "sDom": "<'row-fluid'<'span8'f><'span4'T>r>t<'row-fluid'<'span6'i><'span6'p>>",
+        'sDom': "<'row-fluid'<'span6'f><'span4'T><'span2'ir>>t<'row-fluid'<'span6'l><'span6'p>>"
     });
 }
 
