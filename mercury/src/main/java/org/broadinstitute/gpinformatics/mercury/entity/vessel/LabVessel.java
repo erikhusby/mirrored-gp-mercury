2518263c699f4ff5d803ac7f468ff00621f36f3f
Jon Thompson <thompson@broadinstitute.org>
2017-01-30 16:44:23 -0500

    GPLIM-4396 deleted unused code; TreeSet for sample instances

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/LabVessel.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/vessel/LabVessel.java
@@ -64,7 +64,6 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
