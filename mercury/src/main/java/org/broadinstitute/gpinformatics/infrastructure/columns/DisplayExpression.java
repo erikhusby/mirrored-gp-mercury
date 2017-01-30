8f4c16477413d817a1559ce9506fe7dabd4ee3b8
Jon Thompson <thompson@broadinstitute.org>
2017-01-30 17:06:25 -0500

    GPLIM-4396 move rowObjectToExpressionObject (cherry picked from commit 3273a11)

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/columns/DisplayExpression.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/columns/DisplayExpression.java
@@ -5,16 +5,21 @@ import org.broadinstitute.gpinformatics.infrastructure.SampleData;
 import org.broadinstitute.gpinformatics.infrastructure.search.SearchContext;
 import org.broadinstitute.gpinformatics.infrastructure.search.SearchTerm;
 import org.broadinstitute.gpinformatics.mercury.entity.Metadata;
+import org.broadinstitute.gpinformatics.mercury.entity.OrmUtil;
+import org.broadinstitute.gpinformatics.mercury.entity.labevent.LabEvent;
+import org.broadinstitute.gpinformatics.mercury.entity.labevent.LabEventType;
 import org.broadinstitute.gpinformatics.mercury.entity.reagent.MolecularIndexingScheme;
 import org.broadinstitute.gpinformatics.mercury.entity.sample.MercurySample;
 import org.broadinstitute.gpinformatics.mercury.entity.sample.SampleInstanceV2;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.MaterialType;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Expressions for configurable columns, grouped by ExpressionClass.
@@ -158,5 +163,82 @@ public enum DisplayExpression {
 
     public SearchTerm.Evaluator<?> getEvaluator() {
         return evaluator;
+    }
+
+    /**
+     * The list returned from this must be deterministic.
+     * @param rowObject object from result row
+     * @param expressionClass class against which expression will be evaluated
+     * @param context search parameters
+     * @param <T> expression class
+     * @return list of T classes, must be in same order for repeated calls
+     */
+    public static <T> List<T> rowObjectToExpressionObject(@Nonnull Object rowObject, Class<T> expressionClass,
+            SearchContext context) {
+        if (OrmUtil.proxySafeIsInstance(rowObject, LabVessel.class) && expressionClass.isAssignableFrom(SampleInstanceV2.class)) {
+            // LabVessel to SampleInstance
+            LabVessel labVessel = (LabVessel) rowObject;
+            return (List<T>) new ArrayList<>(labVessel.getSampleInstancesV2());
+
+        } else if (OrmUtil.proxySafeIsInstance(rowObject, LabVessel.class) && expressionClass.isAssignableFrom(SampleData.class)) {
+            // LabVessel to SampleData
+            LabVessel labVessel = (LabVessel) rowObject;
+            List<MercurySample> mercurySamples = new ArrayList<>();
+            for (SampleInstanceV2 sampleInstanceV2 : labVessel.getSampleInstancesV2()) {
+                MercurySample mercurySample = sampleInstanceV2.getRootOrEarliestMercurySample();
+                if (mercurySample != null) {
+                    mercurySamples.add(mercurySample);
+                }
+            }
+
+            List<SampleData> results = new ArrayList<>();
+            if (!mercurySamples.isEmpty()) {
+                BspSampleSearchAddRowsListener bspColumns = (BspSampleSearchAddRowsListener) context.getRowsListener(
+                        BspSampleSearchAddRowsListener.class.getSimpleName());
+                for( MercurySample mercurySample : mercurySamples) {
+                    results.add(bspColumns.getSampleData(mercurySample.getSampleKey()));
+                }
+            }
+            return (List<T>) results;
+
+        } else if (OrmUtil.proxySafeIsInstance(rowObject, LabEvent.class) && expressionClass.isAssignableFrom(SampleInstanceV2.class)) {
+            // LabEvent to SampleInstance
+            LabEvent labEvent = (LabEvent) rowObject;
+            LabVessel labVessel = labEvent.getInPlaceLabVessel();
+            if (labVessel == null) {
+                Set<LabVessel> labVessels;
+                LabEventType.PlasticToValidate plasticToValidate = labEvent.getLabEventType().getPlasticToValidate();
+                switch (plasticToValidate) {
+                    case BOTH:
+                    case SOURCE:
+                        labVessels = labEvent.getSourceLabVessels();
+                        break;
+                    case TARGET:
+                        labVessels = labEvent.getTargetLabVessels();
+                        break;
+                    default:
+                        throw new RuntimeException("Unexpected enum " + plasticToValidate);
+                }
+                Set<SampleInstanceV2> sampleInstances = new TreeSet<>();
+                for (LabVessel vessel : labVessels) {
+                    sampleInstances.addAll(vessel.getSampleInstancesV2());
+                }
+                return (List<T>) new ArrayList<>(sampleInstances);
+            } else {
+                return (List<T>) new ArrayList<>(labVessel.getSampleInstancesV2());
+            }
+
+        } else if (OrmUtil.proxySafeIsInstance(rowObject, MercurySample.class) && expressionClass.isAssignableFrom(SampleInstanceV2.class)) {
+            // MercurySample to SampleInstance
+            MercurySample mercurySample = (MercurySample) rowObject;
+            Set<SampleInstanceV2> sampleInstances = new TreeSet<>();
+            for (LabVessel labVessel : mercurySample.getLabVessel()) {
+                sampleInstances.addAll(labVessel.getSampleInstancesV2());
+            }
+            return (List<T>) new ArrayList<>(sampleInstances);
+
+        } else {
+            throw new RuntimeException("Unexpected combination " + rowObject.getClass() + " to " + expressionClass);
+        }
     }
 }
