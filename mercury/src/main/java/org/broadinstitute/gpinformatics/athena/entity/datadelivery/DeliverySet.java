e2e8a708a5fc145a7b016ae2c6c650c99334a382
Jon Thompson <thompson@broadinstitute.org>
2017-02-11 18:58:04 -0500

    GPLIM-4637 Early draft

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/datadelivery/DeliverySet.java
@@ -0,0 +1,11 @@
+package org.broadinstitute.gpinformatics.athena.entity.datadelivery;
+
+/**
+ * A set of samples for delivery, e.g. to Google Bucket.
+ */
+public class DeliverySet {
+    // Reference to LabVessel?
+    // Reference to metric (arrays or sequencing or both)
+    // Caching to improve performance?
+    // In transfer visualizer, visiting events and vessels is fast; why are sample instances slow?  Profile and diff
+}
---
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/athena/entity/datadelivery/DataDeliveryTest.java
@@ -0,0 +1,19 @@
+package org.broadinstitute.gpinformatics.athena.entity.datadelivery;
+
+import org.testng.annotations.Test;
+
+/**
+ * Prototype entities for Data Delivery
+ */
+public class DataDeliveryTest {
+
+    @Test
+    public void testX() {
+        // Create initial set
+        // Start with PDO
+        // Filter duplicates
+        // Fetch metrics
+        // Choose / exclude from delivery (some may not have metrics yet)
+        // Refresh to reflect new duplicates / metrics / versions?
+    }
+}
