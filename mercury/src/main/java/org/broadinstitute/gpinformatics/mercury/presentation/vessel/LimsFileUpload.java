b5f44cb9efba7fc84bfca73a0b0aa66e02f67219
jowalsh <jowalsh@broadinstitute.org>
2017-03-06 10:41:41 -0500

    GPLIM-4687 remove LimsFileUpload. Never added test xml to git.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/vessel/LimsFileUpload.java
+++
@@ -1,9 +0,0 @@
-package org.broadinstitute.gpinformatics.mercury.presentation.vessel;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.broadinstitute.gpinformatics.mercury.presentation.CoreActionBean;
-
-public class LimsFileUpload extends CoreActionBean {
-    private static final Log log = LogFactory.getLog(CoreActionBean.class);
-}
