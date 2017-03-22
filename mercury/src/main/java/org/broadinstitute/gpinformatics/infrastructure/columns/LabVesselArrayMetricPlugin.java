fd018740a270ebb38f1458247cb32c56bcbd605b
Jon Thompson <thompson@broadinstitute.org>
2017-03-22 16:58:53 -0400

    GPLIM-4733 return empty rows from plugin if metrics are missing; check that plugin returns correct number of rows

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/columns/LabVesselArrayMetricPlugin.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/columns/LabVesselArrayMetricPlugin.java
@@ -110,11 +110,11 @@ public class LabVesselArrayMetricPlugin implements ListPlugin {
         // Populate rows with any available metrics data.
         for( LabVessel labVessel : labVesselList ) {
             ArraysQc arraysQc = mapWellBarcodeToMetric.get(mapSourceToTargetBarcodes.get(labVessel.getLabel()));
+            ConfigurableList.Row row = new ConfigurableList.Row( labVessel.getLabel() );
+            metricRows.add(row);
             if (arraysQc == null) {
                 continue;
             }
-
-            ConfigurableList.Row row = new ConfigurableList.Row( labVessel.getLabel() );
 
             String value = ColumnValueType.THREE_PLACE_DECIMAL.format(
                     arraysQc.getCallRate().multiply(BigDecimal.valueOf(100)), "");
@@ -179,8 +179,6 @@ public class LabVesselArrayMetricPlugin implements ListPlugin {
             }
             row.addCell(new ConfigurableList.Cell(VALUE_COLUMN_TYPE.HAPMAP_CONCORDANCE.getResultHeader(),
                     value, value));
-
-            metricRows.add(row);
         }
 
         return metricRows;
