4cdfa179e85d8f32377b4f9e0ff7617ccea0a67b
Jon Thompson <thompson@broadinstitute.org>
2017-04-23 18:52:26 -0400

    GPLIM-4816 BSP call working with hard coded values

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/labevent/eventhandlers/SamplesDaughterPlateHandler.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/labevent/eventhandlers/SamplesDaughterPlateHandler.java
@@ -24,9 +24,12 @@ import org.broadinstitute.gpinformatics.mercury.bettalims.generated.BettaLIMSMes
 import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Handles bettalims messages that need to be passed to a BSP REST service for processing.
@@ -52,11 +55,63 @@ public class SamplesDaughterPlateHandler {
 
     }
 
+    @XmlRootElement
+    public static class KitSample {
+        private String bspSampleId;
+        private String collaboratorSampleId;
+
+        public KitSample(String bspSampleId, String collaboratorSampleId) {
+            this.bspSampleId = bspSampleId;
+            this.collaboratorSampleId = collaboratorSampleId;
+        }
+
+        /** Used by JAXB. */
+        @SuppressWarnings("unused")
+        public KitSample() {
+        }
+
+        public String getBspSampleId() {
+            return bspSampleId;
+        }
+
+        public void setBspSampleId(String bspSampleId) {
+            this.bspSampleId = bspSampleId;
+        }
+
+        public String getCollaboratorSampleId() {
+            return collaboratorSampleId;
+        }
+
+        public void setCollaboratorSampleId(String collaboratorSampleId) {
+            this.collaboratorSampleId = collaboratorSampleId;
+        }
+    }
+
+    @XmlRootElement
+    public static class CreateKitReturn {
+        private List<KitSample> samples = new ArrayList<>();
+
+        public CreateKitReturn() {
+        }
+
+        private void addSample(KitSample sample) {
+            samples.add(sample);
+        }
+
+        public List<KitSample> getSamples() {
+            return samples;
+        }
+
+        public void setSamples(List<KitSample> samples) {
+            this.samples = samples;
+        }
+    }
+
     public void x() {
         String sheetName = "Sample Submission Form";
         Object[][] rows = {
                 {"Collaborator Sample ID", "Collaborator Patient ID", "Submitted Material Type", "Original Material Type", "Sample Type", "Tumor Type", "Patient Gender", "Patient Diagnosis or Disease"},
-                {"JT1-BUFFY", "JT1-PT", "Whole Blood:Buffy Coat", "Whole Blood:Whole Blood", "Tumor", "Primary", "Male", "Test"}
+                {"JT3-BUFFY", "JT3-PT", "Whole Blood:Buffy Coat", "Whole Blood:Whole Blood", "Tumor", "Primary", "Male", "Test"}
         };
         Workbook workbook = SpreadsheetCreator.createSpreadsheet(sheetName, rows);
         String urlString = bspRestClient.getUrl(BSP_KIT_REST_URL);
@@ -69,10 +124,12 @@ public class SamplesDaughterPlateHandler {
             formDataMultiPart.field("domain", "VIRAL");
             ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
             workbook.write(byteArrayOutputStream);
-            MultiPart multiPart  = formDataMultiPart.bodyPart(
+            MultiPart multiPart = formDataMultiPart.bodyPart(
                      new FormDataBodyPart("spreadsheet", new ByteArrayInputStream(byteArrayOutputStream.toByteArray()),
                              MediaType.APPLICATION_OCTET_STREAM_TYPE));
-            webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).post(multiPart);
+            CreateKitReturn createKitReturn = webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).post(
+                    CreateKitReturn.class, multiPart);
+            createKitReturn.getSamples();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
