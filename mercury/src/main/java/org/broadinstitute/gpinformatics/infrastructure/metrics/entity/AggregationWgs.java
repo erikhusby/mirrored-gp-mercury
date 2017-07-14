a57c975a67d5a914a5162f35585d4a0acb5e5750
Dennis Ryan <dryan@broadinstitute.org>
2017-07-14 14:59:24 -0400

    GPLIM-4923 Mercury needs to support cloud submissions

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/metrics/entity/AggregationWgs.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/metrics/entity/AggregationWgs.java
@@ -11,6 +11,8 @@
 
 package org.broadinstitute.gpinformatics.infrastructure.metrics.entity;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -18,6 +20,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "AGGREGATION_WGS", schema = "METRICS")
+@BatchSize(size = 500)
 public class AggregationWgs {
     @Id
     @Column(name = "AGGREGATION_ID")
