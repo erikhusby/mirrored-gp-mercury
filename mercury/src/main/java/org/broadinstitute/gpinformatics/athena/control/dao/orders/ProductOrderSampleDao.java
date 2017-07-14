e3308f32ad53d4df303da8737511963a756c5384
Dennis Ryan <dryan@broadinstitute.org>
2017-07-14 14:59:24 -0400

    GPLIM-4923 Mercury needs to support cloud submissions

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/control/dao/orders/ProductOrderSampleDao.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/control/dao/orders/ProductOrderSampleDao.java
@@ -179,6 +179,8 @@ public class ProductOrderSampleDao extends GenericDao {
         CriteriaQuery<ProductOrderSample> criteriaQuery =
                 criteriaBuilder.createQuery(ProductOrderSample.class);
         Root<ProductOrderSample> productOrderSampleRoot = criteriaQuery.from(ProductOrderSample.class);
+        productOrderSampleRoot.alias("pdoSample");
+
         Join<ProductOrderSample, ProductOrder> productOrderJoin =
                 productOrderSampleRoot.join(ProductOrderSample_.productOrder);
         Join<ProductOrder, ResearchProject> researchProjectrJoin = productOrderJoin.join(ProductOrder_.researchProject);
