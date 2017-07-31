7213501a6e7e0bbc8c79068896f1666bb9d87c23
Dennis Ryan <dryan@broadinstitute.org>
2017-07-31 11:06:08 -0400

    GPLIM-4923: update dao to handle null processingLocations. Consolidate submission tuple constructors. remove some unused methods. Cache aggregation's submissionTuple variable. increase batch size for some aggregation tables to reduce n+1 fetches.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/metrics/entity/AggregationContam.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/metrics/entity/AggregationContam.java
@@ -21,6 +21,7 @@ import java.io.Serializable;
 
 @Entity
 @Table(name = "AGGREGATION_CONTAM", schema = "METRICS")
+@BatchSize(size = 500)
 public class AggregationContam implements Serializable {
     private static final long serialVersionUID = -4761300827083196585L;
     @Id
