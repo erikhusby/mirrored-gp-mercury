5060fa1dbb678a595f58a5445b3067755fe14953
Dennis Ryan <dryan@broadinstitute.org>
2017-07-31 11:06:08 -0400

    GPLIM-4923: update dao to handle null processingLocations. Consolidate submission tuple constructors. remove some unused methods. Cache aggregation's submissionTuple variable. increase batch size for some aggregation tables to reduce n+1 fetches.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/projects/ResearchProjectEjb.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/boundary/projects/ResearchProjectEjb.java
@@ -315,7 +315,9 @@ public class ResearchProjectEjb {
                     }
                 });
         List<String> errorMessages = new ArrayList<>();
-
+        if (submissionResults.isEmpty()) {
+            errorMessages.add("There was an error.");
+        }
         for (SubmissionStatusDetailBean status : submissionResults) {
             SubmissionTracker submissionTracker = submissionIdentifierToTracker.get(status.getUuid());
             if (CollectionUtils.isNotEmpty(status.getErrors())) {
