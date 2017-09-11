ba57b5ff27a33c8dc30580ca613aae7a573b7e8d
Erik Husby <mhusby@broadinstitute.org>
2017-09-11 16:35:46 -0400

    GPLIM-5067 Adding new build support.

--- mercury/mvnAllTests.sh
+++ mercury/mvnAllTests.sh
@@ -2,24 +2,65 @@
 #
 # Run all the unit tests using each of the several profiles.
 #
+usage() {
+    cat <<EOF
+Usage: $0 [-t <test> ] [-b <build>] [-j <jboss> ]
+
+Where:
+	-h		Show this message
+	-t <test>	Specifies a particular test profile to be run. Defaults to the standard set.
+	-b <build>	Specifies a particular build profile to be used. Defaults to BUILD,Arquillian-JBossAS7-Remote.
+	-j <jboss>	Specifies a particular JBoss or Wildfly installation.
+	-m <maven>	Specifies additional Maven options. Can be mentioned more than once, they accumulate
+	-c 		Runs tests with Clover.
+	-u <java>   Specifes the Java version to use. Default is Java-1.7
+
+The standard set of test profiles includes:
+    Tests.ArqSuite.Standard Tests.ArqSuite.Stubby Tests.Multithreaded Tests.DatabaseFree Tests.ExternalIntegration Tests.Alternatives
+    The Tests.LongRunning is excluded by default.
+
+Each test profile will be executed separately. The results of each test execution wil be stored in ./surefire-reports-profile where "profile"
+is the test profile name. The exit status will be the failure if any test profile fails, otherwise will be success.
+
+The complete build logs will be in tests-$TEST.log
+
+EOF
+}
+TESTS="Tests.ArqSuite.Standard Tests.ArqSuite.Stubby Tests.Multithreaded Tests.DatabaseFree Tests.ExternalIntegration Tests.Alternatives"
+BUILD_PROFILE="BUILD,Arquillian-JBossAS7-Remote"
+CLOVER=0
+ADDITIONAL_OPTIONS=
+JAVA_USE="Java-1.7"
+
+while getopts "hct:b:j:m:u:" OPTION; do
+    case $OPTION in
+	h) usage; exit 1;;
+	t) TESTS=$OPTARG;;
+	b) BUILD_PROFILE=$OPTARG;;
+	j) JBOSS_HOME=$OPTARG;;
+	c) CLOVER=1;;
+	m) ADDITIONAL_OPTIONS="$ADDITIONAL_OPTIONS $OPTARG";;
+	u) JAVA_USE=$OPTARG;;
+	[?]) usage; exit 1;;
+    esac
+done
+
 if [ -e "/broad/tools/scripts/useuse" ]
 then
     source /broad/tools/scripts/useuse
     use Maven-3.1
-    #    use Java-1.8
-    echo "Setting Java-1.8"
-    use -v .java-jdk-1.8.0_112-x86-64
+    use $JAVA_USE
 else
-    echo "Unable to set Java-1.8"
+    echo "Unable to set $JAVA_USE"
     ls -l /broad/tools/scripts/useuse
     exit 1
 fi
 
-if [ "x$JBOSS_HOME" == "x" ]
+if [ ! -e "$JBOSS_HOME" ]
 then
     cat <<EOF
 
-    You must define JBOSS_HOME to point to the Wildfly installation.
+    You must define JBOSS_HOME to point to the JBoss or Wildfly installation.
 
 EOF
     exit 1
@@ -31,28 +72,25 @@ then
     KEYSTORE_PASSWORD="changeit"
     SSL_OPTS="-DkeystoreFile=$KEYSTORE_FILE -DkeystorePassword=$KEYSTORE_PASSWORD"
 fi
+export MAVEN_OPTS="-Xms4g -XX:MaxPermSize=1g $SSL_OPTS"
 
-if [ "x$BUILD_PROFILE" == "x" ]
-then
-    BUILD_PROFILE=BUILD
-fi
-
-MAVEN_OPTS="-Xms4g -XX:MaxMetaspaceSize=1g $SSL_OPTS"
-OPTIONS="-PArquillian-WildFly10-Remote,$BUILD_PROFILE -Djava.awt.headless=true --batch-mode -Dmaven.download.meter=silent "
-PROFILES="Tests.ArqSuite.Standard Tests.ArqSuite.Stubby Tests.Multithreaded Tests.DatabaseFree Tests.ExternalIntegration Tests.Alternatives"
-#PROFILES="Tests.ExternalIntegration"
-#PROFILES="Tests.DatabaseFree"
-#PROFILES="Tests.Multithreaded"
-#PROFILES="Tests.Alternatives"
 
 EXIT_STATUS=0
 
-if [ -f "tests.log" ]
+# Setup the build options
+if [[ $CLOVER -eq 0 ]]
 then
-    rm tests.log
+    GOALS="clean test"
+else
+    GOALS="clean clover:setup verify"
+    rm -rf clover/
+    mkdir clover
+    BUILD_PROFILE="$BUILD_PROFILE,Clover.All -Dmaven.clover.licenseLocation=/prodinfolocal/BambooHome/clover.license -DmercuryCloverDatabase=`pwd`clover/clover.db"
 fi
 
-for PROFILE in $PROFILES
+OPTIONS="-P$BUILD_PROFILE -Djava.awt.headless=true --batch-mode  -Dannotation.outputDiagnostics=false -Dmaven.download.meter=silent $ADDITIONAL_OPTIONS"
+
+for TEST in $TESTS
 do
     cat <<EOF
 >>>>>
@@ -61,28 +99,36 @@ Properties
 JBOSS_HOME=$JBOSS_HOME
 MAVEN_OPTS=$MAVEN_OPTS
 OPTIONS=$OPTIONS
-PROFILES=$PROFILES
-JAVA_HOME=$JAVA_HOME
 
->>>> Executing profile $PROFILE
+>>>> Executing test profile $TEST
 
 EOF
-# Adding the clean to deal with a known Java 1.8 bug when compling from Maven -- see JDK-8067747
-    mvn $OPTIONS -P$PROFILE clean test | tee -a tests.log
+
+# Run the current test set
+    mvn $OPTIONS -P$TEST $GOALS | tee -a tests-$TEST.log
     if [ ${PIPESTATUS[0]} -ne 0 ]
     then
         EXIT_STATUS=${PIPESTATUS[0]}
     fi
 
-#    echo -n 1>&2 "Press return to continue."; read CONTINUE
+# Save the test results outside of the target directory
     if [ -e "target/surefire-reports" ]
     then
-        if [ -e "surefire-reports-$PROFILE" ]
+        if [ -e "surefire-reports-$TEST" ]
         then
-            rm -rf surefire-reports-$PROFILE
+            rm -rf surefire-reports-$TEST
         fi
-        mv target/surefire-reports surefire-reports-$PROFILE
+        mv target/surefire-reports surefire-reports-$TEST
     fi
+    if [ -e "target/clover/surefire-reports" ]
+    then
+        if [ -e "clover/surefire-reports-$TEST" ]
+        then
+            rm -rf clover/surefire-reports-$TEST
+        fi
+        mv target/clover/surefire-reports clover/surefire-reports-$TEST
+    fi
+
 done
 
 exit $EXIT_STATUS
