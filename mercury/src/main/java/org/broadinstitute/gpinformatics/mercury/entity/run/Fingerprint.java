da92695c88da93489a6519b62911ff58a278e875
Jon Thompson <thompson@broadinstitute.org>
2017-10-10 10:13:35 -0400

    GPLIM-5122 sketch entity
    (cherry picked from commit a303fd9)

---
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/run/Fingerprint.java
@@ -0,0 +1,29 @@
+package org.broadinstitute.gpinformatics.mercury.entity.run;
+
+/**
+ * Created by thompson on 10/9/2017.
+ */
+public class Fingerprint {
+    /*
+    reference to sample
+    disposition
+    platform
+    genomeBuild?
+    dateGenerated
+    reference to list of RSIDs
+    genotypes
+    callConfidence
+    fatPandaGq
+    fatPandaPl
+    gender
+     */
+
+    /*
+    GET
+    list of LSIDs
+     */
+
+    /*
+    POST
+     */
+}
