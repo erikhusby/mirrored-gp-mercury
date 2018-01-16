873a643cef2cf359f4911359348dda89481000cb
Jon Thompson <thompson@broadinstitute.org>
2018-01-15 22:05:13 -0500

    GPLIM-5122 more work on FingerprintChecker

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/run/FingerprintResource.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/run/FingerprintResource.java
@@ -2,6 +2,10 @@ package org.broadinstitute.gpinformatics.mercury.boundary.run;
 
 import clover.org.apache.commons.lang3.tuple.ImmutablePair;
 import clover.org.apache.commons.lang3.tuple.Pair;
+import htsjdk.samtools.util.StringUtil;
+import htsjdk.variant.variantcontext.Genotype;
+import htsjdk.variant.variantcontext.GenotypeLikelihoods;
+import htsjdk.variant.variantcontext.VariantContext;
 import org.broadinstitute.gpinformatics.infrastructure.jpa.DaoFree;
 import org.broadinstitute.gpinformatics.mercury.boundary.ResourceException;
 import org.broadinstitute.gpinformatics.mercury.control.dao.run.SnpListDao;
@@ -15,8 +19,12 @@ import org.broadinstitute.gpinformatics.mercury.entity.vessel.LabVessel;
 import org.broadinstitute.gpinformatics.mercury.entity.vessel.TransferTraverserCriteria;
 import org.jetbrains.annotations.NotNull;
 import picard.fingerprint.FingerprintChecker;
+import picard.fingerprint.HaplotypeBlock;
 import picard.fingerprint.HaplotypeMap;
+import picard.fingerprint.HaplotypeProbabilitiesFromGenotype;
+import picard.fingerprint.HaplotypeProbabilitiesFromGenotypeLikelihoods;
 import picard.fingerprint.MatchResults;
+import picard.util.AlleleSubsettingUtils;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
@@ -35,6 +43,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.regex.Pattern;
 
 /**
@@ -178,8 +188,9 @@ public class FingerprintResource {
         }
 
         // todo jmt check concordance
-        FingerprintChecker fingerprintChecker = new FingerprintChecker(new HaplotypeMap(
-                new File("/notes/Homo_sapiens_assembly19.haplotype_database.txt")));
+        HaplotypeMap haplotypes = new HaplotypeMap(
+                new File("/notes/Homo_sapiens_assembly19.haplotype_database.txt"));
+        FingerprintChecker fingerprintChecker = new FingerprintChecker(haplotypes);
         picard.fingerprint.Fingerprint observedFp = new picard.fingerprint.Fingerprint("", null, "");
         observedFp.add();
         picard.fingerprint.Fingerprint expectedFp = new picard.fingerprint.Fingerprint("", null, "");
@@ -187,5 +198,61 @@ public class FingerprintResource {
         matchResults.getLOD();
         mercurySampleDao.flush();
         return "Stored fingerprint";
+    }
+
+    private void x(String sample, HaplotypeMap haplotypes) {
+        final SortedSet<picard.fingerprint.Snp> snps = new TreeSet<>(haplotypes.getAllSnps());
+        for (final picard.fingerprint.Snp snp : snps) {
+
+            final HaplotypeBlock h = haplotypes.getHaplotype(ctx.getContig(), ctx.getStart());
+            final picard.fingerprint.Snp snp = haplotypes.getSnp(ctx.getContig(), ctx.getStart());
+            if (h == null) return;
+
+            final VariantContext usableSnp = AlleleSubsettingUtils.subsetVCToMatchSnp(ctx, snp);
+            if (usableSnp == null) {
+                return;
+            }
+            //PLs are preferred over GTs
+            //TODO: this code is replicated in various places (ReconstructTriosFromVCF for example). Needs refactoring.
+            //TODO: add a way to force using GTs when both are available (why?)
+
+            // Get the genotype for the sample and check that it is useful
+            final Genotype genotype = usableSnp.getGenotype(sample);
+            if (genotype == null) {
+                throw new IllegalArgumentException("Cannot find sample " + sample + " in provided file. ");
+            }
+            if (genotype.hasPL()) {
+
+                final HaplotypeProbabilitiesFromGenotypeLikelihoods hFp = new HaplotypeProbabilitiesFromGenotypeLikelihoods(h);
+                //do not modify the PL array directly fragile!!!!!
+                final int[] pls = genotype.getPL();
+                final int[] newPLs = new int[pls.length];
+                for (int i = 0; i < pls.length; i++) {
+                    newPLs[i] = Math.min(maximalPLDifference, pls[i]);
+                }
+                hFp.addToLogLikelihoods(snp, usableSnp.getAlleles(), GenotypeLikelihoods.fromPLs(newPLs).getAsVector());
+                fp.add(hFp);
+            } else {
+
+                if (genotype.isNoCall()) continue;
+
+                // TODO: when multiple genotypes are available for a Haplotype check that they
+                // TODO: agree. Not urgent since DownloadGenotypes already does this.
+                if (fp.containsKey(h)) continue;
+
+                final boolean hom = genotype.isHom();
+                final byte allele = StringUtil.toUpperCase(genotype.getAllele(0).getBases()[0]);
+
+                final double halfError = this.genotypingErrorRate / 2;
+                final double accuracy = 1 - this.genotypingErrorRate;
+                final double[] probs = new double[]{
+                        (hom && allele == snp.getAllele1()) ? accuracy : halfError,
+                        (!hom) ? accuracy : halfError,
+                        (hom && allele == snp.getAllele2()) ? accuracy : halfError
+                };
+
+                fp.add(new HaplotypeProbabilitiesFromGenotype(snp, h, probs[0], probs[1], probs[2]));
+            }
+        }
     }
 }
