2c6fed993eaa06e5e79d1bdad8d48f5bce7193c4
Erik Husby <mhusby@broadinstitute.org>
2018-01-29 14:43:17 -0500

    GPLIM-3937 Enhanced to allow an individual test profile to be specified.

--- mercury/bambooAllTests.sh
+++ mercury/bambooAllTests.sh
@@ -14,6 +14,7 @@ usage() {
        	-m <maven>	Specifies additional Maven options. Can be mentioned more than once, they accumulate.
        	-b  Specifies a particular build profile to be used. Defaults to BUILD,Arquillian-JBossAS7-Remote.
        	-u <java>  Specifies version of Java to use, defaults to Java-1.7
+	-t <test> Specifies a particular test profile to be used, defaults to all but Tests.LongRunning
 
 Either the JBoss or Wildfly server must be specified.
 
@@ -23,6 +24,7 @@ EOF
 
 TESTS_ARQUILLIAN="Tests.ArqSuite.Standard Tests.ArqSuite.Stubby Tests.ExternalIntegration Tests.Alternatives"
 TESTS_NONARQUILLIAN="Tests.Multithreaded Tests.DatabaseFree "
+TESTS_PROFILE=
 BUILD="BUILD"
 CLOVER=""
 SERVER_HOME=
@@ -46,7 +48,9 @@ while getopts "hcwb:s:w:m:u:" OPTION; do
 	m) ADDITIONAL_OPTIONS="$ADDITIONAL_OPTIONS -m $OPTARG "
 	    ;;
 	u) JAVA_USE=$OPTARG
-	    ;;
+	   ;;
+	t) TEST_PROFILE=$OPTARG
+	   ;;
 	[?]) usage
 	    exit 1
 	    ;;
@@ -60,6 +64,7 @@ USE_WILDFLY=$USE_WILDFLY
 CLOVER=$CLOVER
 ADDITIONAL_OPTIONS=$ADDITIONAL_OPTIONS
 JAVA_USE=$JAVA_USE
+TEST_PROFILE=$TEST_PROFILE
 EOF
 
 if [ -e "/broad/tools/scripts/useuse" ]
@@ -77,12 +82,21 @@ rm -rf clover
 mkdir clover
 rm -rf cloverdb
 mkdir cloverdb
-# Run the NonArquillian tests first
-for TEST in $TESTS_NONARQUILLIAN
-do
-    ./mvnAllTests.sh -t $TEST -b $BUILD $USE_WILDFLY -u $JAVA_USE $CLOVER $ADDITIONAL_OPTIONS
-done
 
+# Define several useful functions.
+# Execute an NonArqillian Test
+nonArquillianTest() {
+    ./mvnAllTests.sh -t $1 -b $BUILD $USE_WILDFLY -u $JAVA_USE $CLOVER $ADDITIONAL_OPTIONS
+}
+
+# Execute an Arquillian Test
+arquillianTest() {
+    server start
+    ./mvnAllTests.sh -t $1 -b $BUILD $USE_WILDFLY -u $JAVA_USE $CLOVER $ADDITIONAL_OPTIONS
+    server stop
+}
+
+# Execute a server start or stop operation
 server() {
     if [ "$USE_WILDFLY" == "-w" ]
     then
@@ -92,13 +106,30 @@ server() {
     fi
 }
 
-# Run the Arquillian tests
-for TEST in $TESTS_ARQUILLIAN
-do
-    server start
-    ./mvnAllTests.sh -t $TEST -b $BUILD $USE_WILDFLY -u $JAVA_USE $CLOVER $ADDITIONAL_OPTIONS
-    server stop
-done
+# if a test profile was not specified then  execute the Non-Arquillian tests first, then the Arquillian tests.
+if [ "x$TEST_PROFILE" == "x" ]
+then
+    # Run the NonArquillian tests first
+    for TEST in $TESTS_NONARQUILLIAN
+    do
+        nonArquillianTest $TEST
+    done
+
+    # Run the Arquillian tests
+    for TEST in $TESTS_ARQUILLIAN
+    do
+        arquillianTest $TEST
+    done
+else
+    # Otherwise, determine if the test relies on Arquillian support or not. Default to an Arquillian test if not
+    # in the list of NonArquillian
+    if [[ $TESTS_NONARQUILLIAN ~= $TEST_PROFILE ]]
+    then
+        nonArquillianTest $TEST_PROFILE
+    else
+        arquillianTest $TEST_PROFILE
+    fi
+fi
 
 # Generate the clover report if necessary
 if [ "$CLOVER" == "-c" ]
