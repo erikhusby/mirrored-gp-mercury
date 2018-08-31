82d74079458de029ad0062203d4c3847ab2b1856
Dennis Ryan <dryan@broadinstitute.org>
2018-08-31 10:56:13 -0400

    GPLIM-5701: add configuration to support billing reversals

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/sap/SapConfig.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/sap/SapConfig.java
@@ -14,13 +14,15 @@ import java.io.Serializable;
 @ConfigKey("sap")
 @ApplicationScoped
 public class SapConfig extends AbstractConfig implements LoginAndPassword, Serializable {
+    private static final String SHORT_CLOSE_REQUEST = "Short Close Request";
+    private static final String REVERSE_BILLING_REQUEST = "Reverse Billing Request";
 
     private String login;
     private String password;
 
-    private String sapShortCloseRecipientEmail;
+    private String sapSupportEmail;
 
-    private String sapShortCloseEmailSubject;
+    private String sapSupportEmailSubjectPrefix;
 
     public SapConfig(){}
 
@@ -50,19 +52,27 @@ public class SapConfig extends AbstractConfig implements LoginAndPassword, Seria
     public static SapConfig produce(Deployment deployment) {
         return produce(SapConfig.class, deployment);
     }
-    public String getSapShortCloseRecipientEmail() {
-        return sapShortCloseRecipientEmail;
+    public String getSapSupportEmail() {
+        return sapSupportEmail;
     }
 
-    public void setSapShortCloseRecipientEmail(String sapShortCloseRecipientEmail) {
-        this.sapShortCloseRecipientEmail = sapShortCloseRecipientEmail;
+    public void setSapSupportEmail(String sapSupportEmail) {
+        this.sapSupportEmail = sapSupportEmail;
     }
 
     public String getSapShortCloseEmailSubject() {
-        return sapShortCloseEmailSubject;
+        return getSapSupportEmailSubjectPrefix() + ": " + SHORT_CLOSE_REQUEST;
     }
 
-    public void setSapShortCloseEmailSubject(String sapShortCloseEmailSubject) {
-        this.sapShortCloseEmailSubject = sapShortCloseEmailSubject;
+    public String getSapReverseBillingSubject() {
+        return getSapSupportEmailSubjectPrefix() + ": " + REVERSE_BILLING_REQUEST;
+    }
+
+    public String getSapSupportEmailSubjectPrefix() {
+        return sapSupportEmailSubjectPrefix;
+    }
+
+    public void setSapSupportEmailSubjectPrefix(String sapSupportEmailSubjectPrefix) {
+        this.sapSupportEmailSubjectPrefix = sapSupportEmailSubjectPrefix;
     }
 }
