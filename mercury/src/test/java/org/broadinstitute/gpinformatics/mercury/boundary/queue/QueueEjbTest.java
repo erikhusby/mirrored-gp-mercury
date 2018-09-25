2059e3044ca120ac81b84328c1cc11abefd564a0
pdunlea <pdunlea@broadinstitute.org>
2018-09-25 16:17:20 -0400

    GPLIM-5743 - Added pico queue to the DB and created a test

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/queue/QueueEjbTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/boundary/queue/QueueEjbTest.java
@@ -1,22 +1,34 @@
 package org.broadinstitute.gpinformatics.mercury.boundary.queue;
 
 
+import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
 import org.broadinstitute.gpinformatics.infrastructure.test.TestGroups;
+import org.broadinstitute.gpinformatics.mercury.entity.queue.GenericQueue;
+import org.broadinstitute.gpinformatics.mercury.entity.queue.QueueType;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+
+import static org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV;
 
 @Test(groups = TestGroups.STANDARD)
 public class QueueEjbTest extends Arquillian {
 
+    @Inject
+    private QueueEjb queueEjb;
+
     @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    public static WebArchive buildMercuryWar() {
+        return DeploymentBuilder.buildMercuryWar(DEV);
     }
 
-
+    @Test(groups = TestGroups.STANDARD)
+    public void loadPicoQueueTest() {
+        GenericQueue picoQueue = queueEjb.findQueueByType(QueueType.PICO);
+        Assert.assertNotNull(picoQueue);
+    }
 }
