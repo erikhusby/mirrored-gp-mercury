b6a52f9f7a55398807b2eb268213948063afce72
jowalsh <jowalsh@broadinstitute.org>
2018-10-19 10:57:56 -0400

    GPLIM-5831 javadoc

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/labevent/eventhandlers/DenatureToDilutionTubeHandler.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/control/labevent/eventhandlers/DenatureToDilutionTubeHandler.java
@@ -114,9 +114,9 @@ public class DenatureToDilutionTubeHandler extends AbstractEventHandler {
              * Check the FCT Tickets of the denature:
              * -- If we find the Batch corresponding to the fct ticket and there is no dilution tube currently
              *      assigned, assign the cherry pick dilution tube to the batch association
-             * -- If we find the batch corresponding to the fct ticket but we don't find the starting vessel
+             * -- If we find the Batch corresponding to the fct ticket and there is an existing dilution tube currently
              *     assigned, throw an exception
-             * -- If we find the batch corresponding to the fct ticket but we don't find the starting  vessel
+             * -- If we find the batch corresponding to the fct ticket but we don't find the starting vessel
              *     in the lab batch then check against the ancestor normalization tube
              * -- If we do not find any fct tickets associated with the denature, throw an exception that the
              *     association was not previously done
