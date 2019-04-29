fc6055dfe636a7b3972345cb9587847e32b0c47a
Jon Thompson <thompson@broadinstitute.org>
2019-04-29 11:53:22 -0400

    GPLIM-6246 handle clinical fingerprints

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/columns/PassingFingerprintPlugin.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/columns/PassingFingerprintPlugin.java
@@ -3,6 +3,9 @@ package org.broadinstitute.gpinformatics.infrastructure.columns;
 import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.broadinstitute.gpinformatics.athena.presentation.Displayable;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPGetExportedSamplesFromAliquots;
+import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPSampleDataFetcher;
+import org.broadinstitute.gpinformatics.infrastructure.bsp.BSPSampleSearchColumn;
+import org.broadinstitute.gpinformatics.infrastructure.bsp.BspSampleData;
 import org.broadinstitute.gpinformatics.infrastructure.bsp.exports.IsExported;
 import org.broadinstitute.gpinformatics.infrastructure.common.ServiceAccessUtility;
 import org.broadinstitute.gpinformatics.infrastructure.search.SearchContext;
@@ -19,6 +22,8 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Configurable List plugin that adds columns for passing fingerprints.
@@ -67,35 +72,52 @@ public class PassingFingerprintPlugin implements ListPlugin {
                 BSPGetExportedSamplesFromAliquots.class);
         MercurySampleDao mercurySampleDao = ServiceAccessUtility.getBean(
                 MercurySampleDao.class);
+        BSPSampleDataFetcher bspSampleDataFetcher = ServiceAccessUtility.getBean(BSPSampleDataFetcher.class);
+        Map<String, BspSampleData> mapIdToSampleData = bspSampleDataFetcher.fetchSampleData(
+                sampleList.stream().map(MercurySample::getSampleKey).collect(Collectors.toList()),
+                BSPSampleSearchColumn.LSID);
 
+        Comparator<Fingerprint> comparator = (o1, o2) -> new CompareToBuilder().
+                append(o1.getPlatform().getPrecedenceForInitial(), o2.getPlatform().getPrecedenceForInitial()).
+                append(o2.getDateGenerated(), o1.getDateGenerated()).
+                build();
         for( MercurySample sample : sampleList ) {
-            // Get exports from BSP to GAP
-            // todo jmt need to search to get lsid
-            List<BSPGetExportedSamplesFromAliquots.ExportedSample> exportedSamples =
-                    samplesFromAliquots.getExportedSamplesFromAliquots(Collections.singleton("broadinstitute.org:bsp.prod.sample:" + sample.getSampleKey().substring(3)),
-                            IsExported.ExternalSystem.GAP);
-            List<String> sampleKeys = new ArrayList<>();
-            for (BSPGetExportedSamplesFromAliquots.ExportedSample exportedSample : exportedSamples) {
-                sampleKeys.add(FingerprintResource.getSmIdFromLsid(exportedSample.getExportedLsid()));
-            }
-
-            // Get most recent passing fingerprint from each export
-            Comparator<Fingerprint> comparator = (o1, o2) -> new CompareToBuilder().
-                    append(o1.getPlatform().getPrecedenceForInitial(), o2.getPlatform().getPrecedenceForInitial()).
-                    append(o2.getDateGenerated(), o1.getDateGenerated()).
-                    build();
-            Map<String, MercurySample> mapIdToMercurySample = mercurySampleDao.findMapIdToMercurySample(sampleKeys);
-            List<Fingerprint> fingerprints = new ArrayList<>();
-            for (Map.Entry<String, MercurySample> idMercurySampleEntry : mapIdToMercurySample.entrySet()) {
-                MercurySample mercurySample = idMercurySampleEntry.getValue();
-                if (mercurySample != null) {
-                    Optional<Fingerprint> optionalFingerprint = mercurySample.getFingerprints().stream().
-                            filter(fingerprint -> fingerprint.getDisposition() == Fingerprint.Disposition.PASS &&
-                                    initialPlatforms.contains(fingerprint.getPlatform())).max(comparator);
-                    optionalFingerprint.ifPresent(fingerprints::add);
+            Set<Fingerprint> rowFingerprints = sample.getFingerprints();
+            Optional<Fingerprint> optionalFingerprint;
+            if (rowFingerprints.isEmpty()) {
+                // Get exports from BSP to GAP
+                BspSampleData bspSampleData = mapIdToSampleData.get(sample.getSampleKey());
+                if (bspSampleData == null) {
+                    continue;
                 }
+                List<BSPGetExportedSamplesFromAliquots.ExportedSample> exportedSamples =
+                        samplesFromAliquots.getExportedSamplesFromAliquots(Collections.singleton(bspSampleData.getSampleLsid()),
+                                IsExported.ExternalSystem.GAP);
+                List<String> sampleKeys = new ArrayList<>();
+                for (BSPGetExportedSamplesFromAliquots.ExportedSample exportedSample : exportedSamples) {
+                    sampleKeys.add(FingerprintResource.getSmIdFromLsid(exportedSample.getExportedLsid()));
+                }
+
+                // Get most recent passing fingerprint from each export
+                Map<String, MercurySample> mapIdToMercurySample = mercurySampleDao.findMapIdToMercurySample(sampleKeys);
+                List<Fingerprint> fingerprints = new ArrayList<>();
+                for (Map.Entry<String, MercurySample> idMercurySampleEntry : mapIdToMercurySample.entrySet()) {
+                    MercurySample mercurySample = idMercurySampleEntry.getValue();
+                    if (mercurySample != null) {
+                        mercurySample.getFingerprints().stream().
+                                filter(fingerprint -> fingerprint.getDisposition() == Fingerprint.Disposition.PASS &&
+                                        initialPlatforms.contains(fingerprint.getPlatform())).
+                                max(comparator).
+                                ifPresent(fingerprints::add);
+                    }
+                }
+                optionalFingerprint = fingerprints.stream().max(comparator);
+            } else {
+                optionalFingerprint = rowFingerprints.stream().
+                        filter(fingerprint -> fingerprint.getDisposition() == Fingerprint.Disposition.PASS &&
+                                initialPlatforms.contains(fingerprint.getPlatform())).
+                        max(comparator);
             }
-            Optional<Fingerprint> optionalFingerprint = fingerprints.stream().max(comparator);
 
             // Add row to table
             ConfigurableList.Row row = new ConfigurableList.Row(sample.getSampleKey());
@@ -103,7 +125,7 @@ public class PassingFingerprintPlugin implements ListPlugin {
                 Fingerprint fingerprint = optionalFingerprint.get();
                 row.addCell(new ConfigurableList.Cell(PluginColumn.PASSING.getHeader(), "Y", "Y"));
                 row.addCell(new ConfigurableList.Cell(PluginColumn.DATE.getHeader(),
-                        fingerprint.getDateGenerated(), fingerprint.getDateGenerated().toString()));
+                        fingerprint.getDateGenerated(), ColumnValueType.DATE.format(fingerprint.getDateGenerated(), "")));
                 row.addCell(new ConfigurableList.Cell(PluginColumn.PLATFORM.getHeader(),
                         fingerprint.getPlatform(), fingerprint.getPlatform().name()));
                 row.addCell(new ConfigurableList.Cell(PluginColumn.ALIQUOT_ID.getHeader(),
