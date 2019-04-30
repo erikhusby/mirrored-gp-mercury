fd4c4a25ad5b2954959047e13a65661fb4ad62bf
jowalsh <jowalsh@broadinstitute.org>
2019-04-30 14:23:54 -0400

    GPLIM-5831 handle merge issues

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/products/Product.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/entity/products/Product.java
@@ -25,6 +25,8 @@ import javax.annotation.Nullable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
