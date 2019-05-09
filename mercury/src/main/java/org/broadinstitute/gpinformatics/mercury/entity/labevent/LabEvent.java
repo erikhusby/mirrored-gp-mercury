047ad4dc19743a2623748f9888c9d53b83c23abb
jowalsh <jowalsh@broadinstitute.org>
2019-05-09 13:35:49 -0400

    GPLIM-6295 delete unused method

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/labevent/LabEvent.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/labevent/LabEvent.java
@@ -378,13 +378,6 @@ public class LabEvent {
         return found;
     }
 
-    public boolean removeLabBatch(LabBatch labBatch) {
-        if (computedLcSets.contains(labBatch)) {
-            return computedLcSets.remove(labBatch);
-        }
-        return false;
-    }
-
     public void addReagentVolume(Reagent reagent, BigDecimal volume) {
         labEventReagents.add(new LabEventReagent(this, reagent, volume));
     }
