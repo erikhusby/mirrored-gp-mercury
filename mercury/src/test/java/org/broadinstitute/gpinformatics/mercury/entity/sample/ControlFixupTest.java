c44a894a092421ef58cb67b454d5a4f27f2dc6e6
Jon Thompson <thompson@broadinstitute.org>
2019-05-28 17:08:03 -0400

    GPLIM-6080 trying to find control SM-IDs

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/sample/ControlFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/sample/ControlFixupTest.java
@@ -19,21 +19,43 @@ public class ControlFixupTest extends Arquillian {
 
     @Test
     public void gplim6080ControlFingerprintFixup() {
+        /*
+        SELECT
+            ss.lsid
+        FROM
+            geno.genotyping_run gr
+            INNER JOIN geno.metric_value mv
+                ON   mv.genotyping_run_id = gr.genotyping_run_id
+            INNER JOIN ppm.sts_sample ss
+                ON   ss.sample_id = mv.sample_id
+            INNER JOIN ppm.sts_clinical_sample scs
+                ON   scs.clinical_sample_id = ss.clinical_sample_id
+            INNER JOIN gap.object_alias oa
+                ON   oa.lsid = scs.lsid
+            INNER JOIN gap.object_alias oa2
+                ON   oa2.lsid = ss.lsid
+            INNER JOIN geno.metric_type mt
+                ON   mt.metric_type_id = mv.metric_type_id
+        WHERE
+            mt.metric_label = 'FLDM.SAMPLE_HAPMAP_CONCORDANCE'
+            AND oa.alias = 'NA12878'
+            AND mv.METRIC_VALUE = 100.0;
+         */
         Map<String, String> mapCollabIdToFpSample = new HashMap<String, String>() {{
-            put("NA12878", "");
-            put("NA12891", "");
-            put("NA12892", "");
-            put("NA18506", "");
-            put("NA18563", "");
-            put("NA18603", "");
-            put("NA18609", "");
-            put("NA18612", "");
-            put("NA18942", "");
-            put("NA18947", "");
-            put("NA18951", "");
-            put("NA18956", "");
-            put("NA18967", "");
-            put("NA18969", "");
+            put("NA12878", "broadinstitute.org:bsp.prod.sample:2522V");
+            put("NA12891", "SM-4D7US");
+            put("NA12892", "SM-41MLW");
+//            put("NA18506", "");
+//            put("NA18563", "");
+//            put("NA18603", "");
+            put("NA18609", "broadinstitute.org:bsp.prod.sample:73DPY");
+            put("NA18612", "broadinstitute.org:gap.prod.sample:4745691");
+            put("NA18942", "broadinstitute.org:gap.prod.sample:4745639");
+            put("NA18947", "broadinstitute.org:gap.prod.sample:4745704");
+            put("NA18951", "broadinstitute.org:bsp.prod.sample:73DOK");
+            put("NA18956", "broadinstitute.org:gap.prod.sample:4745675");
+            put("NA18967", "broadinstitute.org:bsp.prod.sample:73DOM");
+            put("NA18969", "broadinstitute.org:gap.prod.sample:4745642");
         }};
     }
 }
