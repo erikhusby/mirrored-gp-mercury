229bf2d0bbd31f35cda9380b60aab4624fb949f9
Jon Thompson <thompson@broadinstitute.org>
2019-06-03 16:41:38 -0400

    GPLIM-6080 second attempt at control fingerprints

--- mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/sample/ControlFixupTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/mercury/entity/sample/ControlFixupTest.java
@@ -1,15 +1,27 @@
 package org.broadinstitute.gpinformatics.mercury.entity.sample;
 
 import org.broadinstitute.gpinformatics.infrastructure.test.DeploymentBuilder;
+import org.broadinstitute.gpinformatics.mercury.control.dao.sample.ControlDao;
+import org.broadinstitute.gpinformatics.mercury.control.dao.sample.MercurySampleDao;
+import org.broadinstitute.gpinformatics.mercury.entity.envers.FixupCommentary;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.testng.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.inject.Inject;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ControlFixupTest extends Arquillian {
+
+    @Inject
+    private ControlDao controlDao;
+
+    @Inject
+    private MercurySampleDao mercurySampleDao;
 
     @Deployment
     public static WebArchive buildMercuryWar() {
@@ -17,45 +29,37 @@ public class ControlFixupTest extends Arquillian {
                 org.broadinstitute.gpinformatics.infrastructure.deployment.Deployment.DEV, "dev");
     }
 
-    @Test
+    @Test(enabled = false)
     public void gplim6080ControlFingerprintFixup() {
-        /*
-        SELECT
-            ss.lsid
-        FROM
-            geno.genotyping_run gr
-            INNER JOIN geno.metric_value mv
-                ON   mv.genotyping_run_id = gr.genotyping_run_id
-            INNER JOIN ppm.sts_sample ss
-                ON   ss.sample_id = mv.sample_id
-            INNER JOIN ppm.sts_clinical_sample scs
-                ON   scs.clinical_sample_id = ss.clinical_sample_id
-            INNER JOIN gap.object_alias oa
-                ON   oa.lsid = scs.lsid
-            INNER JOIN gap.object_alias oa2
-                ON   oa2.lsid = ss.lsid
-            INNER JOIN geno.metric_type mt
-                ON   mt.metric_type_id = mv.metric_type_id
-        WHERE
-            mt.metric_label = 'FLDM.SAMPLE_HAPMAP_CONCORDANCE'
-            AND oa.alias = 'NA12878'
-            AND mv.METRIC_VALUE = 100.0;
-         */
+        // Plate CO-28400592_04101019
+        // http://gapdev.broadinstitute.org:8080/esp/ViewFluidigmPlate.action?packetId=1200050
         Map<String, String> mapCollabIdToFpSample = new HashMap<String, String>() {{
-            put("NA12878", "broadinstitute.org:bsp.prod.sample:2522V");
-            put("NA12891", "SM-4D7US");
-            put("NA12892", "SM-41MLW");
-//            put("NA18506", "");
-//            put("NA18563", "");
-//            put("NA18603", "");
-            put("NA18609", "broadinstitute.org:bsp.prod.sample:73DPY");
-            put("NA18612", "broadinstitute.org:gap.prod.sample:4745691");
-            put("NA18942", "broadinstitute.org:gap.prod.sample:4745639");
-            put("NA18947", "broadinstitute.org:gap.prod.sample:4745704");
-            put("NA18951", "broadinstitute.org:bsp.prod.sample:73DOK");
-            put("NA18956", "broadinstitute.org:gap.prod.sample:4745675");
-            put("NA18967", "broadinstitute.org:bsp.prod.sample:73DOM");
-            put("NA18969", "broadinstitute.org:gap.prod.sample:4745642");
+            put("NA12878", "SM-IQYG2");
+            put("NA12891", "SM-IQYFS");
+            put("NA12892", "SM-IQYFP");
+            put("NA18506", "SM-IQYFG");
+            put("NA18563", "SM-IQYFV");
+            put("NA18603", "SM-IQYFA");
+            put("NA18609", "SM-IQYGB");
+            put("NA18612", "SM-IQYFY");
+            put("NA18942", "SM-IQYFD");
+            put("NA18947", "SM-IQYGK");
+            put("NA18951", "SM-IQYGE");
+            put("NA18956", "SM-IQYFJ");
+            put("NA18967", "SM-IQYGI");
+            put("NA18969", "SM-IQYFM");
         }};
+        Map<String, MercurySample> mapIdToMercurySample = mercurySampleDao.findMapIdToMercurySample(
+                mapCollabIdToFpSample.values());
+        List<Control> controls = controlDao.findAllActiveControlsByType(Control.ControlType.POSITIVE);
+        for (Control control : controls) {
+            MercurySample mercurySample = mapIdToMercurySample.get(control.getCollaboratorParticipantId());
+            Assert.assertNotNull(mercurySample);
+            System.out.println("Setting " + control.getCollaboratorParticipantId() + " to " +
+                    mercurySample.getSampleKey());
+            control.setConcordanceMercurySample(mercurySample);
+        }
+        controlDao.persist(new FixupCommentary("GPLIM-6080 set fingerprint samples for controls"));
+        controlDao.flush();
     }
 }
