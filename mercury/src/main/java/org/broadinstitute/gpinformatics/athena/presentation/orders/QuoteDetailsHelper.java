0bd2381154e919d83042f0ed78f4c84845437252
Dennis Ryan <dryan@broadinstitute.org>
2019-08-08 13:09:49 -0400

    GPLIM-6526 Why does it say null under quote on PDO Order page

--- mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/orders/QuoteDetailsHelper.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/athena/presentation/orders/QuoteDetailsHelper.java
@@ -138,7 +138,11 @@ public class QuoteDetailsHelper {
                             Optional.ofNullable(sapService.calculateOpenOrderValues(0, quote, null));
 
                     quoteDetail.setQuoteType(quoteSource);
-                    fundingHeaderStatus.ifPresent(status -> quoteDetail.setStatus(status.getStatusText()));
+                    fundingHeaderStatus.ifPresent(status -> {
+                        quoteDetail.setStatus(quote.getQuoteHeader().getQuoteStatus().getStatusText());
+                        quoteDetail.setOverallFundingStatus(status.getStatusText());
+                    });
+
                     BigDecimal fundsRemaining = quote.getQuoteHeader().fundsRemaining();
                     if(sapOrderCalculatedValues.isPresent()) {
                         fundsRemaining =fundsRemaining.subtract(sapOrderCalculatedValues.get().openDeliveryValues());
@@ -307,11 +311,11 @@ public class QuoteDetailsHelper {
 
         public String getFundsRemaining() {
             String fundsRemainingString = "";
-                fundsRemainingString = String.format(FUNDS_REMAINING_FORMAT,
-                        (quoteType == ProductOrder.QuoteSourceType.QUOTE_SERVER)?status:String.format("%s, Funding Status: %s ",
-                        status, overallFundingStatus),
-                    NumberFormat.getCurrencyInstance().format(Optional.ofNullable(fundsRemaining).orElse(0D)),
-                    NumberFormat.getCurrencyInstance().format(Optional.ofNullable(outstandingEstimate).orElse(0D)));
+            fundsRemainingString = String.format(FUNDS_REMAINING_FORMAT,
+                StringUtils.isBlank(overallFundingStatus) ? status :
+                    String.format("%s, Funding Status: %s ", status, overallFundingStatus),
+                NumberFormat.getCurrencyInstance().format(Optional.ofNullable(fundsRemaining).orElse(0D)),
+                NumberFormat.getCurrencyInstance().format(Optional.ofNullable(outstandingEstimate).orElse(0D)));
             return fundsRemainingString;
         }
 
