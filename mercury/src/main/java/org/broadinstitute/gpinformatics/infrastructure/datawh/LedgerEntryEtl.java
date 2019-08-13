8b659966338eb0a8794086adc670dc440c188d1b
Dennis Ryan <dryan@broadinstitute.org>
2019-08-13 10:50:24 -0400

    Revert "GPLIM-6535 extractTransform.backfillEtl needs to test for null products and price items"
    This reverts commit e663e665

--- mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/datawh/LedgerEntryEtl.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/infrastructure/datawh/LedgerEntryEtl.java
@@ -15,7 +15,6 @@ import javax.inject.Inject;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Root;
 import java.util.Collection;
-import java.util.Optional;
 
 @Stateful
 @TransactionManagement(TransactionManagementType.BEAN)
@@ -47,26 +46,26 @@ public class LedgerEntryEtl extends GenericEntityEtl<LedgerEntry, LedgerEntry> {
     @Override
     String dataRecord(String etlDateStr, boolean isDelete, LedgerEntry entity) {
         ProductOrderSample pdoSample = entity.getProductOrderSample();
-        Optional<PriceItem> optionalPriceItem = Optional.ofNullable(entity.getPriceItem());
-        Optional<Product> optionalProduct = Optional.ofNullable(entity.getProduct());
-        if (pdoSample == null) {
+        PriceItem priceItem = entity.getPriceItem();
+        Product product = entity.getProduct();
+        if (pdoSample == null || priceItem == null) {
             return null;
         }
         BillingSession billingSession = entity.getBillingSession();
         LedgerEntry.PriceItemType priceItemType = entity.getPriceItemType();
         return genericRecord(etlDateStr, isDelete,
-            entity.getLedgerId(),
-            format(pdoSample.getProductOrderSampleId()),
-            format(entity.getQuoteId()),
-            optionalPriceItem.map(priceItem -> format(priceItem.getPriceItemId())).orElse(""),
-            format(priceItemType != null ? priceItemType.toString() : null),
-            format(entity.getQuantity()),
-            format(billingSession != null ? billingSession.getBillingSessionId() : null),
-            format(entity.getBillingMessage()),
-            format(entity.getWorkCompleteDate()),
-            format(entity.getWorkItem()),
-            format(entity.getSapDeliveryDocumentId()),
-            optionalProduct.map(product -> format(product.getProductId())).orElse("")
-        );
+                entity.getLedgerId(),
+                format(pdoSample.getProductOrderSampleId()),
+                format(entity.getQuoteId()),
+                format(priceItem.getPriceItemId()),
+                format(priceItemType != null ? priceItemType.toString() : null),
+                format(entity.getQuantity()),
+                format(billingSession != null ? billingSession.getBillingSessionId() : null),
+                format(entity.getBillingMessage()),
+                format(entity.getWorkCompleteDate()),
+                format(entity.getWorkItem()),
+                format(entity.getSapDeliveryDocumentId()),
+                format(product.getProductId())
+                );
     }
 }
