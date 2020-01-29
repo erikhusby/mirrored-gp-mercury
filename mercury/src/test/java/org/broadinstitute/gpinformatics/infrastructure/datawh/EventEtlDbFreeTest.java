9d55c8846a1aebb2c94232b59d94672ea1a03e64
Jon Thompson <thompson@broadinstitute.org>
2020-01-29 16:34:14 -0500

    GPLIM-6069 fix merge

--- mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/datawh/EventEtlDbFreeTest.java
+++ mercury/src/test/java/org/broadinstitute/gpinformatics/infrastructure/datawh/EventEtlDbFreeTest.java
@@ -118,7 +118,7 @@ public class EventEtlDbFreeTest {
         mercurySamples.clear();
         mercurySamples.add(sample);
 
-        tst = new LabEventEtl(wfLookup, dao, sequencingSampleFactEtl, new WorkflowLoader().load(), bspUserList);
+        tst = new LabEventEtl(wfLookup, dao, sequencingSampleFactEtl, new WorkflowLoader().getWorkflowConfig(), bspUserList);
         tst.setAuditReaderDao(auditReader);
     }
 
