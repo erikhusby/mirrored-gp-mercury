a1366d0c22424d2e64fa58839f2f78ef2567121c
Jon Thompson <thompson@broadinstitute.org>
2020-03-09 16:20:26 -0400

    GPLIM-6938 disallow move beyond end of queue

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/queue/QueueEjb.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/boundary/queue/QueueEjb.java
@@ -353,9 +353,14 @@ public class QueueEjb {
 
         List<QueueGrouping> queueGroupings = new ArrayList<>(genericQueue.getQueueGroupings());
 
+        if (positionToMoveTo < 1 || positionToMoveTo >= queueGroupings.size()) {
+            messageCollection.addError("Position to move must be between 1 and " + queueGroupings.size());
+            return;
+        }
         for (QueueGrouping queueGrouping : queueGroupings) {
             if (queueGrouping.getQueueGroupingId().equals(queueGroupingId)) {
                 queueGroupingBeingMoved = queueGrouping;
+                break;
             }
         }
 
@@ -373,6 +378,7 @@ public class QueueEjb {
                     queueGrouping.setSortOrder(currentIndex++);
                 }
             }
+            // todo jmt sanity check for duplicate positions?
         }
     }
 
