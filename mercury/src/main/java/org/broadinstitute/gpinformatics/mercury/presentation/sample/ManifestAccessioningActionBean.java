1ed264b09563331ba1cb770cf10a120858177b1e
Scott Matthews <scottmat@broadinstitute.org>
2020-03-23 10:34:45 -0400

    GPLIM-6989 Handling error during Session Close to avoid terrible Stripes error page.

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/sample/ManifestAccessioningActionBean.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/presentation/sample/ManifestAccessioningActionBean.java
@@ -334,7 +334,7 @@ public class ManifestAccessioningActionBean extends CoreActionBean {
         } catch (Exception e) {
             addGlobalValidationError(e.getMessage());
             logger.error(e);
-            return getContext().getSourcePageResolution();
+            return new ForwardResolution(ACCESSION_SAMPLE_PAGE);
         }
         return new ForwardResolution(getClass(), LOAD_SESSION_ACTION);
     }
