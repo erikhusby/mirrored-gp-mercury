d877a8dcb8e2536fe5ac50f876f343de7b21eacd
Eliot Polk <epolk@broadinstitute.org>
2020-04-05 11:05:15 -0400

    GPLIM-7022 code review, metadata, and validation fixes

--- mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/Metadata.java
+++ mercury/src/main/java/org/broadinstitute/gpinformatics/mercury/entity/Metadata.java
@@ -346,6 +346,7 @@ public class Metadata {
         CALLS(Category.LAB_METRIC, DataType.NUMBER, "Calls", Visibility.USER),
         FLUIDIGM_GENDER(Category.LAB_METRIC, DataType.STRING, "Fluidigm Gender", Visibility.USER),
         INSTITUTE_ID(Category.SAMPLE, DataType.STRING, "Institute ID", Visibility.USER),
+        CDC_TIER(Category.SAMPLE, DataType.STRING, "CDC Tier", Visibility.USER),
         ;
 
         private final Category category;
